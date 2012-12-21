98f8f80177bb824dd3b707ad0f9b1dc3fcb2dd36
Arjun Guha <arjun.guha@gmail.com>
2012-12-20 20:36:45 -0500

    foo

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -6,14 +6,16 @@ open Unix
 
 let switch_thread (uid : int) (t : float) : unit =
   let rec loop () = 
-    printf "%d unpaused ...\n" uid;
+    printf "%d unpaused ...\n%!" uid;
     Thread.delay t;
     loop ()
   in loop ()
 
 
 let main () = 
-  t1 <- Thread.create 
+  Thread.create (switch_thread 10) 1.0;
+  let x =  Thread.create (switch_thread 20) 2.0 in
+  Thread.join x;
   printf "Hello, world"
 
 let _ = main ()
