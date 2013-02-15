da889da67438a853e0ec3cbd4dd0f25566affe60
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-02-15 11:34:18 -0500

    stupid ocaml equality...

--- ocaml/Regex.ml
+++ ocaml/Regex.ml
@@ -6,9 +6,12 @@ open MessagesDef
 module H = Hashtbl
 module Q = Queue
 
+exception NoPath of string*string
+
+
 let rec bfs' target graph queue =
   let (sw, path) = (Q.take queue) in
-  match sw == target with
+  match sw = target with
     | true -> path
     | false -> let () = List.iter (fun x -> Q.add (x, x :: path) queue) (try (H.find graph sw) with _ -> []); H.remove graph sw in
 	       bfs' target graph queue
@@ -16,7 +19,8 @@ let rec bfs' target graph queue =
 let bfs graph src dst = 
   let q = Queue.create () in
   let () = Q.add (src, [src]) q in
-  bfs' dst (Hashtbl.copy graph) q
+  try (bfs' dst (Hashtbl.copy graph) q) 
+  with Queue.Empty -> raise (NoPath(Int64.to_string src, Int64.to_string dst))
 
 let shortest_path = bfs
 
