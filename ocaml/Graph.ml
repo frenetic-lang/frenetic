f0632a9b5d618da6bfd2d1183401a6e99873568a
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-02-18 21:27:58 -0500

    actually make it match the type signature...

--- ocaml/Graph.ml
+++ ocaml/Graph.ml
@@ -1,64 +1,71 @@
 module H = Hashtbl
 module Q = Queue
+module M = MessagesDef
+module P = Packet
 
 module SwSet = Set.Make(
   struct
     let compare = Pervasives.compare
-    type t = MessagesDef.switchId
+    type t = M.switchId
   end)
 
-(* module type GRAPH = *)
-(* sig *)
-(*   type a = MessagesDef.switchId *)
-(*   type b = MessagesDef.portId *)
-(*   type graph *)
-(*   val create : () -> graph *)
-(*   val add_node : graph -> 'a -> unit *)
-(*   val add_edge : graph -> 'a -> 'b -> 'a -> 'b -> unit *)
-(*   val shortest_path : graph -> 'a -> 'a -> 'a list *)
-(*   val get_port : graph -> 'a -> 'a -> 'b option *)
-(*   val get_ports : graph -> a' -> b list *)
-(*   val nodes : graph -> 'a list *)
-(*   val get_other_port : graph -> 'a -> 'b -> '(a*b) option *)
-(*   val get_nbrs : graph -> 'a -> 'a list *)
-(*   exception NoPath *)
-(* end *)
+module type GRAPH =
+sig
+  type a = M.switchId
+  type b = P.portId
+  type graph
+  val create : unit -> graph
+  val add_node : graph -> a -> unit
+  val add_edge : graph -> a -> b -> a -> b -> unit
+  val shortest_path : graph -> a -> a -> a list
+  val get_port : graph -> a -> a -> b option
+  (* val get_ports : graph -> a -> b list *)
+  (* val nodes : graph -> a list *)
+  (* val get_other_port : graph -> a -> b -> (a*b) option *)
+  val get_host_port : graph -> a -> b option
+  val get_nbrs : graph -> a -> a list
+  exception NoPath of string*string
+end
 
-(* module Graph = *)
-(*   struct *)
-(*     type graph = ((MessagesDef.switchId*MessagesDef.portId) H.t) * ( *)
+module Graph : GRAPH =
+  struct
+    type a = M.switchId
+    type b = P.portId
+    type graph = (a,(b, (a*b)) H.t) H.t
+	
 
-exception NoPath of string*string
+    exception NoPath of string*string
 
 (* topo = (G, ports) *)
 (* G = { sw -> {pt} -> (sw, pt) } *)
 
-let add_node graph sw = H.add graph sw (H.create 5)
-let add_edge graph sw1 pt1 sw2 pt2 = H.add (H.find graph sw1) pt1 (sw2, pt2)
-let create () = H.create 5
-let get_nbrs graph sw = try (H.fold (fun pt1 (sw2, pt2) acc -> sw2 :: acc) (H.find graph sw) []) with _ -> []
-let copy graph = H.copy graph
+    let add_node (graph : graph) (sw : a) = H.add graph sw (H.create 5)
+    let add_edge graph sw1 pt1 sw2 pt2 = H.add (H.find graph sw1) pt1 (sw2, pt2)
+    let create () = H.create 5
+    let get_nbrs graph sw = try (H.fold (fun pt1 (sw2, pt2) acc -> sw2 :: acc) (H.find graph sw) []) with _ -> []
+    let copy graph = H.copy graph
 
-let rec bfs' target graph queue =
-  let (sw, path) = (Q.take queue) in
-  match sw = target with
-    | true -> path
-    | false -> let () = List.iter (fun x -> Q.add (x, x :: path) queue) (get_nbrs graph sw); H.remove graph sw in
-	       bfs' target graph queue
+    let rec bfs' target graph queue =
+      let (sw, path) = (Q.take queue) in
+      match sw = target with
+	| true -> path
+	| false -> let () = List.iter (fun x -> Q.add (x, x :: path) queue) (get_nbrs graph sw); H.remove graph sw in
+		   bfs' target graph queue
 
-let bfs graph src dst = 
-  let q = Queue.create () in
-  let () = Q.add (src, [src]) q in
-  try (bfs' dst (copy graph) q) 
-  with Queue.Empty -> raise (NoPath(Int64.to_string src, Int64.to_string dst))
+    let bfs graph src dst = 
+      let q = Queue.create () in
+      let () = Q.add (src, [src]) q in
+      try (bfs' dst (copy graph) q) 
+      with Queue.Empty -> raise (NoPath(Int64.to_string src, Int64.to_string dst))
 
-let shortest_path = bfs
+    let shortest_path = bfs
 
-let get_port topo s1 s2 = let () = Printf.printf "get_hop %Ld %Ld\n" s1 s2 in
-			 try (H.fold (fun pt (sw, pt') acc -> if sw = s2 then (Some pt) else acc) (H.find topo s1) None) with _ -> None
+    let get_port topo s1 s2 = let () = Printf.printf "get_hop %Ld %Ld\n" s1 s2 in
+			      try (H.fold (fun pt (sw, pt') acc -> if sw = s2 then (Some pt) else acc) (H.find topo s1) None) with _ -> None
 
 
 (* let get_other_port topo sw p = try (Some (Hashtbl.find (Hashtbl.find (snd topo) s1) s2) *)
-let nodes topo = H.fold (fun sw sw' acc -> SwSet.union (SwSet.singleton sw) acc) (fst topo) SwSet.empty
+    (* let nodes topo = H.fold (fun sw sw' acc -> SwSet.union (SwSet.singleton sw) acc) (fst topo) SwSet.empty *)
 
-let get_host_port topo host = try (H.fold (fun pt (sw, pt') acc -> Some pt') (Hashtbl.find topo host) None) with _ -> None
+    let get_host_port topo host = try (H.fold (fun pt (sw, pt') acc -> Some pt') (Hashtbl.find topo host) None) with _ -> None
+  end
