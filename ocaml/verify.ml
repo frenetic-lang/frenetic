78e1e9ecb3ec178a7b7dca59cf84411a0f9f475d
Rebecca Coombes <rac347@cornell.edu>
2013-02-22 19:08:46 -0500

    fixed error

--- ocaml/verify.ml
+++ ocaml/verify.ml
@@ -51,6 +51,18 @@ let forwards act pkt1 pkt2 =
     | GetPacket gph -> 
       ZFalse
 
+(*let topo_forwards topo pkt1 pkt2 =
+  ZAnd [ equals ["DlSrc"; "DlDst"] pkt1 pkt2
+       ; ZOr (List.fold_left (fun acc (Link(s1, p1), Link(s2, p2)) -> 
+	 ZOr [ ZAnd [ ZEquals (PktHeader ("Switch", pkt1), Primitive s1)
+		    ; ZEquals (PktHeader ("InPort", pkt1), Primitive (Int64.of_int p1))
+		    ; ZEquals (PktHeader ("Switch", pkt2), Primitive s2)
+		    ; ZEquals (PktHeader ("InPort", pkt2), Primitive (Int64.of_int p2)) ]
+	     ; ZAnd [ ZEquals (PktHeader ("Switch", pkt1), Primitive s2)
+		    ; ZEquals (PktHeader ("InPort", pkt1), Primitive (Int64.of_int p2))
+		    ; ZEquals (PktHeader ("Switch", pkt2), Primitive s1)
+		    ; ZEquals (PktHeader ("InPort", pkt2), Primitive (Int64.of_int p1)) ]]::acc) [] topo) ]*)
+
 let topo_forwards topo pkt1 pkt2 =
   ZAnd [ equals ["DlSrc"; "DlDst"] pkt1 pkt2
        ; ZOr (List.fold_left (fun acc (Link(s1, p1), Link(s2, p2)) -> 
@@ -71,11 +83,12 @@ let rec encode_policy pol pkt1 pkt2 =
 let rec n_forwards n topo pol pkt1 pkt2 =
   match n with
     | 0 -> equals ["Switch"; "InPort"; "DlDst"; "DlSrc"] pkt1 pkt2
-    | 1 -> encode_policy pol pkt1 pkt2
+    | 1 -> ZOr [encode_policy pol pkt1 pkt2
+	       ; equals ["Switch"; "InPort"; "DlDst"; "DlSrc"] pkt1 pkt2 ]
     | _ -> let pkt1' = fresh () in
 	   let pkt1'' = fresh () in
-	   ZAnd [encode_policy pol pkt1 pkt1'
-		; topo_forwards topo pkt1' pkt1''
+	   ZAnd [ encode_policy pol pkt1 pkt1'
+		;  topo_forwards topo pkt1' pkt1''
 		; n_forwards (n-1) topo pol pkt1'' pkt2]
       
 (* temporary front-end for verification stuff *)
@@ -83,7 +96,8 @@ let () =
   let s1 = Int64.of_int 1 in
   let s2 = Int64.of_int 2 in
   let s3 = Int64.of_int 3 in
-  let topo = [(Link (s1, 2), Link (s3, 1)); (Link (s1,3), Link (s2, 1)); (Link (s3, 3), Link (s2, 2))] in
+  let topo = [ (Link (s1, 4), Link (s3, 1)); (Link (s1,3), Link (s2, 1)); (Link (s3, 3), Link (s2, 2))
+	     ; (Link (s3, 1), Link (s1, 4)); (Link (s2,1), Link (s1, 3)); (Link (s2, 2), Link (s3, 3))] in
   let pol = Pol (All, [ToAll]) in
   let pkt1 = fresh () in
   let pkt2 = fresh () in
