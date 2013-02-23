4664be396f98ef7bb563d8e47648087f7ac0f06f
Arjun Guha <arjun.guha@gmail.com>
2013-02-23 16:38:54 -0500

    fixed big-endian bug ... classic

--- ocaml/Test.ml
+++ ocaml/Test.ml
@@ -57,6 +57,58 @@ module Test1 = struct
            Lwt_main.run (body ()))
          (fun () -> tear_down ()))
 
+end 
+
+module Test2 = struct
+
+  open NetCoreEval
+  open NetCoreCompiler
+
+  let pt1 = 1
+  let pt2 = 2
+
+  let pol1 = PoUnion (PoAtom (PrAll, [Forward (PhysicalPort pt1)]),
+                      PoAtom (PrAll, [Forward (PhysicalPort pt2)]))
+
+  let go = 
+    TestList [ (*
+"optimizer test" >::
+    (fun () -> 
+      let ft = compile_opt pol1 0L in
+      Printf.eprintf "length is %d\n" (List.length ft);
+      assert_equal (List.length ft) 2)
+               *) ]
+
 end
 
-let _ = run_test_tt_main Test1.go
+module Test3 = struct
+
+  open Util
+
+  let test0  = "get_byte test" >::
+    (fun () ->
+      printf "get_byte is %d\n"  (Char.code (get_byte 0x112233445566L 5));
+      assert_equal (get_byte 0x112233445566L 5) '\x11')
+
+  let test1 = "manual-check for big-endian encoding" >::
+    (fun () ->
+      assert_equal (bytes_of_mac 0x112233445566L)
+        "\x66\x55\x44\x33\x22\x11")
+
+  let test2 = "mac address parsing test" >::
+      (fun () ->
+        let k = 0x1234567890abL in
+        printf "bytes is %s, str is %Lx\n" (bytes_of_mac k) (mac_of_bytes (bytes_of_mac k));
+        assert_equal (mac_of_bytes (bytes_of_mac k)) k)
+    
+  let go = 
+    TestList [test0; test1; test2]
+
+
+end
+
+
+let _ = run_test_tt_main 
+  (TestList [ Test1.go; 
+              Test2.go;
+              Test3.go ])
--- ocaml/Util.ml
+++ ocaml/Util.ml
@@ -10,15 +10,18 @@ let bit (x : int32) (n : int) (v : bool) : int32 =
   if v then set_bit n x else clear_bit n x
 
 let mac_of_bytes (str:string) : int64 = 
-  let str = Format.sprintf "\x00\x00%s" str in 
-  if String.length str != 8 then
-    raise (Invalid_argument "mac_of_bytes expected eight-byte string");
-  let n = ref 0L in
-  for i = 0 to 7 do
-    let b = Int64.of_int (Char.code (String.get str i)) in
-    n := Int64.logor !n (Int64.shift_left b (8 * i))
-  done;
-  !n  
+  if String.length str != 6 then
+    raise (Invalid_argument 
+             (Format.sprintf "mac_of_bytes expected six-byte string, got %d
+                              bytes" (String.length str)));
+  let byte n = Int64.of_int (Char.code (String.get str n)) in
+  let open Int64 in
+  logor (shift_left (byte 5) (8 * 5))
+    (logor (shift_left (byte 4) (8 * 4))
+       (logor (shift_left (byte 3) (8 * 3))
+          (logor (shift_left (byte 2) (8 * 2))
+             (logor (shift_left (byte 1) (8 * 1))
+                (byte 0)))))
 
 let get_byte (n:int64) (i:int) : char = 
   if i < 0 or i > 5 then
@@ -28,8 +31,8 @@ let get_byte (n:int64) (i:int) : char =
 
 let bytes_of_mac (x:int64) : string = 
   Format.sprintf "%c%c%c%c%c%c"
-    (get_byte x 5) (get_byte x 4) (get_byte x 3)
-    (get_byte x 2) (get_byte x 1) (get_byte x 0)
+    (get_byte x 0) (get_byte x 1) (get_byte x 2)
+    (get_byte x 3) (get_byte x 4) (get_byte x 5)
 
 module type SAFESOCKET = sig
   type t = Lwt_unix.file_descr
