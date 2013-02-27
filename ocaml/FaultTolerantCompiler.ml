06640a93f94a77b5f791c7f198e2860f0abb3773
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-02-27 11:10:55 -0500

    more refactoring

--- ocaml/FaultTolerantCompiler.ml
+++ ocaml/FaultTolerantCompiler.ml
@@ -17,10 +17,10 @@ let ofpp_any = Int32.of_int (-1)
 (* Not even specified in OF 1.3. It's like they're not even trying... *)
 let ofpg_any = Int32.of_int (-1)
 
-let rec watchport acts = match acts with
-  | NetCoreEval.Forward (modif, MessagesDef.PhysicalPort pid) :: acts -> (Int32.of_int pid)
-  | _ :: acts -> watchport acts
-  | _ -> ofpp_any
+(* let rec watchport acts = match acts with *)
+(*   | NetCoreEval.Forward (modif, MessagesDef.PhysicalPort pid) :: acts -> (Int32.of_int pid) *)
+(*   | _ :: acts -> watchport acts *)
+(*   | _ -> ofpp_any *)
 
 let rec mapi' idx f lst = match lst with
   | elm :: lst -> f idx elm :: mapi' (idx+1) f lst
@@ -30,7 +30,8 @@ let mapi f lst = mapi' 0 f lst
 
 (* let insert_groups = mapi (fun idx (pat,(a,b)) -> (pat, (Int32.of_int idx, [(0, (watchport a), a); (0, (watchport b), b)]))) *)
 
-let remove_inport = map (fun (pat, acts) -> (  { pat with ptrnInPort = W.WildcardAll }, acts))
+let remove_inport = let project ((pat, acts) : Pattern.pattern * actions list) = ({ pat with ptrnInPort = W.WildcardAll }, acts) in
+		      map project
 
 let compile_nc = compile_opt
 
@@ -43,7 +44,7 @@ end
 let rec compile_pb pri bak sw =
     let pri_tbl = compile_nc pri sw in
     let bak_tbl = compile_nc bak sw in
-    let merge a b = ((Gensym.next ()), [((watchport a), a); ((watchport b), b)]) in
+    let merge a b = ((Gensym.next ()), [a; b]) in
     let overlap = (inter merge pri_tbl (remove_inport bak_tbl)) in
     let ft_tbl = map (fun (pat, (gid,buckets)) -> (pat, [Group gid])) overlap in
     (ft_tbl @ pri_tbl @ bak_tbl, map snd overlap)
