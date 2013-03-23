aea9a9f0632100fa09f79ca8836a6612cbb2b665
Arjun Guha <arjun.guha@gmail.com>
2013-03-23 13:14:41 -0400

    old stuff

--- coq/FwOF/FwOFSimpleController.v
+++ coq/FwOF/FwOFSimpleController.v
@@ -119,14 +119,112 @@ Module Make (NetAndPol : NETWORK_AND_POLICY) <: ATOMS.
   Definition controller_recv := Recv.
   Definition controller_step := Step.
   Definition controller_send := Send.
-(*
 
-  Fixpoint send (st : state) -> option (st * switchId * fromController) :=
+  Fixpoint send_queued (swsts : list switchState) :=
+    match swsts with
+      | nil => None
+      | (SwitchState sw (msg :: msgs)) :: ss =>
+        Some (SwitchState sw msgs :: ss, sw, msg)
+      | (SwitchState sw nil) :: ss =>
+        match send_queued ss with
+          | None => None
+          | Some (ss', sw',msg) => Some (SwitchState sw nil :: ss', sw', msg)
+        end
+    end.
+
+  Fixpoint send (st : state) :=
     match st with
       | State ((SrcDst sw _ _ pt pk) :: pks) sws =>
-        (State pks sws, sw, PacketOut pt pk)
-      | State nil ((SwitchState sw (msg :: msgs)) :: sws) =>
-        (State nil ((SwitchState sw msgs) :: sws), sw, msg)
-      | State nil (SwitchState 
-*)
+        Some (State pks sws, sw, PacketOut pt pk)
+      | State nil ss => 
+        match send_queued ss with
+          | None => None
+          | Some (ss', sw, msg) => Some (State nil ss', sw, msg)
+        end
+    end.
+
+  Fixpoint recv (st : state) (sw : switchId) (msg : fromSwitch) :=
+    match msg with
+      | BarrierReply _ => st
+      | PacketIn pt pk =>
+        match st with
+          | State pktOuts ss =>
+            State (mkPktOuts sw pt pk ++ pktOuts) ss
+        end
+    end.
+  
+  Hint Constructors Send Recv.
+
+  Lemma Send_cons : forall s ss1 ss2 sw msg,
+    Send (State nil ss1) (State nil ss2) sw msg ->
+    Send (State nil (s::ss1)) (State nil (s::ss2)) sw msg.
+  Proof with auto with datatypes.
+    intros.
+    inversion H; subst.
+    do 2 rewrite -> app_comm_cons...
+  Qed.
+
+  Lemma send_queued_compat : forall ss1 ss2 sw msg,
+    send_queued ss1 = Some (ss2, sw, msg) ->
+    Send (State nil ss1) (State nil ss2) sw msg.
+  Proof with auto with datatypes.
+    intros.
+    generalize dependent ss2.
+    generalize dependent sw.
+    generalize dependent msg.
+    induction ss1; intros...
+    simpl in H. inversion H.
+    simpl in H.
+    destruct a.
+    + destruct pendingCtrlMsgs0.
+      - remember (send_queued ss1) as rest.
+        destruct rest.
+        * destruct p.
+          destruct p.
+          remember (IHss1 f s l eq_refl) as J eqn:X; clear X.
+          inversion H; subst.
+          apply Send_cons...
+        * inversion H.
+      - inversion H; subst.
+        assert (SwitchState sw (msg::pendingCtrlMsgs0) :: ss1 = 
+                nil ++ SwitchState sw (msg::pendingCtrlMsgs0) :: ss1) as X...
+        rewrite -> X; clear X.
+        assert (SwitchState sw pendingCtrlMsgs0 :: ss1 = 
+                nil ++ SwitchState sw pendingCtrlMsgs0 :: ss1) as X...
+        rewrite -> X; clear X...
+  Qed.
+
+  Lemma send_compat : forall st1 st2 sw msg,
+    send st1 = Some (st2, sw, msg) ->
+    Send st1 st2 sw msg.
+  Proof with auto with datatypes.
+    intros.
+    destruct st1.
+    simpl in H.
+    destruct pktsToSend0.
+    + remember (send_queued switchStates0) as J.
+      destruct J.
+      - destruct p as [[ss2 sw2] msg2].
+        symmetry in HeqJ; apply send_queued_compat in HeqJ.
+        inversion H; subst...
+      - inversion H.
+    + destruct s.
+      inversion H; subst...
+  Qed.
+
+
+  Lemma recv_compat : forall st1 sw msg st2,
+    recv st1 sw msg = st2 ->
+    Recv st1 sw msg st2.
+  Proof with auto with datatypes.
+    intros.
+    destruct msg...
+    + destruct st1.
+      simpl in H.
+      subst...
+    + destruct st1.
+      simpl in H.
+      subst...
+  Qed.
+
 End Make.
