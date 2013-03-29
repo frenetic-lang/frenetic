7960e9b5e692ad82b52018d72cca3ed79776d48b
Arjun Guha <arjun.guha@gmail.com>
2013-03-29 16:25:50 -0400

    removed licenses -- you still do no have a license to use this software

--- py/fattree_topo.py
+++ py/fattree_topo.py
@@ -1,34 +1,3 @@
-################################################################################
-# The Frenetic Project                                                         #
-# frenetic@frenetic-lang.org                                                   #
-################################################################################
-# Licensed to the Frenetic Project by one or more contributors. See the        #
-# NOTICE file distributed with this work for additional information            #
-# regarding copyright and ownership. The Frenetic Project licenses this        #
-# file to you under the following license.                                     #
-#                                                                              #
-# Redistribution and use in source and binary forms, with or without           #
-# modification, are permitted provided the following conditions are met:       #
-# - Redistributions of source code must retain the above copyright             #
-#   notice, this list of conditions and the following disclaimer.              #
-# - Redistributions in binary form must reproduce the above copyright          #
-#   notice, this list of conditions and the following disclaimer in            #
-#   the documentation or other materials provided with the distribution.       #
-# - The names of the copyright holds and contributors may not be used to       #
-#   endorse or promote products derived from this work without specific        #
-#   prior written permission.                                                  #
-#                                                                              #
-# Unless required by applicable law or agreed to in writing, software          #
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    #
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     #
-# LICENSE file distributed with this work for specific language governing      #
-# permissions and limitations under the License.                               #
-################################################################################
-# /updates/examples/routing_topo.py                                            #
-# Shortest-path routing topology                                               #
-# $Id$ #
-################################################################################
-
 from mininet.topo import Topo, Node
 
 # 4 hosts on each edge switch
