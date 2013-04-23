8df3efe3e12cd1dc8c156327d307d50f32c953f0
Arjun Guha <arjun.guha@gmail.com>
2013-04-23 13:09:24 -0400

    Nicer interface to vanilla OpenFlow.

--- ocaml/OpenFlow0x01.ml
+++ ocaml/OpenFlow0x01.ml
@@ -1,3 +1,4 @@
 module Parser = OpenFlow0x01_Parser
 module Platform = OpenFlow0x01_Platform
 module Sig = OpenFlow0x01_Sig
+module Types = OpenFlow0x01Types
--- ocaml/OpenFlow0x01.mli
+++ ocaml/OpenFlow0x01.mli
@@ -11,7 +11,7 @@ module Parser : sig
     open OpenFlow0x01Types
 
     type t = message
-      
+        
     val parse : Header.t -> Cstruct.t -> (xid * t) option
     val to_string : t -> string
     val marshal : xid -> t -> string
@@ -44,7 +44,7 @@ module Sig : sig
         itself respond with an [ECHO_REPLY] and block for the next
         message. *)
     val recv_from_switch : switchId -> (xid * message) Lwt.t
-    
+      
     (** [accept_switch] blocks until a switch connects, handles the
         OpenFlow handshake, and returns after the switch sends a
         [FEATURES_REPLY] message. *)
@@ -55,7 +55,7 @@ end
 module Platform : sig
 
   (** An implementation of [Sig.PLATFORM]. The documentation for
-    [PLATFORM] describes its features. *)
+      [PLATFORM] describes its features. *)
 
   open Lwt_unix
   open OpenFlow0x01Types
@@ -71,5 +71,142 @@ module Platform : sig
 
   (** [shutdown] gracefully shuts down the server *) 
   val shutdown : unit -> unit 
+
+end
+
+module Types : sig
+
+  open Packet
+  open WordInterface
+
+  type of_match = {
+    matchDlSrc : dlAddr option;
+    matchDlDst : dlAddr option;
+    matchDlTyp : dlTyp option;
+    matchDlVlan : dlVlan option;
+    matchDlVlanPcp : dlVlanPcp option;
+    matchNwSrc : nwAddr option; 
+    matchNwDst : nwAddr option;
+    matchNwProto : nwProto option;
+    matchNwTos : nwTos option;
+    matchTpSrc : tpPort option;
+    matchTpDst : tpPort option;
+    matchInPort : portId option 
+  }
+
+  type capabilities = {
+    flow_stats : bool;
+    table_stats : bool;
+    port_stats : bool; 
+    stp : bool;
+    ip_reasm : bool;
+    queue_stats : bool; 
+    arp_match_ip : bool 
+  }
+
+  type actions = { 
+    output : bool;
+    set_vlan_id : bool;
+    set_vlan_pcp : bool;
+    strip_vlan : bool; 
+    set_dl_src : bool;
+    set_dl_dst : bool;
+    set_nw_src : bool;
+    set_nw_dst : bool; 
+    set_nw_tos : bool;
+    set_tp_src : bool;
+    set_tp_dst : bool;
+    enqueue : bool;
+    vendor : bool 
+  }
+
+  type features = { 
+    switch_id : Word64.t;
+    num_buffers : Word32.t;
+    num_tables : Word8.t;
+    supported_capabilities : capabilities;
+    supported_actions : actions 
+  }
+
+  type flowModCommand =
+    | AddFlow
+    | ModFlow
+    | ModStrictFlow
+    | DeleteFlow
+    | DeleteStrictFlow
+
+  type switchId = Word64.t
+      
+  type priority = Word16.t
+      
+  type bufferId = Word32.t
+
+  type pseudoPort =
+    | PhysicalPort of portId
+    | InPort
+    | Flood
+    | AllPorts
+    | Controller of Word16.t
+
+  type action =
+    | Output of pseudoPort
+    | SetDlVlan of dlVlan
+    | SetDlVlanPcp of dlVlanPcp
+    | StripVlan
+    | SetDlSrc of dlAddr
+    | SetDlDst of dlAddr
+    | SetNwSrc of nwAddr
+    | SetNwDst of nwAddr
+    | SetNwTos of nwTos
+    | SetTpSrc of tpPort
+    | SetTpDst of tpPort
+
+  type actionSequence = action list
+
+  type timeout =
+    | Permanent
+    | ExpiresAfter of Word16.t
+
+  type flowMod = {
+    mfModCmd : flowModCommand; 
+    mfMatch : of_match;
+    mfPriority : priority; 
+    mfActions : actionSequence;
+    mfCookie : Word64.t; mfIdleTimeOut : timeout;
+    mfHardTimeOut : timeout; mfNotifyWhenRemoved : bool;
+    mfApplyToPacket : bufferId option;
+    mfOutPort : pseudoPort option; mfCheckOverlap : bool }
+
+  type packetInReason =
+    | NoMatch
+    | ExplicitSend
+
+  type packetIn = {
+    packetInBufferId : bufferId option;
+    packetInTotalLen : Word16.t; 
+    packetInPort : portId;
+    packetInReason_ : packetInReason; 
+    packetInPacket :  packet }
+
+  type xid = Word32.t
+
+  type packetOut = { 
+    pktOutBufOrBytes : (bufferId, bytes) Datatypes.sum;
+    pktOutPortId : portId option;
+    pktOutActions : actionSequence 
+  }
+
+  type message =
+    | Hello of bytes
+    | EchoRequest of bytes
+    | EchoReply of bytes
+    | FeaturesRequest
+    | FeaturesReply of features
+    | FlowModMsg of flowMod
+    | PacketInMsg of packetIn
+    | PacketOutMsg of packetOut
+    | BarrierRequest
+    | BarrierReply
+
 
 end
