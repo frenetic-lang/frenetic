c859e708d5597de0f7b61c04f8a1d1b6bd7b6001
Arjun Guha <arjun.guha@gmail.com>
2013-05-01 21:25:38 -0400

    helpers

--- ocaml/OpenFlow0x01.ml
+++ ocaml/OpenFlow0x01.ml
@@ -1,6 +1,39 @@
 module Parser = OpenFlow0x01_Parser
 
-module Types = OpenFlow0x01Types
+module Types = struct
+  include OpenFlow0x01Types
+
+  let match_all = {
+    matchDlSrc = None;
+    matchDlDst = None;
+    matchDlTyp = None;
+    matchDlVlan = None;
+    matchDlVlanPcp = None;
+    matchNwSrc = None; 
+    matchNwDst = None;
+    matchNwProto = None;
+    matchNwTos = None;
+    matchTpSrc = None;
+    matchTpDst = None;
+    matchInPort = None
+  }
+
+  let delete_all_flows = 
+    FlowModMsg {
+      mfModCmd = DeleteFlow;
+      mfMatch = match_all;
+      mfPriority = 0;
+      mfActions = [];
+      mfCookie = 0L;
+      mfIdleTimeOut = Permanent;
+      mfHardTimeOut = Permanent;
+      mfNotifyWhenRemoved = false;
+      mfApplyToPacket = None;
+      mfOutPort = None;
+      mfCheckOverlap = false
+    }
+
+end
 
 module type PLATFORM = sig
   open Types
