21dd6309429085f3b83d65343bba35057ef10e74
Arjun Guha <arjun.guha@gmail.com>
2013-05-02 08:33:07 -0400

    possibly correct sequencing

--- coq/Classifier/Classifier.v
+++ coq/Classifier/Classifier.v
@@ -65,3 +65,73 @@ Fixpoint prioritize
     | nil => nil
     | (pat, act) :: lst' => (prio, pat, act) :: (prioritize (pred prio) lst')
   end.
+
+Section Sequencing.
+
+
+  Variable A : Type.
+  Variable Atom : Type.
+
+  Variable zero : A.
+  Variable par_action : A -> A -> A.
+  Variable seq_action : A -> A -> A.
+  Variable apply_atom : Atom -> portId * packet -> portId * packet.
+  Variable mask_pat : Atom -> pattern -> pattern.
+  Variable atoms : A -> list Atom.
+
+  Definition apply_action (action : A) (ptpk : portId * packet) :=
+    map (fun a => apply_atom a ptpk) (atoms action).
+
+  Local Notation "x || y" := (par_action x y).
+  Local Notation "x ; y" := (seq_action x y) (at level 51, right associativity).
+
+  Fixpoint par_actions (lst : list A) :=
+    match lst with
+      | nil => zero
+      | act :: lst' => par_action act (par_actions lst')
+    end.
+
+  Section Denotation.
+
+    Definition scan' tbl ptpk := 
+      match ptpk with
+        | (pt, pk) => @scan A zero tbl pt pk
+      end.
+
+    Definition seq (tbl1 tbl2 : Classifier A) ptpk :=
+      scan' tbl1 ptpk;
+      par_actions (map (scan' tbl2) (apply_action (scan' tbl1 ptpk) ptpk)).
+
+  End Denotation.
+
+  Section Compiler.
+
+    Fixpoint unions (lst : list (list (pattern * A))) :=
+      match lst with
+        | nil => nil
+        | tbl :: lst' => union par_action tbl (unions lst')
+      end.
+
+    Fixpoint Pick (p1 : pattern) (a1 : A) (atom : Atom) (tbl : Classifier A) :=
+      match tbl with
+        | nil => nil
+        | ((p,a) :: tbl') => 
+           (Pattern.inter p1 (mask_pat atom p),
+            a1; a) 
+             :: (Pick p1 a1 atom tbl')
+      end.
+
+    Fixpoint sequence (tbl1 tbl2 : Classifier A) :=
+      match tbl1 with 
+        | nil => nil
+        | (p,a) :: tbl1' =>
+          match atoms a with
+            | nil => (p, zero) :: sequence tbl1' tbl2
+            | ats => unions (map (fun atom => Pick p a atom tbl2) ats) ++
+                            sequence tbl1' tbl2
+          end
+      end.
+
+  End Compiler.
+
+End Sequencing.
