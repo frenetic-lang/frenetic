54130a4fe1001980566175ad08c9d914cf7e29e0
Arjun Guha <arjun.guha@gmail.com>
2013-05-06 13:44:08 -0400

    neeed for coq8.4

--- coq/Extraction/Extract.v
+++ coq/Extraction/Extract.v
@@ -5,6 +5,9 @@ Require Import Pattern.PatternInterface.
 
 Require Import Extraction.OCaml.
 
+Extract Inductive nat => int [ "0" "Pervasives.succ" ]
+ "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
+
 Cd "../../ocaml/extracted". 
 Recursive Extraction Library NetCoreCompiler.
 Recursive Extraction Library PatternInterface.
