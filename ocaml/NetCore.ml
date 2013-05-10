fc698e3803b96a1a856fab14aaa8fff55264d54a
Nate Foster <jnfoster@cs.cornell.edu>
2013-05-10 06:57:59 -0400

    hygeine

--- ocaml/NetCore.ml
+++ ocaml/NetCore.ml
@@ -7,11 +7,3 @@ let start_controller port policy_stream =
   OpenFlow0x01.Platform.init_with_port port;
   Lwt_main.run (NetCoreOpenFlow.start_controller policy_stream)
 
-(*
-module Modules = struct
-
-  module Learning = NetCore_MacLearning.Learning
-  module Routing = NetCore_MacLearning.Routing
-
-end
-*)
