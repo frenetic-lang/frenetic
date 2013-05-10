67069ba45e3997e8cfb3780646903dd08774c5c1
Nate Foster <jnfoster@cs.cornell.edu>
2013-05-10 07:00:33 -0400

    Eliminate short modules

--- ocaml/Misc.ml
+++ ocaml/Misc.ml
@@ -1,5 +1,43 @@
-module Log = Misc_Log
-module Lwt_channel = Misc_Lwt_channel
+module Log = struct
+
+  let log = ref stderr
+    
+  let get_log_chan () : out_channel = !log
+    
+  let set_log_file (replace : bool) (filename : string) : unit =
+    let chan = 
+      match replace with
+      | true -> open_out filename 
+      | false -> 
+        open_out_gen [Open_wronly; Open_text; Open_creat; Open_excl] 
+          0o600 filename in
+    log := chan;
+    at_exit (fun () -> close_out chan)
+      
+  let printf (fmt : ('a, out_channel, unit) format) : 'a =
+    Printf.printf ".%!";
+    Printf.fprintf !log fmt
+end
+
+module Lwt_channel = struct
+  type 'a t = {
+    stream : 'a Lwt_stream.t;
+    push : 'a option -> unit
+  }
+    
+  let of_pushed_stream stream push = { stream; push }
+    
+  let create () = 
+    let (stream, push) = Lwt_stream.create () in
+    of_pushed_stream stream push
+      
+  let send (v : 'a) (chan : 'a t) = Lwt.return (chan.push (Some v))
+    
+  let recv (chan : 'a t) = Lwt_stream.next chan.stream
+    
+  let to_stream (chan : 'a t) = chan.stream
+    
+end
 
 let test_bit n x = 
   Int32.logand (Int32.shift_right_logical x n) Int32.one = Int32.one
