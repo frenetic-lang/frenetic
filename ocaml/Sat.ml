cf73e09d075684632406490578d9d022a987f90c
Nate Foster <jnfoster@cs.cornell.edu>
2013-05-10 13:27:09 -0400

    Rejigger

--- ocaml/Sat.ml
+++ ocaml/Sat.ml
@@ -1,3 +1,18 @@
+(* Topology Stuff *)
+type link = Link of OpenFlow0x01.Types.switchId * Packet.Types.portId
+
+type topology = Topology of (link * link) list
+
+let reverse_edge (sp1, sp2) = 
+  (sp2, sp1)
+
+let bidirectionalize (Topology topo) = 
+  let topo' = 
+    List.fold_left 
+      (fun acc edge -> edge::(reverse_edge edge)::acc) 
+      [] topo in 
+  Topology topo'
+
 (* Switch, Port, DlDst, DlSrc *)
 type zPacket = 
   ZPacket of Int64.t * int * Int64.t * Int64.t
