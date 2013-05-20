a3bcb0a66d857cd5cc35a3e32ee1cf255b2c0614
Nate Foster <jnfoster@cs.cornell.edu>
2013-05-20 14:45:33 -0400

    Moar code

---
+++ src/Syntax.mli
@@ -0,0 +1,94 @@
+open OpenFlow0x01.Types
+open NetworkPacket
+open Packet.Types
+open Misc
+open List
+open Word
+
+module Internal : sig
+  type pred =
+  | PrHdr of Pattern.t
+  | PrOnSwitch of switchId
+  | PrOr of pred * pred
+  | PrAnd of pred * pred
+  | PrNot of pred
+  | PrAll
+  | PrNone
+
+  type pol =
+  | PoAction of Action.Output.t
+  | PoFilter of pred
+  | PoUnion of pol * pol
+  | PoSeq of pol * pol
+
+  type payload = 
+  | Buf of bufferId
+  | Data of bytes 
+
+  type value =
+  | Pkt of switchId * Pattern.port * packet * payload
+
+  val match_pred : pred -> switchId -> Pattern.port -> packet -> bool
+
+  val classify : pol -> value -> value list
+
+  val pred_to_string : pred -> string
+
+  val pol_to_string : pol -> string
+
+  val value_to_string : value -> string 
+end
+
+module External : sig
+
+  type get_packet_handler = 
+    switchId -> portId -> packet -> unit
+
+  type predicate =
+  | And of predicate * predicate
+  | Or of predicate * predicate
+  | Not of predicate
+  | All
+  | NoPackets
+  | Switch of switchId
+  | InPort of portId
+  | DlSrc of Int64.t
+  | DlDst of Int64.t
+  | DlVlan of int option (** 12-bits *)
+  | SrcIP of Int32.t
+  | DstIP of Int32.t
+  | TcpSrcPort of int (** 16-bits, implicitly IP *)
+  | TcpDstPort of int (** 16-bits, implicitly IP *)
+
+  type action =
+  | To of int
+  | ToAll
+  | UpdateDlSrc of Int64.t * Int64.t
+  | UpdateDlDst of Int64.t * Int64.t
+  | UpdateDlVlan of int option * int option (** 12-bits *)
+  | GetPacket of get_packet_handler
+      
+  type policy =
+  | Empty
+  | Act of action
+  | Par of policy * policy (** parallel composition *)
+  | Seq of policy * policy
+  | Filter of predicate
+  | Slice of predicate * policy * predicate
+
+  val par : policy list -> policy
+      
+  val predicate_to_string : predicate -> string
+        
+  val action_to_string : action -> string
+        
+  val policy_to_string : policy -> string
+
+end
+
+val desugar : 
+  (unit -> int) -> 
+  (unit -> int option) -> 
+  External.policy -> 
+  (int, External.get_packet_handler) Hashtbl.t -> 
+  Internal.pol
