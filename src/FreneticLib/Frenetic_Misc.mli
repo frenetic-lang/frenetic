5ed436978ba814bb4685ea0811f5dd244cf07dfe
Nate Foster <jnfoster@cs.cornell.edu>
2013-05-22 00:22:23 -0400

    More Makefile hacking. Got FreneticLib running

--- src/FreneticLib/Frenetic_Misc.mli
+++ src/FreneticLib/Frenetic_Misc.mli
@@ -12,39 +12,18 @@ module Log : sig
 
 end
 
-(** [Lwt_channel] is a cleaner than [Lwt_stream]. *)
-module Lwt_channel : sig
+(* val test_bit : int -> Int32.t -> bool *)
+(* val clear_bit : int -> Int32.t -> Int32.t *)
+(* val set_bit : int -> Int32.t -> Int32.t *)
+(* val bit : Int32.t -> int -> bool -> Int32.t *)
+(* val mac_of_bytes : string -> Int64.t *)
+(* val get_byte : Int64.t -> int -> int *)
+(* val string_of_mac : Int64.t -> string *)
+(* val bytes_of_mac : Int64.t -> string *)
+(* val string_of_ip : Int32.t -> string *)
 
-  type 'a t
-
-  (** [of_pushed_stream stream push] creates a channel from an [Lwt_stream] and
-      its push function. The type system cannot ensure that [push] actually
-      pushes values onto [stream], so use with care. *)
-  val of_pushed_stream : 'a Lwt_stream.t -> ('a option -> unit) -> 'a t
-
-  val create : unit -> 'a t
-
-  val send : 'a -> 'a t -> unit Lwt.t
-
-  val recv : 'a t -> 'a Lwt.t
-
-  val to_stream : 'a t -> 'a Lwt_stream.t
-
-end
-
-
-val test_bit : int -> Int32.t -> bool
-val clear_bit : int -> Int32.t -> Int32.t
-val set_bit : int -> Int32.t -> Int32.t
-val bit : Int32.t -> int -> bool -> Int32.t
-val mac_of_bytes : string -> Int64.t
-val get_byte : Int64.t -> int -> int
-val string_of_mac : Int64.t -> string
-val bytes_of_mac : Int64.t -> string
-val string_of_ip : Int32.t -> string
-
-val filter_map : ('a -> 'b option) -> 'a list -> 'b list
-val concat_map : ('a -> 'b list) -> 'a list -> 'b list
+(* val filter_map : ('a -> 'b option) -> 'a list -> 'b list *)
+(* val concat_map : ('a -> 'b list) -> 'a list -> 'b list *)
 val intersperse : 'a -> 'a list -> 'a list
 
 val string_of_list : ('a -> string) -> 'a list -> string
@@ -53,11 +32,3 @@ val string_of_pair : ('a -> string) -> ('b -> string) -> ('a * 'b) -> string
 
 val is_some : 'a option -> bool
 val is_none : 'a option -> bool
-
-module type SAFESOCKET = sig
-  type t = Lwt_unix.file_descr
-  val create : Lwt_unix.file_descr -> t
-  val recv : t -> string -> int -> int -> bool Lwt.t
-end
-
-module SafeSocket : SAFESOCKET
