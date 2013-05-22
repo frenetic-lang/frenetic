ff62a1ba85142818a4414d64a9ec8e8b3fc7d6b6
Arjun Guha <arjun.guha@gmail.com>
2013-05-22 08:24:55 -0400

    Need to expose for testing.

--- src/NetCoreLib/NetCore_Compiler.mli
+++ src/NetCoreLib/NetCore_Compiler.mli
@@ -3,3 +3,8 @@ val flow_table_of_policy : OpenFlow0x01.switchId -> NetCore_Types.Internal.pol -
 val parse_from_chan  : in_channel -> string -> NetCore_Types.External.policy NetCore_Stream.t
 
 val parse_from_lexbuf : Lexing.lexbuf -> string -> NetCore_Types.External.policy NetCore_Stream.t
+
+module OutputClassifier : NetCore_Classifier.CLASSIFIER
+  with type action = NetCore_Action.Output.t
+
+val compile_pol : NetCore_Types.Internal.pol -> OpenFlow0x01.switchId -> OutputClassifier.t
