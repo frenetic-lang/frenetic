407af9c508bd2f48969fe0f8e8a84ffddcfbce3c
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-05-30 11:27:57 -0400

    Added consistent updaate alg, no runtime integration yet

---
+++ src/NetCoreLib/NetCore_ConsistentUpdates.ml
@@ -0,0 +1,39 @@
+open NetCore_Types
+
+(* No topo info ATM, so overly heavy instrumentation Here *)
+
+let allPtrn = 
+  { ptrnDlSrc = WildcardAll;
+    ptrnDlDst = WildcardAll;
+    ptrnDlType = WildcardAll;
+    ptrnDlVlan = WildcardAll;
+    ptrnDlVlanPcp = WildcardAll;
+    ptrnNwSrc = WildcardAll;
+    ptrnNwDst = WildcardAll;
+    ptrnNwProto = WildcardAll;
+    ptrnNwTos = WildcardAll;
+    ptrnTpSrc = WildcardAll;
+    ptrnTpDst = WildcardAll;
+    ptrnInPort = WildcardAll
+  }
+
+let internal_policy p ver =
+  PoSeq(PoFilter (PrHdr {allPtrn with ptrnDlVlan = WildcardExact (Some ver)}), p)
+
+let edge_policy p ver =
+  PoSeq(PoFilter(PrHdr {allPtrn with ptrnDlVlan = WildcardExact None}),
+	PoSeq(p, PoAction[SwitchAction {id with outPort = Here; 
+	  outDlVlan = Some (None, Some ver);outDlVlanPcp = Some (0,1) }]))
+
+let prOr = List.fold_left (fun pr1 pr2 -> PrOr(pr1, pr2)) PrNone
+
+let ingressPort pt = PrHdr {allPtrn with ptrnInPort = pt}
+
+let strip_policy ver switches extPorts =
+  PoITE(prOr (List.map (fun sw -> (PrAnd(prOr (List.map ingressPort (extPorts sw)), PrOnSwitch sw))) switches), 
+	PoAction [SwitchAction {id with outPort = Here; outDlVlan = Some (Some ver, None)}], 
+	PoAction [SwitchAction {id with outPort = Here}])
+  
+let gen_update_pols orig ver switches extPorts =
+  (PoSeq (internal_policy orig ver, strip_policy ver switches extPorts),
+   PoSeq (edge_policy orig ver, strip_policy ver switches extPorts))
