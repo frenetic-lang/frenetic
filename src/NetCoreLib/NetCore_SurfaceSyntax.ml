bdb76e8634f6dcd30d3bb8b5f63aa34ce7d55859
Nate Foster <jnfoster@cs.cornell.edu>
2013-06-03 07:35:11 -0700

    Learning!

--- src/NetCoreLib/NetCore_SurfaceSyntax.ml
+++ src/NetCoreLib/NetCore_SurfaceSyntax.ml
@@ -24,6 +24,14 @@ and value =
 
 type env = value Env.t
 
+let init_env = 
+  Env.add
+    "learning"
+     (let (init, pol) = NetCore_MacLearning.make () in
+      let (lwt_e, stream) = NetCore_Stream.from_stream init pol in
+      PolStream (lwt_e, stream))
+  Env.empty
+
 exception CompileError of string
 
 let sprintf = Format.sprintf
@@ -85,6 +93,6 @@ let rec compile (env : env) = function
     failwith "NYI: slice surface syntax."
 
 let compile_program exp = 
-  match compile Env.empty exp with
+  match compile init_env exp with
     | PolStream (lwt_e, stream) -> (lwt_e, stream)
     | Pol pol -> (fst (Lwt.wait ()), NetCore_Stream.constant pol)
