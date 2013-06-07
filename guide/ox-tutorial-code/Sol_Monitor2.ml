89eb6ff84ce0b5422c34c30c6afff177d8b6a87c
Arjun Guha <arjun.guha@gmail.com>
2013-06-06 22:44:05 -0400

    Solution to Monitor Part 2

---
+++ guide/ox-tutorial-code/Sol_Monitor2.ml
@@ -0,0 +1,101 @@
+open OxPlatform
+open OpenFlow0x01_Core
+module Stats = OpenFlow0x01_Stats
+
+module MyApplication = struct
+
+  include OxStart.DefaultTutorialHandlers
+
+  let rec periodic_stats_request (sw : switchId) (interval : float) 
+      (xid : xid) (pat : pattern) : unit =
+    let callback () =
+      Printf.printf "Sending stats request to %Ld\n%!" sw; 
+      send_stats_request sw xid
+        (Stats.AggregateRequest (pat, 0xff, None));
+      periodic_stats_request sw interval xid pat in
+    timeout interval callback
+  
+  let match_http_response = 
+    { match_all with dlTyp = Some 0x800; nwProto = Some 6; tpSrc = Some 80 }
+
+  let match_http_request = 
+    { match_all with dlTyp = Some 0x800; nwProto = Some 6; tpDst = Some 80 }
+
+  let match_icmp =
+    { match_all with dlTyp = Some 0x800; nwProto = Some 1}
+
+
+  let switch_connected (sw : switchId) : unit =
+    Printf.printf "Switch %Ld connected.\n%!" sw;
+    periodic_stats_request sw 5.0 500l match_http_request;
+    periodic_stats_request sw 5.0 700l match_http_response;
+    send_flow_mod sw 0l (add_flow 200 match_icmp []);
+    send_flow_mod sw 0l (add_flow 199 match_http_request [Output AllPorts]);
+    send_flow_mod sw 0l (add_flow 198 match_http_response [Output AllPorts]);
+    send_flow_mod sw 0l (add_flow 197 match_all [Output AllPorts])
+
+  let is_http_request_packet (pk : Packet.packet) : bool = 
+    Packet.dlTyp pk = 0x800 &&
+    Packet.nwProto pk = 6 &&
+    Packet.tpDst pk = 80
+
+  let is_http_response_packet (pk : Packet.packet) : bool = 
+    Packet.dlTyp pk = 0x800 &&
+    Packet.nwProto pk = 6 &&
+    Packet.tpSrc pk = 80
+
+  let is_icmp_packet (pk : Packet.packet) =
+    Packet.dlTyp pk = 0x800 && Packet.nwProto pk = 1
+
+  let is_http_packet (pk : Packet.packet) =
+    is_http_request_packet pk || is_http_response_packet pk
+
+  let firewall_packet_in (sw : switchId) (xid : xid) (pktIn : packetIn) : unit =
+    let payload = pktIn.input_payload in
+    let pk = parse_payload payload in
+    if is_icmp_packet pk then
+      send_packet_out sw 0l
+        { output_payload = payload;
+          port_id = None;
+          apply_actions = []
+        }
+    else 
+      send_packet_out sw 0l
+        { output_payload = payload;
+          port_id = None;
+          apply_actions = [Output AllPorts]
+        }
+
+
+  let num_http_packets = ref 0
+   
+  let packet_in (sw : switchId) (xid : xid) (pktIn : packetIn) : unit =
+    Printf.printf "%s\n%!" (packetIn_to_string pktIn);
+    firewall_packet_in sw xid pktIn;
+    if is_http_packet (parse_payload pktIn.input_payload) then
+      begin
+        num_http_packets := !num_http_packets + 1;
+        Printf.printf "Seen %d HTTP packets.\n%!" !num_http_packets
+      end
+
+  let num_http_request_packets = ref 0L
+  let num_http_response_packets = ref 0L
+  let stats_reply (sw : switchId) (xid : xid) (stats : Stats.reply) : unit =
+    match stats with
+      | Stats.AggregateFlowRep rep ->
+        let k = rep.Stats.total_packet_count in
+        (match xid with
+          | 500l -> 
+            num_http_request_packets := Int64.add !num_http_request_packets k
+          | 700l ->
+            num_http_response_packets := Int64.add !num_http_response_packets k
+          | _ -> Printf.printf "Unexpected xid (%ld) in StatsReply\n%!" xid);
+        Printf.printf "Seen %Ld HTTP packets.\n%!"
+          (Int64.add !num_http_request_packets !num_http_response_packets)
+      | _ -> 
+        Printf.printf "Unexpected stats message.\n%!"
+
+
+end
+
+module Controller = OxStart.Make (MyApplication)
