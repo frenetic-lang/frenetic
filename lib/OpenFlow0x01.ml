26b0f1f5c2f28d111f5a5aa1ad738e66cacf5b71
Nate Foster <jnfoster@cs.cornell.edu>
2013-08-11 12:11:22 -0400

    Revert VLAN bogosity (OVS just prints things differently, using the vlan_tci field)

--- lib/OpenFlow0x01.ml
+++ lib/OpenFlow0x01.ml
@@ -145,7 +145,7 @@ module Match = struct
       Wildcards.dl_vlan = 
 	(match m.dlVlan with 
 	  | None -> true
-	  | Some None -> true
+	  | Some None -> false
 	  | Some (Some _) -> false);
       Wildcards.dl_src = is_none m.dlSrc;
       Wildcards.dl_dst = is_none m.dlDst;
@@ -177,38 +177,6 @@ module Match = struct
     | Some n -> n
     | None -> 0L
 
-  (* Helper for to_string *)
-  let fld_str (lbl : string) (pr : 'a -> string) (v : 'a option)
-      : string option =
-    match v with
-      | None -> None
-      | Some a -> Some (sprintf "%s = %s" lbl (pr a))
-
-  let to_string (x : t) : string =
-    let all_fields =
-      [ fld_str "dlSrc" string_of_mac x.dlSrc;
-        fld_str "dlDst" string_of_mac x.dlDst;
-        fld_str "dlTyp" Packet.string_of_dlTyp x.dlTyp;
-        (match x.dlVlan with
-          | None -> None
-          | Some None -> Some "dlVlan = none"
-          | Some (Some vlan) -> fld_str "dlVlan" string_of_int (Some vlan));
-        fld_str "dlVlanPcp" Packet.string_of_dlVlanPcp x.dlVlanPcp;
-        fld_str "nwSrc" Packet.string_of_nwAddr x.nwSrc;
-        fld_str "nwDst" Packet.string_of_nwAddr x.nwDst;
-        fld_str "nwProto" Packet.string_of_nwProto x.nwProto;
-        fld_str "nwTos" Packet.string_of_nwTos x.nwTos;
-        fld_str "tpSrc" Packet.string_of_tpPort x.tpSrc;
-        fld_str "tpDst" Packet.string_of_tpPort x.tpDst;
-        fld_str "inPort" string_of_portId x.inPort ] in
-    let set_fields =
-      List.fold_right
-        (fun fo acc -> match fo with None -> acc | Some f -> f :: acc)
-        all_fields [] in
-    match set_fields with
-      | [] -> "{*}"
-      | _ ->  "{" ^ (String.concat ", " set_fields) ^ "}"
-
   let marshal m bits =
     set_ofp_match_wildcards bits (Wildcards.marshal (wildcards_of_match m));
     set_ofp_match_in_port bits (if_some16 m.inPort);
@@ -216,8 +184,8 @@ module Match = struct
     set_ofp_match_dl_dst (bytes_of_mac (if_word48 m.dlDst)) 0 bits;
     let vlan = match m.dlVlan with
       | Some (Some v) -> v
-      | Some None -> 0
-      | None -> 0 in
+      | Some None -> vlan_none
+      | None -> vlan_none in 
     set_ofp_match_dl_vlan bits (vlan);
     set_ofp_match_dl_vlan_pcp bits (if_some8 m.dlVlanPcp);
     set_ofp_match_dl_type bits (if_some16 m.dlTyp);
@@ -303,6 +271,37 @@ module Match = struct
           Some (get_ofp_match_in_port bits);
     }
 
+  (* Helper for to_string *)
+  let fld_str (lbl : string) (pr : 'a -> string) (v : 'a option)
+      : string option =
+    match v with
+      | None -> None
+      | Some a -> Some (sprintf "%s = %s" lbl (pr a))
+
+  let to_string (x : t) : string =
+    let all_fields =
+      [ fld_str "dlSrc" string_of_mac x.dlSrc;
+        fld_str "dlDst" string_of_mac x.dlDst;
+        fld_str "dlTyp" Packet.string_of_dlTyp x.dlTyp;
+        (match x.dlVlan with
+          | None -> None
+          | Some None -> Some "dlVlan = none"
+          | Some (Some vlan) -> fld_str "dlVlan" string_of_int (Some vlan));
+        fld_str "dlVlanPcp" Packet.string_of_dlVlanPcp x.dlVlanPcp;
+        fld_str "nwSrc" Packet.string_of_nwAddr x.nwSrc;
+        fld_str "nwDst" Packet.string_of_nwAddr x.nwDst;
+        fld_str "nwProto" Packet.string_of_nwProto x.nwProto;
+        fld_str "nwTos" Packet.string_of_nwTos x.nwTos;
+        fld_str "tpSrc" Packet.string_of_tpPort x.tpSrc;
+        fld_str "tpDst" Packet.string_of_tpPort x.tpDst;
+        fld_str "inPort" string_of_portId x.inPort ] in
+    let set_fields =
+      List.fold_right
+        (fun fo acc -> match fo with None -> acc | Some f -> f :: acc)
+        all_fields [] in
+    match set_fields with
+      | [] -> "{*}"
+      | _ ->  "{" ^ (String.concat ", " set_fields) ^ "}"
 end
 
 module PseudoPort = struct
