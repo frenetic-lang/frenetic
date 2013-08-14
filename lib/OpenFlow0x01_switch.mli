4eed3e04d134d702f5194465ddad301b4e6ab876
Nate Foster <jnfoster@cs.cornell.edu>
2013-08-14 00:19:31 -0400

    Expose more guts

--- lib/OpenFlow0x01_switch.mli
+++ lib/OpenFlow0x01_switch.mli
@@ -16,12 +16,26 @@ val features : t -> OpenFlow0x01.SwitchFeatures.t
 	  requests and echo replies. *)
 val handshake : Lwt_unix.file_descr -> t option Lwt.t
 
-(** [send_to_switch switch_id xid msg] sends [msg] to the switch,
+(** [send switch_id xid msg] sends [msg] to [switch_id],
     blocking until the send completes. *)
 val send : t -> OpenFlow0x01_Core.xid 
   -> OpenFlow0x01.Message.t -> unit Lwt.t
 
-(** [recv_from_switch switch_id] blocks until [switch_id] sends a
+(** [send_to_switch_fd fd xid msg] sends [msg] to the switch at [fd],
+    blocking until the send completes. *)
+val send_to_switch_fd : 
+  Lwt_unix.file_descr -> 
+  OpenFlow0x01_Core.xid ->
+  OpenFlow0x01.Message.t -> 
+  unit option Lwt.t
+
+(** [recv_from_switch_fd fd] blocks until the switch at [fd] sends a
+    message. *)
+val recv_from_switch_fd : 
+  Lwt_unix.file_descr -> 
+  (OpenFlow0x01_Core.xid * OpenFlow0x01.Message.t) option Lwt.t
+
+(** [recv switch_id] blocks until [switch_id] sends a
     message. *)
 val recv : t
   -> (OpenFlow0x01_Core.xid * OpenFlow0x01.Message.t) Lwt.t
