b13ab6b4b760911a9dda2308aa568a03116b0c6d
Arjun Guha <arjun.guha@gmail.com>
2013-08-18 23:14:15 -0400

    Removed PlatformSig

--- lib/OpenFlow0x01_Platform.mli
+++ lib/OpenFlow0x01_Platform.mli
@@ -1,9 +1,9 @@
-(** An implementation of [PLATFORM]. The documentation for [PLATFORM] describes its features. *)
-
+(** An OpenFlow platform is responsible for accepting and managing
+    connections to OpenFlow-enabled switches.  It relies on the
+    [Message] module for parsing and marshaling messages.
+*)
 open Lwt_unix
 open OpenFlow0x01
-
-include OpenFlow0x01_PlatformSig.PLATFORM
 
 (** [init_with_port p] accepts connections from switches on port [p], which
     is usually [6633]. *)
@@ -14,3 +14,23 @@ val init_with_fd : file_descr -> unit Lwt.t
 
 (** [shutdown] shuts down the server gracefully *) 
 val shutdown : unit -> unit 
+
+(** [send_to_switch switch_id xid msg] sends [msg] to the switch,
+    blocking until the send completes. *)
+val send_to_switch : switchId -> xid -> Message.t-> unit Lwt.t
+
+(** [recv_from_switch switch_id] blocks until [switch_id] sends a
+    message.
+
+    If the switch sends an [ECHO_REQUEST], [recv_from_switch] will
+    itself respond with an [ECHO_REPLY] and block for the next
+    message. *)
+val recv_from_switch : switchId -> (xid * Message.t) Lwt.t
+
+(** [accept_switch] blocks until a switch connects, handles the
+    OpenFlow handshake, and returns after the switch sends a
+    [FEATURES_REPLY] message. *)
+val accept_switch : unit -> SwitchFeatures.t Lwt.t
+
+(** [wait_disconnect switch_id] sleeps until the switch has disconnected. *)
+val wait_disconnect : switchId -> unit Lwt.t
--- lib/OpenFlow0x01_PlatformSig.ml
+++
@@ -1,8 +0,0 @@
-open OpenFlow0x01
-
-module type PLATFORM = sig
-  val send_to_switch : switchId -> xid -> Message.t -> unit Lwt.t
-  val recv_from_switch : switchId -> (xid * Message.t) Lwt.t
-  val accept_switch : unit -> SwitchFeatures.t Lwt.t
-  val wait_disconnect : switchId -> unit Lwt.t
-end
