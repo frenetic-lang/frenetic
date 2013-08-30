d98fb545b4c24e108d8eaee427efd4dec77600a6
Arjun Guha <arjun.guha@gmail.com>
2013-08-29 21:51:22 -0400

    Is this a controller?
    Would it run if it could?

--- lib/Controller.ml
+++ lib/Controller.ml
@@ -1,5 +1,7 @@
+module Platform = SDN
 module SDN = SDN_types
 module NetKAT = NetKAT_Types
+module Stream = NetCore_Stream
 
 let pred_to_pattern (sw : SDN.fieldVal) (pred : ONF.pred) : SDN.pattern option =
 	let f (h : NetKAT.hdr) (v : NetKAT.hdrVal) (pat : SDN.pattern) =
@@ -50,3 +52,19 @@ let rec local_to_tbl (sw : SDN.fieldVal) (local : ONF.local) : SDN.flowTable =
      | None -> local_to_tbl sw else_
      | Some pat ->
         (simpl_flow pat (sum_to_action then_)) :: (local_to_tbl sw else_))
+
+
+(* Keeps the switch configured with the latest policy on onf_stream. *)
+let switch_thread 
+  (onf_stream : ONF.local Stream.t)
+  (feats : SDN.switchFeatures) : unit Lwt.t =
+  let sw_id = feats.SDN.switch_id in
+  let config_switch onf = 
+    Platform.setup_flow_table sw_id (local_to_tbl sw_id onf) in
+  lwt () = config_switch (Stream.now onf_stream) in
+  Lwt_stream.iter_s config_switch (Stream.to_stream onf_stream)
+
+let rec start ~port ~pol =
+  let onf_stream = Stream.map ONF.compile pol in
+  lwt (stop_accept, new_switches) = Platform.accept_switches port  in
+  Lwt_stream.iter_p (switch_thread onf_stream) new_switches
--- lib/Controller.mli
+++ lib/Controller.mli
@@ -1,1 +1,4 @@
 (** Implements a controller for ONF. *)
+
+val start : port:int -> pol:(NetKAT_Types.pol NetCore_Stream.t)
+  -> unit Lwt.t
