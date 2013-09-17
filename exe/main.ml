e07664abbc33a90984941e3a260236602532eb88
Arjun Guha <arjun@cs.umass.edu>
2013-09-17 15:30:47 -0400

    oasis setup

--- exe/main.ml
+++ exe/main.ml
@@ -1,12 +1,11 @@
 open DOT_Types
-open DOT_Parser
 open Topology
 
 let _ =
   let fname = Sys.argv.(1) in
   Printf.printf "Attempting to topology from file: %s\n%!" fname;
-  let ast = Converter.parse_dotfile fname in
-  let topo = Converter.topo_from_ast ast in
+  let ast = DOT_Parser.parse_dotfile fname in
+  let topo = DOT_Parser.topo_from_ast ast in
   Printf.printf "Dot Topology: \n%s\n" (Topology.to_dot topo);
   Printf.printf "Mininet Script: \n%s\n" (Topology.to_mininet topo);
   topo
