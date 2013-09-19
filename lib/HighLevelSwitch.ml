a58d70549415c05ce18a008d31de12da64f73fed
Marco Canini <marco@net.t-labs.tu-berlin.de>
2013-09-19 04:08:10 -0300

    Starting hacking 0x04

--- lib/HighLevelSwitch.ml
+++ lib/HighLevelSwitch.ml
@@ -1,6 +1,7 @@
 type t =
   | OF0x01 of HighLevelSwitch0x01.t
-
+  | OF0x04 of HighLevelSwitch0x04.t
+  
 let initialize (fd : Lwt_unix.file_descr) : t option Lwt.t =
 	match_lwt OpenFlow0x01_Switch.handshake fd with
 	| None -> Lwt.return None
@@ -8,18 +9,24 @@ let initialize (fd : Lwt_unix.file_descr) : t option Lwt.t =
 
 let setup_flow_table (t : t) = match t with
 	| OF0x01 u -> HighLevelSwitch0x01.setup_flow_table u
+	| OF0x04 u -> HighLevelSwitch0x04.setup_flow_table u
 
 let flow_stats_request (t : t) = match t with
   | OF0x01 u -> HighLevelSwitch0x01.flow_stats_request u
+  | OF0x04 u -> HighLevelSwitch0x04.flow_stats_request u
 
 let packet_in (t : t) = match t with
   | OF0x01 u -> HighLevelSwitch0x01.packet_in u
+  | OF0x04 u -> HighLevelSwitch0x04.packet_in u
 
 let packet_out (t : t) = match t with
   | OF0x01 u -> HighLevelSwitch0x01.packet_out u
+  | OF0x04 u -> HighLevelSwitch0x04.packet_out u
 
 let disconnect (t : t) = match t with
   | OF0x01 u -> HighLevelSwitch0x01.disconnect u
+  | OF0x04 u -> HighLevelSwitch0x04.disconnect u
 
 let features (t : t) = match t with
   | OF0x01 u -> HighLevelSwitch0x01.features u
+  | OF0x04 u -> HighLevelSwitch0x04.features u
