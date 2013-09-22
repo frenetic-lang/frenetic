e2dc54b831bf04b179448d2c19ea2e8f97866243
Jung Hyun Eun <junghyun.eun@gmail.com>
2013-09-22 11:30:34 -0400

    now the graph is directed

--- gen_fattree.py
+++ gen_fattree.py
@@ -17,7 +17,7 @@ def main(argv):
 		switches = ['s'+ str(i) for i in range(1, ((1 - (num_fan_out ** num_layers)) / (1 - num_fan_out))+1)]
 		hosts = ['h' + str(i) for i in range(1, num_fan_out ** num_layers + 1)]
 		nodes = switches + hosts
-		g = nx.MultiGraph()
+		g = nx.MultiDiGraph()
 		g.add_nodes_from(nodes)
 		for i in range(num_layers,0,-1):
 			'''length of temp1 = (length of temp2) * num_fan_out'''
@@ -28,10 +28,13 @@ def main(argv):
 				for k in range(num_fan_out):
 					p = num_fan_out ** (num_layers - i)
 					if p == 1:
-						g.add_edge(temp2[j],temp1[num_fan_out * j + k],attr_dict={'sport':1,'dport':k+1,'capacity':'1Gbps','cost':'1'})
+						g.add_edge(temp1[num_fan_out * j + k],temp2[j],attr_dict={'sport':1,'dport':k+1,'capacity':'1Gbps','cost':'1'})
+						g.add_edge(temp2[j],temp1[num_fan_out * j + k],attr_dict={'sport':k+1,'dport':1,'capacity':'1Gbps','cost':'1'})
+
 					else:
 						for l in range(p):
-							g.add_edge(temp2[j],temp1[num_fan_out * j + k],attr_dict={'sport':p+l+1,'dport': num_fan_out * k + l+1,'capacity':'1Gbps','cost':'1'})
+							g.add_edge(temp1[num_fan_out * j + k],temp2[j],attr_dict={'sport':p+l+1,'dport': num_fan_out * k + l+1,'capacity':'1Gbps','cost':'1'})
+							g.add_edge(temp2[j],temp1[num_fan_out * j + k],attr_dict={'sport':num_fan_out * k + l+1,'dport': p+l+1,'capacity':'1Gbps','cost':'1'})
 
 		nx.write_dot(g,'./' + out_file + '.dot')
 
