3d89b4f8c3260fede7b69b546a22653fb86190db
nimishgupta <nimish28@gmail.com>
2013-10-06 14:03:37 -0400

    more checks added, printing of field names in headers modified to match that of parser

--- lib/NetKAT_Types.ml
+++ lib/NetKAT_Types.ml
@@ -87,8 +87,33 @@ module Formatting = struct
 
   open Format
 
+  (* 
+    TODO : Can be changed in SDN_Types.format_feild, will save duplication of 
+           effort and can be carried into NetCore pretty parser *)
+  let format_field (fmt : formatter) (f : SDN_Types.field) : unit =
+      pp_print_string fmt
+      (match f with
+        | SDN_Types.InPort ->     "inPort"
+        | SDN_Types.EthType ->    "dlTyp"
+        | SDN_Types.EthSrc ->     "dlSrc"
+        | SDN_Types.EthDst ->     "dlDst"
+        | SDN_Types.Vlan ->       "vlan"
+        | SDN_Types.VlanPcp ->    "vlanPcp"
+        | SDN_Types.IPProto ->    "nwProto"
+        | SDN_Types.IP4Src ->     "srcIP"
+        | SDN_Types.IP4Dst ->     "dstIP"
+        | SDN_Types.TCPSrcPort -> "tcpSrcPort"
+        | SDN_Types.TCPDstPort -> "tcpDstPort")
+
+  let format_field_value (fmt : formatter) (v : VInt.t) : unit =
+    (*if (Int64.of_int (NetKAT_Parser.vlan_none) = VInt.get_int64 v) then pp_print_string fmt "<none>" *)
+    if (Int64.of_int (-1) = VInt.get_int64 v) then pp_print_string fmt "<none>"
+    else VInt.format fmt v
+
+
   let header (fmt : formatter) (h : header) : unit = match h with
-    | Header h' -> SDN_Types.format_field fmt h'
+    (* | Header h' -> SDN_Types.format_field fmt h' *)
+    | Header h' -> format_field fmt h'
     | Switch -> pp_print_string fmt "switch"
 
 
@@ -97,11 +122,11 @@ module Formatting = struct
   let rec pred (cxt : predicate_context) (fmt : formatter) (pr : pred) : unit = 
     match pr with
     | True -> 
-      fprintf fmt "@[true@]"
+      fprintf fmt "@[pass@]"
     | False -> 
-      fprintf fmt "@[false@]"
+      fprintf fmt "@[drop@]"
     | (Test (h, v)) -> 
-      fprintf fmt "@[%a = %a@]" header h VInt.format v
+      fprintf fmt "@[%a = %a@]" header h format_field_value v
 
     | Neg p' -> 
       begin match cxt with
@@ -136,9 +161,14 @@ module Formatting = struct
   let rec pol (cxt : policy_context) (fmt : formatter) (p : policy) : unit =
     match p with
     | Filter pr -> 
-      pred PAREN fmt pr
+      (match pr with
+         | True 
+         | False -> pred PAREN fmt pr
+         | _ -> pp_print_string fmt "filter "; pred PAREN fmt pr)
+
     | Mod (h, v) -> 
-      fprintf fmt "@[%a <- %a@]" header h VInt.format v
+      fprintf fmt "@[%a -> %a@]" header h format_field_value v
+
     | Star p' -> 
       begin match cxt with
         | PAREN 
