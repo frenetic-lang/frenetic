43942a7c89d5ee4b91b8c78e94e6cbc29b6646c6
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-10-17 14:15:22 -0400

    Added Choice to dehopification

--- lib/Dehop.ml
+++ lib/Dehop.ml
@@ -7,6 +7,7 @@ type explicit_topo_pol =
     (* switch, port -> switch, port *)
   | Link of header_val*header_val*header_val*header_val
   | Par of explicit_topo_pol * explicit_topo_pol
+  | Choice of explicit_topo_pol * explicit_topo_pol
   | Seq of explicit_topo_pol * explicit_topo_pol
   | Star of explicit_topo_pol
 
@@ -30,6 +31,7 @@ type virtual_pol =
     (* switch, port -> switch, port *)
   | VLink of header_val*header_val*header_val*header_val
   | VPar of virtual_pol * virtual_pol
+  | VChoice of virtual_pol * virtual_pol
   | VSeq of virtual_pol * virtual_pol
   | VStar of virtual_pol
       
@@ -50,7 +52,7 @@ module Formatting = struct
     (* The type of the immediately surrounding context, which guides parenthesis-
        intersion. *)
     (* JNF: YES. This is the Right Way to pretty print. *)
-  type context = SEQ | PAR | STAR | NEG | PAREN
+  type context = SEQ | PAR | CHOICE | STAR | NEG | PAREN
 
   let rec pred (cxt : context) (fmt : formatter) (pr : pred) : unit = 
     match pr with
@@ -68,13 +70,14 @@ module Formatting = struct
       | Or (p1, p2) -> 
         begin match cxt with
           | PAREN
-          | PAR -> fprintf fmt "@[%a + %a@]" (pred PAR) p1 (pred PAR) p2
-          | _ -> fprintf fmt "@[(@[%a + %a@])@]" (pred PAR) p1 (pred PAR) p2
+          | PAR -> fprintf fmt "@[%a | %a@]" (pred PAR) p1 (pred PAR) p2
+          | _ -> fprintf fmt "@[(@[%a | %a@])@]" (pred PAR) p1 (pred PAR) p2
         end
       | And (p1, p2) -> 
         begin match cxt with
           | PAREN
           | SEQ
+          | CHOICE
           | PAR -> fprintf fmt "@[%a ; %a@]" (pred SEQ) p1 (pred SEQ) p2
           | _ -> fprintf fmt "@[(@[%a ; %a@])@]" (pred SEQ) p1 (pred SEQ) p2
         end
@@ -98,13 +101,20 @@ module Formatting = struct
       | VPar (p1, p2) -> 
         begin match cxt with
           | PAREN
-          | PAR -> fprintf fmt "@[%a + %a@]" (vpol PAR) p1 (vpol PAR) p2
-          | _ -> fprintf fmt "@[(@[%a + %a@])@]" (vpol PAR) p1 (vpol PAR) p2
+          | PAR -> fprintf fmt "@[%a | %a@]" (vpol PAR) p1 (vpol PAR) p2
+          | _ -> fprintf fmt "@[(@[%a | %a@])@]" (vpol PAR) p1 (vpol PAR) p2
+        end
+      | VChoice (p1, p2) -> 
+        begin match cxt with
+          | PAREN
+          | CHOICE -> fprintf fmt "@[%a + %a@]" (vpol CHOICE) p1 (vpol CHOICE) p2
+          | _ -> fprintf fmt "@[(@[%a + %a@])@]" (vpol CHOICE) p1 (vpol CHOICE) p2
         end
       | VSeq (p1, p2) -> 
         begin match cxt with
           | PAREN
           | SEQ
+          | CHOICE
           | PAR -> fprintf fmt "@[%a ; %a@]" (vpol SEQ) p1 (vpol SEQ) p2
           | _ -> fprintf fmt "@[(@[%a ; %a@])@]" (vpol SEQ) p1 (vpol SEQ) p2
         end
@@ -127,13 +137,20 @@ module Formatting = struct
       | Par (p1, p2) -> 
         begin match cxt with
           | PAREN
-          | PAR -> fprintf fmt "@[%a + %a@]" (epol PAR) p1 (epol PAR) p2
-          | _ -> fprintf fmt "@[(@[%a + %a@])@]" (epol PAR) p1 (epol PAR) p2
+          | PAR -> fprintf fmt "@[%a | %a@]" (epol PAR) p1 (epol PAR) p2
+          | _ -> fprintf fmt "@[(@[%a | %a@])@]" (epol PAR) p1 (epol PAR) p2
+        end
+      | Choice (p1, p2) -> 
+        begin match cxt with
+          | PAREN
+          | CHOICE -> fprintf fmt "@[%a + %a@]" (epol CHOICE) p1 (epol CHOICE) p2
+          | _ -> fprintf fmt "@[(@[%a + %a@])@]" (epol CHOICE) p1 (epol CHOICE) p2
         end
       | Seq (p1, p2) -> 
         begin match cxt with
           | PAREN
           | SEQ
+          | CHOICE
           | PAR -> fprintf fmt "@[%a ; %a@]" (epol SEQ) p1 (epol SEQ) p2
           | _ -> fprintf fmt "@[(@[%a ; %a@])@]" (epol SEQ) p1 (epol SEQ) p2
         end
@@ -238,106 +255,75 @@ let rec dehopify (p : explicit_topo_pol) : restricted_pol =
       VPar(t_p,t_q),
       VPar(VSeq(VTest(h,h0), e_p),
            VSeq(VTest(h,h1), e_q))
+    | Choice (p,q) -> 
+      let i_p,s_p,t_p,e_p = dehopify p in
+      let i_q,s_q,t_q,e_q = dehopify q in
+      let h = gen_header 2 in
+      let h0 = VInt.Int16 0 in
+      let h1 = VInt.Int16 1 in
+      VChoice(VSeq(VMod(Tag h,h0), i_p),
+           VSeq(VMod(Tag h,h1), i_q)),
+      VPar(VSeq(VTest(h,h0),s_p),
+           VSeq(VTest(h,h1),s_q)),
+      VPar(t_p,t_q),
+      VPar(VSeq(VTest(h,h0), e_p),
+           VSeq(VTest(h,h1), e_q))
     | Seq (p,q) -> 
       let i_p,s_p,t_p,e_p = dehopify p in
       let i_q,s_q,t_q,e_q = dehopify q in
-      let h = gen_header 8 in
-      let h0 = VInt.Int16 0 in
-      let h1 = VInt.Int16 1 in
-      let h1' = VInt.Int16 2 in
-      let h2 = VInt.Int16 3 in
-      let h2' = VInt.Int16 4 in
-      let h3 = VInt.Int16 5 in
-      let h3' = VInt.Int16 6 in
-      let h3'' = VInt.Int16 7 in
-      let h4 = VInt.Int16 8 in
-      VSeq(parList [VMod(Tag h,h0);
-                    VMod(Tag h,h1);
-                    VMod(Tag h,h2);
-                    VMod(Tag h,h3)],
+      let h = gen_header 4 in
+      let h1 = VInt.Int16 0 in
+      let h2 = VInt.Int16 1 in
+      let h2' = VInt.Int16 2 in
+      let h4 = VInt.Int16 3 in
+      VSeq(parList [VMod(Tag h,h1);
+                    VMod(Tag h,h2)],
            i_p),
-      parList [seqList [VTest(h,h0);
-                        s_p;
-                        e_p;
-                        i_q;
-                        s_q;
-                        VMod(Tag h,h4)];
-               seqList [VTest(h,h1);
-                        s_p;
-                        VPar(VFilter True,
-                             VMod(Tag h,h1'))];
-               seqList [VTest(h,h1');
-                        s_p;
-                        e_p;
-                        i_q;
-                        s_q;
-                        VMod(Tag h,h4)];
+      parList [ seqList [VTest(h,h1);
+                         s_p;
+                         VPar(VFilter True,
+                              VMod(Tag h,h2))];
                seqList [VTest(h,h2);
                         s_p;
-                        e_p;
-                        i_q;
-                        s_q;
-                        VPar(VMod(Tag h,h2'),
-                             VMod(Tag h, h4))];
-               VSeq (VTest(h,h2'), s_q);
-               seqList [VTest(h,h3);
-                        s_p;
-                        VMod(Tag h,h3')];
-               seqList [VTest(h,h3');
-                        s_p;
                         VPar(VFilter True,
-                             seqList [e_p;
-                                      i_q;
-                                      s_q;
-                                      VPar(VMod(Tag h, h3''),
-                                           VMod(Tag h, h4))])];
-               seqList [VTest(h,h3'');
+                             seqList [
+                               e_p;
+                               i_q;
+                               s_q;
+                               VPar(VMod(Tag h,h2'),
+                                    VMod(Tag h, h4))])];
+               seqList [VTest(h,h2');
                         s_q;
-                        VPar(VFilter True,
+                        VPar(VFilter True, 
                              VMod(Tag h, h4))]],
       VPar(t_p,t_q),
       VSeq(VTest(h, h4), e_q)
     | Star p -> 
       let i_p,s_p,t_p,e_p = dehopify p in
-      let h = gen_header 7 in
+      let h = gen_header 5 in
       let h0 = VInt.Int16 0 in
       let h1 = VInt.Int16 1 in
-      let h2 = VInt.Int16 2 in
-      let h2' = VInt.Int16 3 in
-      let h2'' = VInt.Int16 4 in
-      let h3 = VInt.Int16 5 in
-      let h4 = VInt.Int16 6 in
+      let h1' = VInt.Int16 2 in
+      let h2 = VInt.Int16 3 in
+      let h3 = VInt.Int16 4 in
       parList [VMod(Tag h,h0);
-               VMod(Tag h,h1);
-               VMod(Tag h,h2)],
+               VMod(Tag h,h1)],
       parList [ VSeq(VTest(h,h0),
-                     VMod(Tag h,h3));
+                     VMod(Tag h,h2));
                 seqList [VTest(h,h1);
                          i_p;
                          s_p;
                          VStar(seqList [e_p;i_p;s_p]);
-                         VMod(Tag h,h4)];
-                seqList [VTest(h,h2);
-                         i_p;
+                         VPar(VMod(Tag h, h1'),
+                              VMod(Tag h,h3))];
+                seqList [VTest(h,h1');
                          s_p;
-                         VStar(seqList [e_p;i_p;s_p]);
-                         VPar(VMod(Tag h,h2'),
-                              VMod(Tag h,h2''))];
-                seqList [VTest(h,h2');
-                         s_p;
-                         VPar(VMod(Tag h,h2'),
-                              VMod(Tag h,h2''))];
-                seqList [VTest(h,h2'');
-                         s_p;
-                         VStar(seqList [e_p;i_p;s_p]);                                     
-                         VPar(VMod(Tag h,h2'),
-                              VMod(Tag h,h2''))]],
+                         VPar(VFilter True,
+                              VMod(Tag h,h3))]],
       t_p,
-      VPar(VSeq(VPar(VTest(h,h2'),
-                     VPar(VTest(h,h2''),
-                          VTest(h,h4))),
+      VPar(VSeq(VTest(h,h3),
                 e_p),
-           VTest(h,h3))
+           VTest(h,h2))
 
   (* Optimizations Observation: Virtual headers are semantically
      meaningless if they aren't matched in the NetKAT code itself. Thus,
@@ -382,6 +368,19 @@ let rec simplify_vpol p =
             | p', VFilter False -> p'
             | p',q' -> VPar(p',q')
         end
+    | VChoice(p, VChoice(q,r)) ->
+      if p = q then simplify_vpol (VChoice(q,r))
+      else let p' = simplify_vpol p in
+           if p' = p then
+             VChoice(p, simplify_vpol (VChoice(q,r)))
+           else
+             simplify_vpol (VChoice(p', VChoice(q,r)))
+    | VChoice(p,q) ->
+      if p = q then p
+      else
+        let p' = simplify_vpol p in
+        let q' = simplify_vpol q in
+        if p' = q' then p' else VChoice(p',q')
     | VStar(VFilter False) -> VFilter True
     | VStar(VFilter True) -> VFilter True
     | VStar(p) -> 
@@ -414,25 +413,63 @@ let rec vpol_to_linear_vpol p =
       if p' = p && q' = q then
         VSeq(p,q)
       else vpol_to_linear_vpol (VSeq(p',q'))
+    | VChoice(p,q) ->
+      let p' = vpol_to_linear_vpol p in
+      let q' = vpol_to_linear_vpol q in
+      if p' = p && q' = q then
+        VChoice(p,q)
+      else vpol_to_linear_vpol (VChoice(p',q'))
     | VStar p -> VStar (vpol_to_linear_vpol p)
     | _ -> p
 
-  (* distributes seq over par *)
+(* Normal form: choice of unions of sequences *)
 let rec distribute_seq p =
   match p with
-      (* (p|q);r = p;r | q;r *)
+    | VSeq(VChoice(p,q), r) ->
+      VChoice(distribute_seq (VSeq(p,r)), distribute_seq (VSeq(q,r)))
+    | VSeq(p, VChoice(q,r)) ->
+      VChoice(distribute_seq (VSeq(p,q)), distribute_seq (VSeq(p,r)))
+    (* (p|q);r = p;r | q;r *)
     | VSeq(VPar(p,q), r) -> 
-      VPar(distribute_seq (VSeq(p,r)), distribute_seq(VSeq(q,r)))
+      let p_r = distribute_seq (VSeq(p,r)) in
+      let q_r = distribute_seq (VSeq(q,r)) in
+      begin
+        match p_r,q_r with
+          | VChoice _,_ -> distribute_seq (VPar(p_r,q_r))
+          | _, VChoice _ -> distribute_seq (VPar(p_r,q_r))
+          | _,_ -> VPar(p_r, q_r)
+      end
       (* p;(q|r) = p;q | p;r *)
     | VSeq(p, VPar(q,r)) -> 
-      VPar(distribute_seq (VSeq(p,q)), distribute_seq(VSeq(p,r)))
+      let p_q = distribute_seq (VSeq(p,q)) in
+      let p_r = distribute_seq (VSeq(p,r)) in
+      begin
+        match p_q,p_r with
+          | VChoice _,_ -> distribute_seq (VPar(p_q,p_r))
+          | _, VChoice _ -> distribute_seq (VPar(p_q,p_r))
+          | _,_ -> VPar(p_q, p_r)
+      end
     | VSeq(p,q) -> let p' = distribute_seq p in
                    let q' = distribute_seq q in
                    if p = p' & q = q' then
                      VSeq(p',q')
                    else
                      distribute_seq(VSeq(p',q'))
-    | VPar(p,q) -> VPar(distribute_seq p,distribute_seq q)
+    | VPar(VChoice(p,q), r) ->
+      let p_r = distribute_seq (VPar(p,r)) in
+      let q_r = distribute_seq (VPar(q,r)) in
+      VChoice(p_r, q_r)
+    | VPar(p, VChoice(q,r)) ->
+      let p_q = distribute_seq (VPar(p,q)) in
+      let p_r = distribute_seq (VPar(p,r)) in
+      VChoice(p_q, p_r)
+    | VPar(p,q) -> let p' = distribute_seq p in
+                   let q' = distribute_seq q in 
+                   if p' = p && q' = q then
+                     VPar(p',q')
+                   else
+                     distribute_seq (VPar(p',q'))
+    | VChoice(p,q) -> VChoice(distribute_seq p, distribute_seq q)
     | VStar(p) -> VStar(distribute_seq p)
     | _ -> p
 
@@ -487,6 +524,12 @@ let rec normalize_seq p =
                      normalize_seq (VPar(q',p'))
                    else
                      VPar(p',q')
+    | VChoice(p,q) -> let p' = normalize_seq p in
+                      let q' = normalize_seq q in
+                      if q' < p' then
+                        normalize_seq (VChoice(q',p'))
+                      else
+                        VChoice(p',q')
     | VStar(p) -> VStar(normalize_seq p)
     | _ -> p
 
@@ -533,6 +576,7 @@ let rec remove_matches p =
                    else
                      remove_matches (VSeq(p'',q''))
     | VPar(p,q) -> VPar(remove_matches p, remove_matches q)
+    | VChoice(p,q) -> VChoice(remove_matches p, remove_matches q)
     | VStar(p) -> VStar(remove_matches p)
     | p -> p
 
@@ -564,6 +608,10 @@ let rec elim_vtest' p live_headers =
       let p', _  = elim_vtest_helper p live_headers in        
       let q', _ = elim_vtest_helper q live_headers in
       VPar(elim_vtest' p' live_headers, elim_vtest' q' live_headers)
+    | VChoice(p,q) ->
+      let p', _  = elim_vtest_helper p live_headers in        
+      let q', _ = elim_vtest_helper q live_headers in
+      VChoice(elim_vtest' p' live_headers, elim_vtest' q' live_headers)
     | _ -> p
 
 let elim_vtest p = elim_vtest' p TagSet.empty
@@ -587,6 +635,10 @@ let rec elim_vmod' p live_headers =
       let p', live_headers' = elim_vmod' p live_headers in
       let q', live_headers'' = elim_vmod' q live_headers in
       VPar(p', q'), TagSet.union live_headers' live_headers''
+    | VChoice(p,q) ->
+      let p', live_headers' = elim_vmod' p live_headers in
+      let q', live_headers'' = elim_vmod' q live_headers in
+      VChoice(p', q'), TagSet.union live_headers' live_headers''
     | _ -> elim_vmod_helper p live_headers
 
 let elim_vmod p = fst (elim_vmod' p TagSet.empty)
@@ -618,6 +670,7 @@ let rec collect_tags' p tags =
     | VTest(h, _) -> TagSet.add h tags
     | VPar(p,q) -> TagSet.union (collect_tags' p tags) (collect_tags' q tags)
     | VSeq(p,q) -> TagSet.union (collect_tags' p tags) (collect_tags' q tags)
+    | VChoice(p,q) -> TagSet.union (collect_tags' p tags) (collect_tags' q tags)
     | VStar(p) -> collect_tags' p tags
     | _ -> tags
 
@@ -700,6 +753,9 @@ let rec convert_tag_to_hdr tag_alist h p written =
     | VPar(p,q) -> let p', written' = convert_tag_to_hdr tag_alist h p written in
                    let q', written'' = convert_tag_to_hdr tag_alist h q written in
                    Par(p',q'), written' || written''
+    | VChoice(p,q) -> let p', written' = convert_tag_to_hdr tag_alist h p written in
+                      let q', written'' = convert_tag_to_hdr tag_alist h q written in
+                      Choice(p',q'), written' || written''
     | VStar(p) -> let p',written = convert_tag_to_hdr tag_alist h p written in
                   Star(p'), written
     | VLink(sw,pt,sw',pt') -> Seq(Seq(Filter(Test(Switch, sw)), Filter(Test(Header SDN_Types.InPort, pt))),
@@ -709,4 +765,10 @@ let rec convert_tag_to_hdr tag_alist h p written =
 let dehop_policy p =
   let (i,s,t,e) = dehopify p in
   let p' = (VSeq(i, VSeq(VStar(VSeq(s,t)), VSeq(s,e)))) in
+  let () = Printf.printf "%s\n%!" (string_of_vpolicy p') in
+  fst (convert_tag_to_hdr (collect_tags p') SDN_Types.Vlan p' false)
+
+let dehop_policy_opt p =
+  let (i,s,t,e) = dehopify p in
+  let p' = optimize (VSeq(i, VSeq(VStar(VSeq(s,t)), VSeq(s,e)))) in
   fst (convert_tag_to_hdr (collect_tags p') SDN_Types.Vlan p' false)
