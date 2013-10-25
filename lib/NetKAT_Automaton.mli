78e6ad9617d0e8fa289b6db0370f741fcb1f49c6
Spiros Eliopoulos <seliopou@gmail.com>
2013-10-25 19:44:29 -0400

    auto: expose module internals

--- lib/NetKAT_Automaton.mli
+++ lib/NetKAT_Automaton.mli
@@ -1,7 +1,14 @@
-type policy
+type policy = NetKAT_Types.policy
 type lf_policy
 type link
-type regex
+
+type regex =
+  | Char of lf_policy * link
+  | Alt of regex * regex
+  | Cat of regex * regex
+  | Kleene of regex
+  | Empty
+
 
 val regex_of_policy : policy -> regex
 val regex_to_policy : regex -> policy
