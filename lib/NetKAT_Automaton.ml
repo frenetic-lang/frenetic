5011856bd24512f9218dfe9326be0242cadd00ad
Spiros Eliopoulos <seliopou@gmail.com>
2013-10-25 20:34:48 -0400

    auto: rework continuation data type, actually works
    A few things. First, the previous represntation of continuations
    suffered the limitation that it was impossible to combine a
    Seq(NL, TP). This commit fixes this issue by having NL take an optional,
    `link_provider`, which coincidentally is the same type that the TP
    constructor raps. Raps.
    
    Because of this change, regex_of_policy is now complete, with the
    exception of choice, which we're saving for later.
    
    Second, the initial constructors for TP and NL functions were incorrect.
    They would drop previously accumulated values.

--- lib/NetKAT_Automaton.ml
+++ lib/NetKAT_Automaton.ml
@@ -3,6 +3,21 @@ type pred = NetKAT_Types.pred
 type header = NetKAT_Types.header
 type header_val = NetKAT_Types.header_val
 
+(* BEGIN HELPER FUNCTIONS --------------------------------------------------- *)
+
+let lift_option2 f m1 m2 = 
+  match m1, m2 with
+    | None, _ 
+    | _   , None       -> None
+    | Some e1, Some e2 -> Some(f e1 e2)
+
+let is_none m =
+  match m with
+    | None   -> true
+    | Some _ -> false
+
+(* END HELPER FUNCTIONS ----------------------------------------------------- *)
+
 type lf_policy =
   | Filter of pred
   | Mod of header * header_val
@@ -21,35 +36,51 @@ type regex =
   | Empty
 
 type inter =
-  | TP of (lf_policy option -> inter)
-  (* TODO(seliopou): This should be `lf_policy link either` *)
-  | NL of (lf_policy option -> link option -> inter)
-  | S of regex (* this is only in the star case *)
+  | TP of link_provider
+  | NL of link_consumer
+  | S  of regex (* this is only in the star case *)
+and link_provider = lf_policy option -> inter
+and link_consumer = lf_policy option -> link_provider option -> inter
 
-let is_none m =
-  match m with
-    | None   -> true
-    | Some _ -> false
+
+let rec mk_seq (mp : lf_policy option) (q : lf_policy) =
+  match mp with
+    | None   -> q
+    | Some p -> Seq(p, q)
+
+let rec mk_nl (lfp : lf_policy) : link_consumer =
+  fun mlfp mlp ->
+    let lfp' = mk_seq mlfp lfp in
+    match mlp with
+      | None    -> NL(mk_nl lfp')
+      | Some lp -> lp (Some(lfp'))
+
+let rec mk_tp (l : link) (macc : lf_policy option) : link_provider =
+  fun mlfq ->
+    match mlfq with
+      | None -> 
+        let lfp = match macc with
+          | None     -> Filter(NetKAT_Types.True)
+          | Some acc -> acc in
+        S(Char(lfp, l))
+      | Some _ -> 
+        TP(fun mlfp -> mk_tp l (lift_option2 (fun x y -> Seq(x, y)) mlfq macc) mlfp)
 
 let regex_of_policy (p : policy) : regex =
 
-  let rec of_mlink (ml : link option) : inter =
-    match ml with
-      | None   -> NL(fun mp ml -> assert (is_none mp); of_mlink ml)
-      | Some l -> S(Char(Filter NetKAT_Types.True, l)) in
-  let mk_seq (mp : lf_policy option) (q : lf_policy) : lf_policy=
-    match mp with
-      | None -> q
-      | Some p -> Seq(p, q) in
+  let rec of_mlink (mlp : link_provider option) : inter =
+    match mlp with
+      | None    -> NL(fun mp mlp -> assert (is_none mp); of_mlink mlp)
+      | Some lp -> lp None in
 
-  let rec rpc (p : policy) : inter = 
+  let rec rpc (p : policy) : inter =
     begin match p with
       | NetKAT_Types.Filter(q) ->
-        NL(e_nl_seq_trans (Filter q))
+        NL(mk_nl (Filter q))
       | NetKAT_Types.Mod(h, v) ->
-        NL(e_nl_seq_trans (Mod(h, v)))
+        NL(mk_nl (Mod(h, v)))
       | NetKAT_Types.Link(sw1, pt1, sw2, pt2) ->
-        TP(e_tp_seq_trans (sw1, pt1, sw2, pt2))
+        TP(mk_tp (sw1, pt1, sw2, pt2) None)
       | NetKAT_Types.Seq(p1, p2) ->
         rpc_seq (rpc p1) (rpc p2)
       | NetKAT_Types.Par(p1, p2) ->
@@ -63,7 +94,7 @@ let regex_of_policy (p : policy) : regex =
     begin match i, j with
       | TP f1, NL f2 -> NL(fun mp ml -> rpc_seq (f1 mp) (f2 None ml))
       | TP f1, i -> let r = run i in TP(fun mp -> rpc_seq (f1 mp) (S(r)))
-      | NL f1, TP f2 -> TP(failwith "nyi")
+      | NL f1, TP f2 -> f1 None (Some f2)
       | NL f1, NL f2 -> NL(fun mp ml -> rpc_seq (f1 mp None) (f2 None ml))
       | NL f1, S  s2 -> failwith "Cat(NL, Star) can't be represented"
       | S  s1, i -> s_trans s1 i (fun x y -> Cat(x, y))
@@ -83,21 +114,6 @@ let regex_of_policy (p : policy) : regex =
       | S  s1, i -> s_trans s1 i (fun x y -> Alt(x, y))
     end
 
-  and e_nl_seq_trans (q : lf_policy) (mp : lf_policy option) (ml : link option) =
-    begin match ml with
-      | None -> NL(e_nl_seq_trans (mk_seq mp q))
-      | Some(sw1, pt1, sw2, pt2) ->
-        e_tp_seq_trans (sw1, pt1, sw2, pt2) (Some(mk_seq mp q))
-    end
-
-  and e_tp_seq_trans (l : link) (mq : lf_policy option) =
-    begin match mq with
-      | None ->
-        S(Char(Filter NetKAT_Types.True, l))
-      | Some q ->
-        TP(fun mp -> e_tp_seq_trans l (Some(mk_seq mp q)))
-    end
-
   and s_trans (r : regex) (i : inter) c =
     begin match i with
       | NL f ->
@@ -106,10 +122,10 @@ let regex_of_policy (p : policy) : regex =
         S(c r (run i))
     end
 
-  and run_with (i : inter) (mp : lf_policy option) (ml : link option) : inter =
+  and run_with (i : inter) (mp : lf_policy option) (mlp : link_provider option) : inter =
     begin match i with
       | TP f -> f mp
-      | NL f -> f mp ml
+      | NL f -> f mp mlp
       | S  r -> S(r)
     end
 
@@ -155,3 +171,4 @@ let rec regex_to_policy (r : regex) : policy =
     | Kleene(r) ->
       NetKAT_Types.Star(regex_to_policy r)
     | Empty -> NetKAT_Types.Filter(NetKAT_Types.True)
+
