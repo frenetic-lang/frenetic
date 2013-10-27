995e43a0cd9d24d31940666c46938bab7e1a7e29
Spiros Eliopoulos <seliopou@gmail.com>
2013-10-27 14:31:40 -0400

    auto: expose link-free and link functions
    Useful in general, but in particular for debugging and testing.

--- lib/NetKAT_Automaton.mli
+++ lib/NetKAT_Automaton.mli
@@ -2,6 +2,9 @@ type policy = NetKAT_Types.policy
 type lf_policy
 type link
 
+val lf_policy_to_policy : lf_policy -> policy
+val link_to_policy : link -> policy
+
 type regex =
   | Char of lf_policy * link
   | Alt of regex * regex
@@ -12,3 +15,6 @@ type regex =
 
 val regex_of_policy : policy -> regex
 val regex_to_policy : regex -> policy
+
+val regex_to_string : regex -> string
+val lf_policy_to_string : lf_policy -> string
