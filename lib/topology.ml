67290480be3c955df2d0368694d5dc241a8efc8d
Shrutarshi Basu <shr@basus.me>
2013-10-28 13:30:33 -0400

    Fixed topology interface to keep dependent code compiling

--- lib/topology.ml
+++ lib/topology.ml
@@ -1,7 +1,90 @@
+open Graph
+open Packet
 
 type switchId = int64
 type portId = int32
-type rate = Rate of int64 * int64
+
+module type NODE =
+sig
+  type t = Host of string
+           | Switch of string * switchId
+           | Mbox of string * string list
+  type label = t
+
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val to_dot : t -> string
+  val to_string : t -> string
+
+  val id_of_switch : t -> switchId
+end
+
+module type LINK =
+sig
+  type v
+  type t = {
+    srcport : portId;
+    dstport : portId;
+    cost : int64;
+    capacity : int64;
+  }
+  type e = (v * t * v)
+  val default : t
+  val compare : t -> t -> int
+
+  (* Constructors *)
+  val mk_edge : v -> v -> t -> e
+  val mk_link : v -> int -> v -> int -> int64 -> int64 -> e
+  val reverse : e -> e
+
+  (* Accesssors *)
+  val src : e -> v
+  val dst : e -> v
+  val label : e -> t
+
+  val capacity : e -> Int64.t
+  val cost : e -> Int64.t
+  val srcport : e -> Int32.t
+  val dstport : e -> Int32.t
+
+  (* Utilities *)
+  val name : e -> string
+  val string_of_label : e -> string
+  val to_dot : e -> string
+  val to_string : e -> string
+end
+
+module type TOPO =
+sig
+  include Sig.P
+
+  (* Constructors *)
+  val add_node : t -> V.t -> t
+  val add_host : t -> string -> t
+  val add_switch : t -> string -> switchId -> t
+  val add_switch_edge : t -> V.t -> portId -> V.t -> portId -> t
+
+  (* Accessors *)
+  val get_vertices : t -> V.t list
+  val get_edges : t -> E.t list
+  val get_ports : t -> V.t -> V.t -> (portId * portId)
+  val get_hosts : t -> V.t list
+  val get_switches : t -> V.t list
+  val get_switchids : t -> switchId list
+  val ports_of_switch : t -> V.t -> portId list
+  val edge_ports_of_switch : t -> V.t -> portId list
+  val next_hop : t -> V.t -> portId -> V.t
+
+  (* Utility functions *)
+  val shortest_path : t -> V.t -> V.t -> E.t list
+  val to_dot : t -> string
+  val to_string : t -> string
+  val to_mininet : t -> string
+
+  (* Exceptions *)
+  exception NotFound of string
+  exception NoPath of string * string
+end
 
 module Node = Core.Node
 module Link = Core.Link
