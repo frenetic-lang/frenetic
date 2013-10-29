f055132c4ee2f8f18806e57560e8726b24e7bed4
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-10-29 13:45:33 -0400

    Added string_of_pred, exposed Types/Pretty/Controller in oasis

--- lib/Controller.mli
+++ lib/Controller.mli
@@ -1,3 +1,4 @@
 (** Implements a controller for ONF. *)
 
 val start : port:int -> pols:Types.policy NetCore_Stream.t -> unit Lwt.t
+val start_no_dehop : port:int -> pols:Types.policy NetCore_Stream.t -> unit Lwt.t
