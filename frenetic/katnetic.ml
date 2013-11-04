e59184f950ec7b85e2680791cc533a86c7d91663
Nate Foster <jnfoster@cs.cornell.edu>
2013-11-04 10:35:42 -0500

    Local compiler bugfixes and front-end improvements
    * Fixed several bugs in `LocalCompiler`
    
      - `bin_local` now keeps the three disjoint sub-policies (`p_inter_q`,
        `p_only`, and `q_only`) completely separate
    
      - `group_to_string` and `to_netkat` was backwards for groups
    
      - removed `op` for `extend`, which is no longer needed
    
    * Fixed one of the unit tests in light of these changes
    
    * Front-end improvements to run examples. For example:
    
        [nate@northstar ~/shared/frenetic> ./katnetic.byte dump examples/choice_loops.kat
        filter switch = 1 and port = 1;
        (filter vlanId = 6; (id; vlanId := 17) |
         filter vlanId = 7; (id; (vlanId := 11 + vlanId := 12)) |
         filter vlanId = 9; (id; (vlanId := 11 + vlanId := 12))) |
        drop
        [{pattern={InPort=1,Vlan=6,action=SetField(Vlan,17); OutputPort(1),
              cookie=0,idle_timeout=Permanent,hard_timeout=Permanent}
             {pattern={InPort=1,Vlan=7,
             action=(SetField(Vlan,11); OutputPort(1)) +
                    SetField(Vlan,12); OutputPort(1),
             cookie=0,idle_timeout=Permanent,hard_timeout=Permanent}
             {pattern={InPort=1,Vlan=9,
             action=(SetField(Vlan,11); OutputPort(1)) +
                    SetField(Vlan,12); OutputPort(1),
             cookie=0,idle_timeout=Permanent,hard_timeout=Permanent}]

--- frenetic/katnetic.ml
+++ frenetic/katnetic.ml
@@ -21,11 +21,29 @@ let run args = match args with
 
 let dump args = match args with 
   | [ filename ] ->
+    let sw = VInt.Int64 1L in 
     let cin = open_in filename in
     let pol = Parser.program Lexer.token (Lexing.from_channel cin) in
-    let ifm = LocalCompiler.RunTime.compile pol in  
-    let tbl = LocalCompiler.RunTime.to_table (VInt.Int64 0L) ifm in 
-    Format.printf "%a\n%!" SDN_Types.format_flowTable tbl
+    let i,m,_ = NetKAT_Automaton.dehopify pol in     
+    let pol0 = 
+      let open Types in 
+      let open SDN_Types in 
+      NetKAT_Automaton.SwitchMap.fold 
+        (fun (swi,pti) poli acc -> 
+          if sw = swi then 
+            Par(Seq(Filter (And(Test(Switch, swi), 
+                                Test(Header InPort, pti))),
+                    poli),
+                acc)
+          else 
+            acc) 
+        m Types.drop in 
+    let pol0' = Types.Seq(i,pol0) in 
+    let ifm0 = LocalCompiler.RunTime.compile pol0' in 
+    let tbl0 = LocalCompiler.RunTime.to_table sw ifm0 in 
+    Format.printf "@[%a\n%a@\n@]%!"
+      Pretty.format_policy pol0
+      SDN_Types.format_flowTable tbl0
   | _ -> help [ "dump" ]
 
 let () = 
