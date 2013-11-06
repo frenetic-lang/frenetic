6fc51d121c53485433873bf95cab69d097bb575b
Spiros Eliopoulos <seliopou@gmail.com>
2013-11-06 11:26:01 -0500

    autoc: katnetic dump now dumps all switch tables

--- frenetic/katnetic.ml
+++ frenetic/katnetic.ml
@@ -21,23 +21,24 @@ let run args = match args with
 
 let dump args = match args with 
   | [ filename ] ->
-    let sw = VInt.Int64 1L in 
     let cin = open_in filename in
     let pol = Parser.program Lexer.token (Lexing.from_channel cin) in
     let i,m,_,e = NetKAT_Automaton.dehopify pol in
     let switch_policies =
         NetKAT_Automaton.switch_port_policies_to_switch_policies m in
-    let pol0 = 
-      let open Types in 
-      let open SDN_Types in 
-      Seq(Filter(Test(Switch, sw)),
-          NetKAT_Automaton.SwitchMap.find sw switch_policies) in
-    let pol0' = Types.(Seq(Seq(Par(i, id),pol0),Par(e, id))) in
-    let ifm0 = LocalCompiler.RunTime.compile pol0' in 
-    let tbl0 = LocalCompiler.RunTime.to_table sw ifm0 in 
-    Format.printf "@[%a\n%a@\n@]%!"
-      Pretty.format_policy pol0'
-      SDN_Types.format_flowTable tbl0
+    NetKAT_Automaton.SwitchMap.iter (fun sw p ->
+      let pol0 =
+        let open Types in
+        let open SDN_Types in
+        Seq(Filter(Test(Switch, sw)),
+            NetKAT_Automaton.SwitchMap.find sw switch_policies) in
+      let pol0' = Types.(Seq(Seq(Par(i, id),pol0),Par(e, id))) in
+      let ifm0 = LocalCompiler.RunTime.compile pol0' in
+      let tbl0 = LocalCompiler.RunTime.to_table sw ifm0 in
+      Format.printf "@[%a\n%a@\n\n@]%!"
+        Pretty.format_policy pol0'
+        SDN_Types.format_flowTable tbl0)
+    switch_policies
   | _ -> help [ "dump" ]
 
 let () = 
