6751e5c55cac8adc9e87082542acf602e871f689
Marco Canini <marco@net.t-labs.tu-berlin.de>
2013-11-07 17:31:42 -0200

    Simple fix.

--- lib/PolicyGenerator.ml
+++ lib/PolicyGenerator.ml
@@ -35,9 +35,9 @@ let all_pairs_shortest_paths (g : t) =
   List.fold_left
     (fun pol p ->
       match p with
-        | ((Node.Host src, Node.Host dst), []) -> 
+        | ((Node.Host (src,_,_), Node.Host (dst,_,_)), []) -> 
 	  pol
-        | ((Node.Host src, Node.Host dst), v::path) -> 
+        | ((Node.Host (src,_,_), Node.Host (dst,_,_)), v::path) -> 
 	  (* TODO(basus): replace with MAC address *)
 	  let src = VInt.Int48 0L in 
 	  let dst = VInt.Int48 1L in 
