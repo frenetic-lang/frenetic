bdf48ac4242a1bbd77a7d88179848a5fbb2ee391
Spiros Eliopoulos <seliopou@gmail.com>
2013-12-11 11:01:49 -0500

    separate: remove unused Misc code
    These functions were unused, and most of them are exported by the Packet
    module anyways.

--- lib/OpenFlow0x04_Misc.mli
+++ lib/OpenFlow0x04_Misc.mli
@@ -33,13 +33,6 @@ val test_bit : int -> Int32.t -> bool
 val clear_bit : int -> Int32.t -> Int32.t
 val set_bit : int -> Int32.t -> Int32.t
 val bit : Int32.t -> int -> bool -> Int32.t
-val mac_of_bytes : string -> Int64.t
-val get_byte : Int64.t -> int -> int
-val string_of_mac : Int64.t -> string
-val bytes_of_mac : Int64.t -> string
-
-val filter_map : ('a -> 'b option) -> 'a list -> 'b list
-val intersperse : 'a -> 'a list -> 'a list
 
 module type SAFESOCKET = sig
   type t = Lwt_unix.file_descr
