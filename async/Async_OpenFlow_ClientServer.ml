da30c6676b9b367711d0bec0e9461dbd3a889241
Arjun Guha <arjun@cs.umass.edu>
2013-12-27 10:10:38 -0500

    async: better logging

--- async/Async_OpenFlow_ClientServer.ml
+++ async/Async_OpenFlow_ClientServer.ml
@@ -35,7 +35,8 @@ module Make (M : Message.Message) : S with type t = M.t = struct
   let deserialize ?(label="") (raw_reader : Reader.t) : M.t Pipe.Reader.t =
     let (msg_reader_r, msg_reader_w) = Pipe.create () in
     let _ = Deferred.repeat_until_finished () (fun () ->
-      Serialization.deserialize raw_reader
+      Serialization.deserialize ~tags:[("openflow", "serialization")] 
+        ~label:label raw_reader
       >>= function
       | `Eof -> return (`Finished ())
       | `Ok m -> Pipe.write_without_pushback msg_reader_w m; 
@@ -45,12 +46,8 @@ module Make (M : Message.Message) : S with type t = M.t = struct
   let serializer ?(label="") (raw_writer : Writer.t) : M.t Pipe.Writer.t =
     let (msg_writer_r, msg_writer_w) = Pipe.create () in          
     let _ = Pipe.iter_without_pushback msg_writer_r (fun msg ->
-      Serialization.serialize raw_writer msg;
-      Log.of_lazy
-        ~level:`Debug
-        ~tags:[("openflow", "serialization")] 
-        (lazy (sprintf "[%s] write message: %s"
-                 label (M.to_string msg)))) in
+      Serialization.serialize ~tags:[("openflow", "serialization")] ~label:label
+        raw_writer msg) in
     msg_writer_w
 
   let listen where_to_listen msg_handler_receiver =
