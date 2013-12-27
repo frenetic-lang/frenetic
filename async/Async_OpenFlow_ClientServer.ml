0b5eb3669199be5492338b4ed5eb4f913f032f97
Arjun Guha <arjun@cs.umass.edu>
2013-12-27 18:25:24 -0500

    async: robustness for ClientServer module
    - If the socket closes, we also close the pipes carrying parsed messages
    - Evaluate getpeername eagerly. It will throw an exception if applied to
      a closed socket. (!!!)
    - Ignore errors signalled by a handler (default behavior is to stop listening).
      In the future, we might want to print an error instead, but stop-listening is
      sort of ridiculous.

--- async/Async_OpenFlow_ClientServer.ml
+++ async/Async_OpenFlow_ClientServer.ml
@@ -38,25 +38,32 @@ module Make (M : Message.Message) : S with type t = M.t = struct
       Serialization.deserialize ~tags:[("openflow", "serialization")] 
         ~label:label raw_reader
       >>= function
-      | `Eof -> return (`Finished ())
+      | `Eof ->
+        Pipe.close msg_reader_w;
+        Log.info ~tags:[("openflow", "socket")]
+          "[%s] Socket reader closed, thus message also reader closed." label;
+        return (`Finished ())
       | `Ok m -> Pipe.write_without_pushback msg_reader_w m; 
                  return (`Repeat ())) in
     msg_reader_r    
 
   let serializer ?(label="") (raw_writer : Writer.t) : M.t Pipe.Writer.t =
-    let (msg_writer_r, msg_writer_w) = Pipe.create () in          
+    let (msg_writer_r, msg_writer_w) = Pipe.create () in
+    (* We close msg_writer_w, which prevents any further writes. *)
+    Stream.iter (Monitor.errors (Writer.monitor raw_writer)) ~f:(fun exn -> 
+      Pipe.close msg_writer_w; (* TODO(arjun): what if already closed? will error! *)
+      Log.error ~tags:[("openflow", "socket")]
+        "[%s] socket write failed, thus message writer also closed." label);
     let _ = Pipe.iter_without_pushback msg_writer_r (fun msg ->
       Serialization.serialize ~tags:[("openflow", "serialization")] ~label:label
         raw_writer msg) in
     msg_writer_w
 
   let listen where_to_listen msg_handler_receiver =
-    Tcp.Server.create where_to_listen (fun sock r w ->
+    Tcp.Server.create ~on_handler_error:`Ignore where_to_listen (fun sock r w ->
       let sock_str = Socket.Address.to_string sock in
-      Log.of_lazy
-        ~level:`Info
-        ~tags:[("openflow", "socket")] 
-        (lazy (sprintf "new client %s" sock_str));
+      Log.info ~tags:[("openflow", "socket")] 
+        "new client %s" sock_str;
       msg_handler_receiver (deserialize ~label:sock_str r)
                            (serializer ~label:sock_str w))
 
@@ -66,12 +73,10 @@ module Make (M : Message.Message) : S with type t = M.t = struct
                 'a Deferred.t) 
     : 'a Deferred.t =
     Tcp.with_connection addr (fun sock sock_reader sock_writer ->
-      let sock_str = Socket.Address.to_string (Socket.getsockname sock) in
-      Log.of_lazy
-        ~level:`Info
-        ~tags:[("openflow", "socket")] 
-        (lazy (sprintf "new connection %s (peer is %s)" sock_str
-                 (Socket.Address.to_string (Socket.getpeername sock))));
+      Log.info ~tags:[("openflow", "socket")] 
+        "new connection %s (peer is %s)"
+        (Socket.Address.to_string (Socket.getsockname sock))
+        (Socket.Address.to_string (Socket.getpeername sock));
       handler sock (deserialize ~label:sock_str sock_reader)
                      (serializer ~label:sock_str sock_writer))
 
