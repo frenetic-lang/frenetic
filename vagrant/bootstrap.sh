6854ac17487fad165d3b9f3b117c62e4c8ac9253
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-12-30 13:24:56 -0500

    Fix typo in mininet provisioning. Add wireshark to VM.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get install -y python-software-properties  build-essential m4
 add-apt-repository ppa:avsm/ppa
 apt-get update
-apt-get install -y ocaml opam
+apt-get install -y ocaml opam python-networkx wireshark
