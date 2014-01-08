dbce695478518bb63979c6b6a2bda5270d255212
Nate Foster <jnfoster@cs.cornell.edu>
2014-01-07 20:45:09 -0500

    Travis CI fixes for Jane Street libraries

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt cstruct ocamlgraph ounit pa_ounit quickcheck"
+OPAM_DEPENDS="lwt cstruct ocamlgraph ounit pa_ounit quickcheck core async sexplib"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
