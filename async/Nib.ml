5cd6e7e74b83971caac2b2f52f1e09a25142a8b1
Spiros Eliopoulos <seliopou@gmail.com>
2014-02-04 13:43:49 -0500

    topo: implement switch topology discovery

--- async/Nib.ml
+++ async/Nib.ml
@@ -120,3 +120,158 @@ module Probe = struct
     ; nw = Unparsable(protocol, marshal' t)
     }
 end
+
+module Protocol : sig
+  open Async.Std
+
+  type t
+
+  module SwitchMap : Map.S
+    with type Key.t = SDN_Types.switchId
+
+  module PortSet : Set.S
+    with type Elt.t = Int64.t
+
+  val create
+    : ?nib:State.t
+    -> unit
+    -> t
+
+  val add_switch
+    : t
+    -> send:(OpenFlow0x01.Message.t -> unit Deferred.t)
+    -> OpenFlow0x01.SwitchFeatures.t
+    -> t Deferred.t
+
+  val remove_switch
+    : t
+    -> SDN_Types.switchId
+    -> t
+
+  val handle_port_status
+    : t
+    -> send:(OpenFlow0x01.Message.t -> unit Deferred.t)
+    -> SDN_Types.switchId
+    -> OpenFlow0x01.PortStatus.t
+    -> t Deferred.t
+
+  val handle_packet_in
+    : t
+    -> send:(OpenFlow0x01.Message.t -> unit Deferred.t)
+    -> SDN_Types.switchId
+    -> OpenFlow0x01.PacketIn.t
+    -> (t * OpenFlow0x01.PacketIn.t option) Deferred.t
+
+end = struct
+  open Async.Std
+
+  module SwitchMap = Map.Make(Int64)
+  module PortSet = Set.Make(Int64)
+
+  type t =
+    { nib : State.t
+    ; pending : PortSet.t SwitchMap.t
+    }
+
+  let create ?nib () =
+    let nib = match nib with
+      | Some(n) -> n
+      | None -> State.empty in
+    { nib; pending = SwitchMap.empty }
+
+  open OpenFlow0x01
+
+  (* XXX(seliopou): Depending on how the switch is configured, these may not be
+   * strictly necessary. Also, see the comment below for notes on timing.
+   * *)
+  let setup_flows =
+    let open Message in
+    let open OpenFlow0x01_Core in
+    let nib_probe = { match_all
+      with dlTyp = Some(Probe.protocol)
+         ; dlSrc = Some(Probe.mac)
+         ; dlDst = Some(0xffffffffffffL)
+    } in
+    [ FlowModMsg (add_flow 65535 nib_probe  [Output(Controller(512))])
+    ]
+
+  let port_useable descr =
+    let open PortDescription in
+     if descr.config.PortConfig.down
+       then false
+       else not (descr.state.PortState.down)
+
+  let receive_probe t switch_id port_id probe =
+    let open Probe in
+    let ports = try SwitchMap.find_exn t.pending probe.switch_id
+      with Not_found -> PortSet.empty in
+    let n1, n2 = State.Switch switch_id, State.Switch probe.switch_id in
+    let nib = match State.has_port t.nib n1 port_id with
+      | Some(l) ->
+        assert (l = (n2, probe.port_id));
+        t.nib
+      | None ->
+        State.add_link t.nib n1 port_id n2 probe.port_id in
+    { nib
+    ; pending = SwitchMap.add t.pending switch_id (PortSet.remove ports probe.port_id)
+    }
+
+  let add_port t ~send ~switch_id port =
+    let port_no = port.PortDescription.port_no in
+    let port_id = Int64.of_int port_no in
+    let set = match SwitchMap.find t.pending switch_id with
+      | Some(set0) -> PortSet.add set0 port_id
+      | None -> PortSet.singleton port_id in
+    let open OpenFlow0x01_Core in
+    let output_payload =
+      NotBuffered (Packet.marshal Probe.(to_packet { switch_id; port_id })) in
+    let apply_actions = [Output (PhysicalPort(port_no))] in
+    (* XXX(seliopou): In addition to sending the initial probe, this function
+     * should also start a timer that checks that the probe has been received by
+     * the controller and, if it has not, should send another probe.
+     *
+     * Until everything else is in working order, this will do.
+     * *)
+    send (Message.PacketOutMsg { output_payload; port_id = None; apply_actions })
+    >>| fun _ -> { t
+      with pending = SwitchMap.add t.pending switch_id set }
+
+  let remove_port t ~switch_id port =
+    let port_no = port.OpenFlow0x01.PortDescription.port_no in
+    { t with nib = State.remove_port t.nib (State.Switch switch_id) (Int64.of_int port_no) }
+
+  let add_switch t ~send feats =
+    let open SwitchFeatures in
+    let switch_id = feats.switch_id in
+    let ports = List.filter feats.ports ~f:port_useable in
+    Deferred.List.map ~how:`Parallel setup_flows ~f:send >>= fun _ ->
+    Deferred.List.fold ports ~init:t ~f:(add_port ~send ~switch_id)
+    >>| fun t' -> { t'
+      with nib = State.add_node t'.nib (State.Switch feats.switch_id) }
+
+  let remove_switch t switch_id =
+    { t with nib = State.remove_node t.nib (State.Switch switch_id) }
+
+  let handle_port_status t ~send switch_id port_status =
+    let open PortStatus in
+    match port_status.reason, port_useable port_status.desc with
+      | ChangeReason.Add, true
+      | ChangeReason.Modify, true ->
+        add_port t ~switch_id port_status.desc ~send
+      | ChangeReason.Delete, _
+      | ChangeReason.Modify, false ->
+        return (remove_port t ~switch_id port_status.desc)
+      | _ ->
+        return t
+
+  let handle_packet_in t ~send switch_id pi =
+    let open OpenFlow0x01_Core in
+    let open Packet in
+    match OpenFlow0x01_Core.parse_payload pi.input_payload with
+      | { dlDst = 0xffffffffffffL; dlSrc; nw = Unparsable (dlTyp, bytes) }
+        when dlTyp = Probe.protocol && dlSrc = Probe.mac ->
+          let t = receive_probe t switch_id (Int64.of_int pi.port) (Probe.parse bytes) in
+          return (t, None)
+      | _ -> return (t, Some(pi))
+
+end
