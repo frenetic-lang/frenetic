0a4fa3f5a381c87eaa6eda953201c7048953b797
Shrutarshi Basu <shr@basus.me>
2014-02-27 22:25:46 -0500

    Added single-node topology parser test

--- test/Parser_Test.ml
+++ test/Parser_Test.ml
@@ -1,10 +1,10 @@
 open Network
+open Physical
 
 open OUnitHack
 TEST "true" = true
 
 TEST "single node" =
-    let t = Physical.Parse.from_dotfile "test/dot/single_node.dot" in
-    Printf.printf "Parsed\n";
-    Printf.printf "\n%s\n" (Physical.Pretty.to_string t);
-    false
+    let t = Parse.from_dotfile "test/dot/single_node.dot" in
+    Printf.printf "\n%s\n" (Pretty.to_string t);
+    (Topology.VertexSet.cardinal (Topology.vertexes t) = 1)
