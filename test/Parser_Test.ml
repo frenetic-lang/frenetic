cb48306cfa2fee90e5f62b7aa1871cad26561c9c
Shrutarshi Basu <shr@basus.me>
2014-02-27 23:05:26 -0500

    Added single-edge test

--- test/Parser_Test.ml
+++ test/Parser_Test.ml
@@ -7,3 +7,8 @@ TEST "true" = true
 TEST "single node" =
     let t = Parse.from_dotfile "test/dot/single_node.dot" in
     (Topology.VertexSet.cardinal (Topology.vertexes t) = 1)
+
+TEST "single edge" =
+    let t = Parse.from_dotfile "test/dot/single_edge.dot" in
+    (Topology.VertexSet.cardinal (Topology.vertexes t) = 2) &&
+      (Topology.EdgeSet.cardinal (Topology.edges t) = 1)
