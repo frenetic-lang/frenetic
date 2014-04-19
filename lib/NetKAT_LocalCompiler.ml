frenetic-lang/frenetic

commit 9f61484d27359bdb2b2db2919d87ffb3ec97bc86

vint: port to vint branch of ocaml-openflow
User-facing changes:

  * parse specific bit-widths for specific fields

Incidental bugfixes:

  * fix issue where vlan was being parsed as ethTyp

lib/NetKAT_LocalCompiler.ml
@@ -2,8 +2,6 @@ open Core.Std
 open Sexplib.Conv
 open SDN_Types
 
-type location = NetKAT_Types.location
-
 (* Option functor *)
 module Option (H:NetKAT_Types.Headers.HEADER) =
 struct
@@ -218,8 +216,8 @@ module Action = struct
       ~ethType:(g (fun v -> EthType v))
       ~ipProto:(g (fun v -> IPProto v))
       (* TODO(arjun): I assume this means an exact match /32 *)
-      ~ipSrc:(g (fun v -> IP4Src (v, 32)))
-      ~ipDst:(g (fun v -> IP4Dst (v, 32)))
+      ~ipSrc:(g (fun v -> IP4Src (v, 32l)))
+      ~ipDst:(g (fun v -> IP4Dst (v, 32l)))
       ~tcpSrcPort:(g (fun v -> TCPSrcPort v))
       ~tcpDstPort:(g (fun v -> TCPDstPort v))
 
@@ -351,8 +349,8 @@ module Pattern = struct
       ~ethType:(g PN16.S.fold (fun v -> EthType v))
       ~ipProto:(g PN8.S.fold (fun v -> IPProto v))
       (* TODO(arjun): I assume this means an exact match /32 *)
-      ~ipSrc:(g PNIp.S.fold (fun v -> IP4Src (v, 32)))
-      ~ipDst:(g PNIp.S.fold (fun v -> IP4Dst (v, 32)))
+      ~ipSrc:(g PNIp.S.fold (fun v -> IP4Src (v, 32l)))
+      ~ipDst:(g PNIp.S.fold (fun v -> IP4Dst (v, 32l)))
       ~tcpSrcPort:(g PN16.S.fold (fun v -> TCPSrcPort v))
       ~tcpDstPort:(g PN16.S.fold (fun v -> TCPDstPort v))
 
@@ -784,14 +782,7 @@ end
 
 module RunTime = struct
 
-  let to_action (a:Action.t) (pto: fieldVal option) : seq =
-    let i8 x = VInt.Int8 x in
-    let i16 x = VInt.Int16 x in
-    let i32 x = VInt.Int32 x in
-    let i48 x = VInt.Int64 x in
-    let pto = match pto with
-      | Some(v) -> Some(VInt.get_int32 v)
-      | None -> None in
+  let to_action (a:Action.t) (pto: portId option) : seq =
     (* If an action sets the location to a pipe, ignore all other modifications.
      * They will be applied at the controller by Semantics.eval. Otherwise, the
      * port must be determined either by the pattern or by the action. The pto
@@ -804,23 +795,23 @@ module RunTime = struct
         [Controller 128]
       | Some (NetKAT_Types.Physical pt), _
       | None, Some pt ->
-        let g h c act f =
+        let g h act f =
           match Field.get f a with
             | None -> act
-            | Some v -> SetField(h,c v)::act in
+            | Some v -> Modify(h v)::act in
         HOV.Fields.fold
-          ~init:[OutputPort (VInt.Int32 pt)]
+          ~init:[OutputPort pt]
           ~location:(fun act _ -> act)
-          ~ethSrc:(g EthSrc i48)
-          ~ethDst:(g EthDst i48)
-          ~vlan:(g Vlan i16)
-          ~vlanPcp:(g VlanPcp i8)
-          ~ethType:(g EthType i16)
-          ~ipProto:(g IPProto i8)
-          ~ipSrc:(g IP4Src i32)
-          ~ipDst:(g IP4Dst i32)
-          ~tcpSrcPort:(g TCPSrcPort i16)
-          ~tcpDstPort:(g TCPDstPort i16)
+          ~ethSrc:(g (fun v -> SetEthSrc v))
+          ~ethDst:(g (fun v -> SetEthDst v))
+          ~vlan:(g (fun v -> SetVlan (Some(v))))
+          ~vlanPcp:(g (fun v -> SetVlanPcp v))
+          ~ethType:(g (fun v -> SetEthTyp v))
+          ~ipProto:(g (fun v -> SetIPProto v))
+          ~ipSrc:(g (fun v -> SetIP4Src v))
+          ~ipDst:(g (fun v -> SetIP4Dst v))
+          ~tcpSrcPort:(g (fun v -> SetTCPSrcPort v))
+          ~tcpDstPort:(g (fun v -> SetTCPDstPort v))
       | None, None ->
         failwith "indeterminate location"
 
@@ -832,7 +823,7 @@ module RunTime = struct
    * requires two copies of the packet at the switch, which is not possible.
    * Policies like these must be implemented at the controller.
    * *)
-  let set_to_action (s:Action.Set.t) (pto : fieldVal option) : par =
+  let set_to_action (s:Action.Set.t) (pto : portId option) : par =
     let f par a = (to_action a pto)::par in
     List.dedup (Action.Set.fold s ~f:f ~init:[])
 
@@ -843,69 +834,121 @@ module RunTime = struct
       idle_timeout = Permanent;
       hard_timeout = Permanent }
 
+  module Expanded(H:NetKAT_Types.Headers.HEADER) = struct
+    type t = (H.t option * bool) list with sexp
+    let compare = Pervasives.compare
+    let equal = (=)
+    let to_string hs =
+      Printf.sprintf "[%s]" 
+        (List.fold_left hs
+          ~init:"" ~f:(fun acc (mh, b) ->
+            Printf.sprintf "%s(%s,%b)"
+            (if acc = "" then acc else "; ")
+            (match mh with None -> "None" | Some(h) -> H.to_string h)
+            b))
+    let is_wild _ = false
+  end
+
+  module EHeaders = struct
+    include NetKAT_Types.Headers.Make
+    (Expanded(NetKAT_Types.LocationHeader))
+    (Expanded(NetKAT_Types.Int64Header))
+    (Expanded(NetKAT_Types.Int64Header))
+    (Expanded(NetKAT_Types.IntHeader))
+    (Expanded(NetKAT_Types.IntHeader))
+    (Expanded(NetKAT_Types.IntHeader))
+    (Expanded(NetKAT_Types.IntHeader))
+    (Expanded(NetKAT_Types.Int32Header))
+    (Expanded(NetKAT_Types.Int32Header))
+    (Expanded(NetKAT_Types.IntHeader))
+    (Expanded(NetKAT_Types.IntHeader))
+
+    let empty =
+      { location = []; 
+        ethSrc = [];
+        ethDst = [];
+        vlan = [];
+        vlanPcp = [];
+        ethType = [];
+        ipProto = [];
+        ipSrc = [];
+        ipDst = [];
+        tcpSrcPort = [];
+        tcpDstPort = [] }
+  end
+
   let expand_rules (x:Pattern.t) (s:Action.Set.t) : flowTable =
-    let i8 x = VInt.Int8 x in
-    let i16 x = VInt.Int16 x in
-    let i32 x = VInt.Int32 x in
-    let i48 x = VInt.Int64 x in
-    let il x = match x with
-      | NetKAT_Types.Pipe _ ->
-        failwith "indeterminate port"
-      | NetKAT_Types.Physical n ->
-        VInt.Int32 n in
-    let g os c h acc f =
-      let v = Field.get f x in
-      let l =
-        List.map (os v)
-          ~f:(fun (o,b) ->
-            match o with
-              | Some v -> (Some (c v), b)
-              | None -> (None, b)) in
-      FieldMap.add h l acc in
-    let m : ((fieldVal option * bool) list) FieldMap.t =
-      HPN.Fields.fold
-        ~init:FieldMap.empty
-        ~location:PNL.(g expand il InPort)
-        ~ethSrc:PN48.(g expand i48 EthSrc)
-        ~ethDst:PN48.(g expand i48 EthDst)
-        ~vlan:PN16.(g expand i16 Vlan)
-        ~vlanPcp:PN8.(g expand i8 VlanPcp)
-        ~ethType:PN16.(g expand i16 EthType)
-        ~ipProto:PN8.(g expand i8 IPProto)
-        ~ipSrc:PNIp.(g expand i32 IP4Src)
-        ~ipDst:PNIp.(g expand i32 IP4Dst)
-        ~tcpSrcPort:PN16.(g expand i16 TCPSrcPort)
-        ~tcpDstPort:PN16.(g expand i16 TCPDstPort) in
+    let m : EHeaders.t = let open EHeaders in
+      { location = PNL.(expand x.HPN.location);
+        ethSrc = PN48.(expand x.HPN.ethSrc);
+        ethDst = PN48.(expand x.HPN.ethDst);
+        vlan = PN16.(expand x.HPN.vlan);
+        vlanPcp = PN8.(expand x.HPN.vlanPcp);
+        ethType = PN16.(expand x.HPN.ethType);
+        ipProto = PN8.(expand x.HPN.ipProto);
+        ipSrc = PNIp.(expand x.HPN.ipSrc);
+        ipDst = PNIp.(expand x.HPN.ipDst);
+        tcpSrcPort = PN16.(expand x.HPN.tcpSrcPort);
+        tcpDstPort = PN16.(expand x.HPN.tcpDstPort);
+      }
+    in
     (* computes a cross product *)
-    let rec cross m : (fieldVal option FieldMap.t * bool) list =
-      FieldMap.fold
-        (fun h l rs ->
-          List.fold_right
-            l
-            ~init:[]
-            ~f:(fun (o,b) acc ->
-              List.fold_right
-                rs
-                ~init:acc
-                ~f:(fun (p,c) acc ->
-                  (FieldMap.add h o p, b && c)::acc)))
-        m [(FieldMap.empty,true)] in
+    let rec cross m : (HOV.t * bool) list =
+      let empty = let open HOV in
+      { location = None; ethSrc = None; ethDst = None;
+        vlan = None; vlanPcp = None; ethType = None;
+        ipProto = None; ipSrc = None; ipDst = None;
+        tcpSrcPort = None; tcpDstPort = None;
+      } in
+      let g h rs f =
+        let l = Field.get f m in
+        List.fold_right
+          l
+          ~init:[]
+          ~f:(fun (o, b) acc ->
+            List.fold_right
+              rs
+              ~init:acc
+              ~f:(fun (p, c) acc -> (h p o, b && c)::acc)) in
+      EHeaders.Fields.fold
+        ~init:[(empty, true)]
+        ~location:(g (fun p o -> { p with HOV.location = o }))
+        ~ethSrc:(g (fun p o -> { p with HOV.ethSrc = o }))
+        ~ethDst:(g (fun p o -> { p with HOV.ethDst = o }))
+        ~vlan:(g (fun p o -> { p with HOV.vlan = o }))
+        ~vlanPcp:(g (fun p o -> { p with HOV.vlanPcp = o }))
+        ~ethType:(g (fun p o -> { p with HOV.ethType = o }))
+        ~ipProto:(g (fun p o -> { p with HOV.ipProto = o }))
+        ~ipSrc:(g (fun p o -> { p with HOV.ipSrc = o }))
+        ~ipDst:(g (fun p o -> { p with HOV.ipDst= o }))
+        ~tcpSrcPort:(g (fun p o -> { p with HOV.tcpSrcPort = o }))
+        ~tcpDstPort:(g (fun p o -> { p with HOV.tcpDstPort = o }))
+      in
 
     (* helper function to generate the actual (pattern * par) rules for the SDN_Types.flowTable *)
-    let go (l:(fieldVal option FieldMap.t * bool) list) : flowTable =
-      List.map
-        l
-        ~f:(fun (x,b) ->
-          let pto = FieldMap.find InPort x in
-          let a = if b then set_to_action s pto else [] in
-          let y =
-            FieldMap.fold
-              (fun h o acc ->
-                match o with
-                  | None -> acc
-                  | Some v -> FieldMap.add h v acc)
-              x FieldMap.empty in
-          simpl_flow y a) in
+    let go (cd : (HOV.t * bool) list) : flowTable =
+      let il x = match x with
+        | NetKAT_Types.Pipe _ ->
+          failwith "indeterminate port"
+        | NetKAT_Types.Physical n ->
+          n in
+      List.map cd ~f:(fun (x, b) ->
+        let default_port = Core_kernel.Option.map ~f:il x.HOV.location in
+        let actions = if b then set_to_action s default_port else [] in
+        let pattern =
+          { SDN_Types.dlSrc = x.HOV.ethSrc
+          ; SDN_Types.dlDst = x.HOV.ethDst
+          ; SDN_Types.dlTyp = x.HOV.ethType
+          ; SDN_Types.dlVlan = x.HOV.vlan
+          ; SDN_Types.dlVlanPcp = x.HOV.vlanPcp
+          ; SDN_Types.nwSrc = x.HOV.ipSrc
+          ; SDN_Types.nwDst = x.HOV.ipDst
+          ; SDN_Types.nwProto = x.HOV.ipProto
+          ; SDN_Types.tpSrc = x.HOV.tcpSrcPort
+          ; SDN_Types.tpDst = x.HOV.tcpSrcPort
+          ; SDN_Types.inPort = default_port }
+        in
+        simpl_flow pattern actions) in
     go (cross m)
 
   type i = Local.t
@@ -989,11 +1032,22 @@ module Local_Optimize = struct
    *   ∀f, f ∈ p ⇒ f ∈ q ∧ (p(f) = q(f))
    *)
   let pattern_shadows (p: pattern) (q: pattern) : bool =
-    let check k v =
-      try FieldMap.find k q = v
-      with Not_found -> false
+    let check m1 m2 =
+      match m1 with
+        | None -> true
+        | Some(v1) ->
+          begin match m2 with
+            | None -> false
+            | Some(v2) -> v1 = v2
+          end
     in
-    FieldMap.for_all check p
+    let open SDN_Types in
+    check p.dlSrc q.dlSrc && check p.dlDst q.dlDst && check p.dlTyp q.dlTyp
+      && check p.dlVlan q.dlVlan && check p.dlVlanPcp q.dlVlanPcp
+      && check p.nwSrc q.nwSrc && check p.nwDst q.nwDst
+      && check p.nwProto q.nwProto
+      && check p.tpSrc q.tpSrc && check p.tpDst q.tpDst
+      && check p.inPort q.inPort
 
   (*
    * Optimize a flow table by removing rules which are shadowed by other rules.

async/Async_NetKAT_Controller.ml
@@ -68,20 +68,20 @@ let headers_to_actions
      | NetKAT_Types.Pipe p ->
        raise (Assertion_failed (Printf.sprintf
          "Controller.headers_to_action: impossible pipe location \"%s\"" p))
-     | NetKAT_Types.Physical p -> [OutputPort(VInt.Int32 p)] in
+     | NetKAT_Types.Physical p -> [OutputPort(p)] in
   NetKAT_Types.HeadersValues.Fields.fold
     ~init
     ~location:(fun acc f -> acc)
-    ~ethSrc:(g (fun v -> SetField(EthSrc, VInt.Int48 v)))
-    ~ethDst:(g (fun v -> SetField(EthDst, VInt.Int48 v)))
-    ~vlan:(g (fun v -> SetField(Vlan, VInt.Int16 v)))
-    ~vlanPcp:(g (fun v -> SetField(VlanPcp, VInt.Int8 v)))
-    ~ethType:(g (fun v -> SetField(EthType, VInt.Int16 v)))
-    ~ipProto:(g (fun v -> SetField(IPProto, VInt.Int8 v)))
-    ~ipSrc:(g (fun v -> SetField(IP4Src, VInt.Int32 v)))
-    ~ipDst:(g (fun v -> SetField(IP4Dst, VInt.Int32 v)))
-    ~tcpSrcPort:(g (fun v -> SetField(TCPSrcPort, VInt.Int16 v)))
-    ~tcpDstPort:(g (fun v -> SetField(TCPDstPort, VInt.Int16 v)))
+    ~ethSrc:(g (fun v -> Modify(SetEthSrc v)))
+    ~ethDst:(g (fun v -> Modify(SetEthDst v)))
+    ~vlan:(g (fun v -> Modify(SetVlan (Some(v)))))
+    ~vlanPcp:(g (fun v -> Modify(SetVlanPcp v)))
+    ~ethType:(g (fun v -> Modify(SetEthTyp v)))
+    ~ipProto:(g (fun v -> Modify(SetIPProto v)))
+    ~ipSrc:(g (fun v -> Modify(SetIP4Src v)))
+    ~ipDst:(g (fun v -> Modify(SetIP4Dst v)))
+    ~tcpSrcPort:(g (fun v -> Modify(SetTCPSrcPort v)))
+    ~tcpDstPort:(g (fun v -> Modify(SetTCPDstPort v)))
 
 exception Unsupported_mod of string
 
@@ -135,9 +135,11 @@ let packet_out_to_message (_, bytes, buffer_id, port_id, actions) =
   let output_payload = match buffer_id with
     | Some(id) -> Buffered(id, bytes)
     | None -> NotBuffered bytes in
-  let port_id = match port_id with
-    | Some(vi) -> Some(VInt.get_int vi)
-    | None -> None in
+  (* XXX(seliopou): This does not do a bounds check on the port. Leaving it for
+   * now as this entire function should be moved to ocaml-openflow in the very
+   * near future, at which point appropriate bounds checking will be added.
+   * *)
+  let port_id = Option.map port_id (fun x -> Int32.to_int_exn x) in
   let apply_actions = SDN_OpenFlow0x01.from_group port_id [[actions]] in
   OpenFlow0x01.Message.PacketOutMsg{ output_payload; port_id; apply_actions }
 
@@ -164,7 +166,7 @@ let to_event w_out (t : t) evt =
       return ((SwitchUp sw_id) :: (List.fold ports ~init:[] ~f:(fun acc pd ->
         let open OpenFlow0x01.PortDescription in
         if port_desc_useable pd && pd.port_no < 0xff00 then
-          let pt_id = VInt.Int32 (Int32.of_int_exn pd.port_no) in
+          let pt_id = Int32.of_int_exn pd.port_no in
           PortUp(sw_id, pt_id)::acc
         else
           acc)))
@@ -172,7 +174,7 @@ let to_event w_out (t : t) evt =
       let open Net.Topology in
       let v  = vertex_of_label !(t.nib) (Async_NetKAT.Switch switch_id) in
       let ps = vertex_to_ports !(t.nib) v in
-      return (PortSet.fold (fun p acc -> (PortDown(switch_id, VInt.Int32 p))::acc)
+      return (PortSet.fold (fun p acc -> (PortDown(switch_id, p))::acc)
         ps [SwitchDown switch_id])
     | `Message (c_id, (xid, msg)) ->
       let open OpenFlow0x01.Message in
@@ -180,7 +182,7 @@ let to_event w_out (t : t) evt =
       begin match msg with
         | PacketInMsg pi ->
           let open OpenFlow0x01_Core in
-          let port_id = VInt.Int16 pi.port in
+          let port_id = Int32.of_int_exn pi.port in
           let buf_id, bytes = match pi.input_payload with
             | Buffered(n, bs) -> Some(n), bs
             | NotBuffered(bs) -> None, bs in
@@ -226,11 +228,11 @@ let to_event w_out (t : t) evt =
             | ChangeReason.Add, true
             | ChangeReason.Modify, true ->
               let pt_id = Int32.of_int_exn (ps.desc.OpenFlow0x01.PortDescription.port_no) in
-              return [PortUp(switch_id, VInt.Int32 pt_id)]
+              return [PortUp(switch_id, pt_id)]
             | ChangeReason.Delete, _
             | ChangeReason.Modify, false ->
               let pt_id = Int32.of_int_exn (ps.desc.OpenFlow0x01.PortDescription.port_no) in
-              return [PortDown(switch_id, VInt.Int32 pt_id)]
+              return [PortDown(switch_id, pt_id)]
             | _ ->
               return []
           end

async/discovery.ml
@@ -87,9 +87,8 @@ module Switch = struct
     let to_out switch_id port_id =
       let bytes = Packet.marshal
         Probe.(to_packet { switch_id; port_id = port_id }) in
-      let port_id' = VInt.Int32 port_id in
-      let action = SDN_Types.OutputPort(port_id') in
-      (switch_id, bytes, None, Some(port_id'), [action])
+      let action = SDN_Types.OutputPort(port_id) in
+      (switch_id, bytes, None, Some(port_id), [action])
 
     let loop t =
       Deferred.repeat_until_finished () (fun () ->
@@ -202,8 +201,8 @@ module Switch = struct
           let t, v2 = add_vertex t (Switch probe.switch_id) in
           let t, _  = add_edge t v1 port_id () v2 probe.port_id in
           let t, _  = add_edge t v2 probe.port_id () v1 port_id in
-          let e1 = (switch_id, VInt.Int32 port_id) in
-          let e2 = (probe.switch_id, VInt.Int32 probe.port_id) in
+          let e1 = (switch_id, port_id) in
+          let e2 = (probe.switch_id, probe.port_id) in
           Ctl.send_event ctl (LinkUp (e1, e2));
           t in
 
@@ -218,10 +217,9 @@ module Switch = struct
             then raise (Assertion_failed (Printf.sprintf
                 "Discovery.handler: not listening to pipe \"%s\"" p));
           let open Packet in
-          let pt_id' = VInt.get_int32 pt_id in
           begin match parse bytes with
             | { nw = Unparsable (dlTyp, bytes) } ->
-              t := handle_probe !t sw_id pt_id' (Probe.parse bytes)
+              t := handle_probe !t sw_id pt_id (Probe.parse bytes)
             | _ -> ();
           end;
           return None
@@ -235,39 +233,37 @@ module Switch = struct
           t := remove_vertex !t (vertex_of_label !t (Switch sw_id));
           return None
         | PortUp (sw_id, pt_id) ->
-          let pt_id' = VInt.get_int32 pt_id in
-          Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port = %lu }" sw_id pt_id';
-          Ctl.add ctl sw_id pt_id'
+          Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port = %lu }" sw_id pt_id;
+          Ctl.add ctl sw_id pt_id
           >>| fun () ->
             None
         | PortDown (sw_id, pt_id) ->
-          let pt_id' = VInt.get_int32 pt_id in
-          Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port = %lu }" sw_id pt_id';
-          ignore (Ctl.remove ctl sw_id pt_id');
+          Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port = %lu }" sw_id pt_id;
+          ignore (Ctl.remove ctl sw_id pt_id);
           let v = vertex_of_label !t (Switch sw_id) in
-          let mh = next_hop !t v pt_id' in
+          let mh = next_hop !t v pt_id in
           begin match mh with
             | None -> ()
             | Some(e) ->
               let (v2, pt_id2) = edge_dst e in
               begin match vertex_to_label !t v2 with
                 | Switch(sw_id2) ->
-                  t := remove_endpoint !t (v, pt_id');
+                  t := remove_endpoint !t (v, pt_id);
                   Ctl.send_event ctl
-                    (LinkDown((sw_id, pt_id), (sw_id2, VInt.Int32 pt_id2)))
+                    (LinkDown((sw_id, pt_id), (sw_id2, pt_id2)))
                 | Host _ -> ()
               end
           end;
           return None
         | LinkUp ((sw1, pt1), (sw2, pt2)) ->
           Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port %lu }, { switch = %Lu; port = %lu }"
-            sw1 (VInt.get_int32 pt1)
-            sw2 (VInt.get_int32 pt2);
+            sw1 pt1
+            sw2 pt2;
           return None
         | LinkDown ((sw1, pt1), (sw2, pt2)) ->
           Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port %lu }, { switch = %Lu; port = %lu }"
-            sw1 (VInt.get_int32 pt1)
-            sw2 (VInt.get_int32 pt2);
+            sw1 pt1
+            sw2 pt2;
           return None
         | HostUp _
         | HostDown _
@@ -292,7 +288,6 @@ module Host = struct
       let open Net.Topology in
       match e with
         | PacketIn (_, sw_id, pt_id, bytes, len, buf) ->
-          let pt_id' = VInt.get_int32 pt_id in
           let open Packet in
           let dlAddr, nwAddr = match parse bytes with
             | { nw = Arp (Arp.Query(dlSrc, nwSrc, _ )) }
@@ -301,22 +296,21 @@ module Host = struct
             | _ -> assert false in
           let h = try Some(vertex_of_label !t (Host(dlAddr, nwAddr)))
             with _ ->  None in
-          begin match Switch.Ctl.is_pending ctl sw_id pt_id', h with
+          begin match Switch.Ctl.is_pending ctl sw_id pt_id, h with
             | true, None    ->
               let t', h = add_vertex !t (Host(dlAddr, nwAddr)) in
               let t', s = add_vertex t' (Switch sw_id) in
-              let t', _ = add_edge t' s pt_id' () h 0l in
-              let t', _ = add_edge t' h 0l () s pt_id' in
+              let t', _ = add_edge t' s pt_id () h 0l in
+              let t', _ = add_edge t' h 0l () s pt_id in
               t := t';
               Switch.Ctl.send_event ctl (HostUp((sw_id, pt_id), (dlAddr, nwAddr)));
               return None
             | _   , _       -> return None
           end
         | PortDown (sw_id, pt_id) ->
-          let pt_id' = VInt.get_int32 pt_id in
-          ignore (Switch.Ctl.remove ctl sw_id pt_id');
+          ignore (Switch.Ctl.remove ctl sw_id pt_id);
           let v = vertex_of_label !t (Switch sw_id) in
-          let mh = next_hop !t v pt_id' in
+          let mh = next_hop !t v pt_id in
           (* Do not put the above line below the below line. The code needs to
            * read the current view of the network before modifying it. *)
           begin match mh with
@@ -326,7 +320,7 @@ module Host = struct
               begin match vertex_to_label !t v2 with
                 | Switch _ -> ()
                 | Host (dlAddr, nwAddr) ->
-                  t := remove_endpoint !t (v, pt_id');
+                  t := remove_endpoint !t (v, pt_id);
                   Switch.Ctl.send_event ctl
                     (HostDown((sw_id, pt_id), (dlAddr, nwAddr)))
               end
@@ -334,13 +328,13 @@ module Host = struct
           return None
         | HostUp ((sw_id, pt_id), (dlAddr, nwAddr)) ->
           Log.info ~tags "[topology.host] ↑ { switch = %Lu; port %lu }, { ip = %s; mac = %s }"
-            sw_id (VInt.get_int32 pt_id)
+            sw_id pt_id
             (Packet.string_of_ip nwAddr)
             (Packet.string_of_mac dlAddr);
           return None
         | HostDown ((sw_id, pt_id), (dlAddr, nwAddr)) ->
           Log.info ~tags "[topology.host] ↓ { switch = %Lu; port %lu }, { ip = %s; mac = %s }"
-            sw_id (VInt.get_int32 pt_id)
+            sw_id pt_id
             (Packet.string_of_ip nwAddr)
             (Packet.string_of_mac dlAddr);
           return None

async/learning.ml
@@ -20,7 +20,7 @@ let create () =
       false
     else begin
       Log.info ~tags "[learning] switch %Lu: learn %s => %Lu@%lu"
-        switch_id (Packet.string_of_mac ethSrc) switch_id (VInt.get_int32 port_id);
+        switch_id (Packet.string_of_mac ethSrc) switch_id port_id;
       state := SwitchMap.add !state switch_id (MacMap.add mac_map ethSrc port_id);
       true
     end in
@@ -38,7 +38,7 @@ let create () =
       | Some(p) ->
         Log.of_lazy ~tags ~level:`Info (lazy (Printf.sprintf
           "[learning] switch %Lu: port %lu %s"
-              switch_id (VInt.get_int32 p) (Packet.to_string packet)));
+              switch_id p (Packet.to_string packet)));
         OutputPort p in
 
   let default = Mod(Location(Pipe "learn")) in
@@ -49,7 +49,7 @@ let create () =
       let known, unknown_pred = MacMap.fold mac_map ~init:(drop, True)
         ~f:(fun ~key:mac ~data:port (k, u) ->
           let k' = Union(Seq(Filter(Test(EthDst mac)),
-                             Mod(Location(Physical (VInt.get_int32 port)))),
+                             Mod(Location(Physical port))),
                          k) in
           let u' = And(Neg(Test(EthDst mac)), u) in
           (k', u')) in

frenetic/netkat_test.ml
@@ -23,7 +23,6 @@ let _  =
 
 module Example = struct
   open SDN_Types
-  open VInt
   open NetKAT_Types
 
   (* End to end connectivity via mac addresses routing, hard coded for a tree topo using 4 nodes *)

lib/NetKAT_Lexer.mll
@@ -3,7 +3,7 @@
   open NetKAT_Parser
 
   let parse_byte str = Int64.of_string ("0x" ^ str)
-  let parse_decbyte str = Int64.of_string str
+  let parse_decbyte str = Int32.of_string str
 }
 
 let blank = [ ' ' '\t'  ]
@@ -51,11 +51,6 @@ rule token = parse
   | "ipProto" { PROTOCOLTYPE }
   | "tcpSrcPort" { TCPSRCPORT }
   | "tcpDstPort" { TCPDSTPORT }
-  | "arp" { ARP }
-  | "ip" { IP }
-  | "icmp" { ICMP }
-  | "tcp" { TCP }
-  | "udp" { UDP }
   | "begin" { BEGIN }
   | "end" { END }
   | "if" { IF }
@@ -87,15 +82,43 @@ rule token = parse
   | float_ as f { FLOAT (float_of_string f) }
 
   | (decbyte as b4) "." (decbyte as b3) "." (decbyte as b2) "." (decbyte as b1)
-    { let open Int64 in
+    { let open Int32 in
       IPADDR 
         (logor (shift_left (parse_decbyte b4) 24)
            (logor (shift_left (parse_decbyte b3) 16)
               (logor (shift_left (parse_decbyte b2) 8)
                  (parse_decbyte b1)))) }
 
-  | decimal as n { Scanf.sscanf n "%Lu" (fun i -> INT64 i) }
-  | hex as n { Scanf.sscanf n "0x%Lx" (fun i -> INT64 i) }
+  | decimal as n
+    { Scanf.sscanf n "%Lu" (fun i ->
+        if i >= 0L && i <= 0xffffffffL then
+          let i = Int64.to_int32 i in
+          if i <= 0xffl then
+            INT8 i
+          else if i <= 0xffffl then
+            INT16 i
+          else 
+            INT32 i
+        else if i >= 0L && i < 0xffffffffffffL then
+          INT48 i
+        else
+          INT64 i)
+    }
+  | hex as n
+    { Scanf.sscanf n "0x%Lx" (fun i ->
+        if i >= 0L && i <= 0xffffffffL then
+          let i = Int64.to_int32 i in
+          if i <= 0xffl then
+            INT8 i
+          else if i <= 0xffffl then
+            INT16 i
+          else 
+            INT32 i
+        else if i >= 0L && i < 0xffffffffffffL then
+          INT48 i
+        else
+          INT64 i)
+    }
   | '"' (string_body as s) '"' { STRING s }
   | id as s { IDENT s }
 

commit 24948510309d71c0419d9b0763e667e6ad95245c

flowterp: fix bug/typo in flowtable generation
Copy/paste bug introduced in 9f61484d.

lib/NetKAT_LocalCompiler.ml
@@ -945,7 +945,7 @@ module RunTime = struct
           ; SDN_Types.nwDst = x.HOV.ipDst
           ; SDN_Types.nwProto = x.HOV.ipProto
           ; SDN_Types.tpSrc = x.HOV.tcpSrcPort
-          ; SDN_Types.tpDst = x.HOV.tcpSrcPort
+          ; SDN_Types.tpDst = x.HOV.tcpDstPort
           ; SDN_Types.inPort = default_port }
         in
         simpl_flow pattern actions) in