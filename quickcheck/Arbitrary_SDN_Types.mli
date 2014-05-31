b6411875e3e7300e14ba2ca95c1451b6fd163d59
Spiros Eliopoulos <seliopou@gmail.com>
2014-05-31 16:44:35 -0400

    lattice: add arbitrary instance for Pattern.t

--- quickcheck/Arbitrary_SDN_Types.mli
+++ quickcheck/Arbitrary_SDN_Types.mli
@@ -5,3 +5,5 @@ val arbitrary_bytes : int -> Cstruct.t arbitrary
 val arbitrary_bufferId : SDN_Types.bufferId arbitrary
 
 val arbitrary_payload : SDN_Types.payload arbitrary
+
+val arbitrary_pattern : SDN_Types.Pattern.t arbitrary
