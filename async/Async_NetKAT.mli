76bd942c64696104f571978395da0cc3df148dc4
Spiros Eliopoulos <seliopou@gmail.com>
2014-09-10 11:57:25 -0400

    filter: constructor for asyncronous Pred.t updates

--- async/Async_NetKAT.mli
+++ async/Async_NetKAT.mli
@@ -24,8 +24,10 @@ module Pred : sig
   type t
 
   type handler = Net.Topology.t -> event -> pred option Deferred.t
+  type async_handler = Net.Topology.t -> pred Pipe.Writer.t -> event -> pred option Deferred.t
 
   val create : pred -> handler -> t
+  val create_async : pred -> async_handler -> t
 
   val create_static : pred -> t
   val create_from_string : string -> t
