0600763314db2edb923c43546739fae3aef6453f
Spiros Eliopoulos <seliopou@gmail.com>
2014-09-12 10:48:03 -0400

    daft_punk: reimplement switch and link and discovery

--- async/discovery.ml
+++ async/discovery.ml
@@ -110,209 +110,166 @@ module Switch = struct
       }
   end
 
-  module SwitchMap = Map.Make(Int64)
-
-  module Ctl = struct
-    module PortSet = Async_NetKAT.Net.Topology.PortSet
-
-    type t = {
-      r_evts : NetKAT_Types.event Pipe.Reader.t;
-      (* NB: Do not write with pushback to the event pipe. These events will
-       * evetually be fed back into this app, so blocking on the write may cause
-       * a deadlock.
-       * *)
-      w_evts : NetKAT_Types.event Pipe.Writer.t;
-      w_outs : (SDN_Types.switchId * SDN_Types.pktOut) Pipe.Writer.t;
-      mutable pending : PortSet.t SwitchMap.t;
-      mutable probe : bool;
+  type t =
+    { (* *)
+      flip_ctl : Flip.t;
+      (* *)
+      flip_app : Async_NetKAT.Pred.t;
+      (* *)
+      edge_app : (Net.Topology.t ref, (SDN_Types.switchId * SDN_Types.portId) list) Async_NetKAT.Raw.t;
+      (* *)
+      pkt_outs : (SDN_Types.switchId * SDN_Types.pktOut) Pipe.Writer.t;
+      (* *)
+      mutable probe_period : Time.Span.t;
+      (* *)
       mutable loop : unit Deferred.t
     }
 
-    let to_out switch_id port_id =
-      let bytes = Packet.marshal
-        Probe.(to_packet { switch_id; port_id = port_id }) in
-      let action = SDN_Types.(Output(Physical(port_id))) in
-      (switch_id, (SDN_Types.NotBuffered(bytes), Some(port_id), [action]))
+  let enabled t =
+    t.flip_ctl.Flip.enabled
 
-    let loop t =
-      Deferred.repeat_until_finished () (fun () ->
-        if t.probe then
-          Clock.after (Time.Span.of_sec 3.0)
-          >>= fun () ->
-          let outs = SwitchMap.fold t.pending ~init:[] ~f:(fun ~key ~data acc ->
-            PortSet.fold (fun pt_id acc -> (to_out key pt_id):: acc) data acc) in
-          Deferred.List.iter outs ~f:(Pipe.write t.w_outs)
-          >>| fun _ -> `Repeat ()
-        else
-          return (`Finished ()))
+  let to_out switch_id port_id =
+    let bytes = Packet.marshal
+      Probe.(to_packet { switch_id; port_id = port_id }) in
+    let action = SDN_Types.(Output(Physical(port_id))) in
+    (switch_id, (SDN_Types.NotBuffered(bytes), Some(port_id), [action]))
 
-    let send_event t evt =
-      (* NB: See the deadlock comment above. *)
-      Pipe.write_without_pushback t.w_evts evt
+  let loop t =
+    Deferred.repeat_until_finished () (fun () ->
+      if t.flip_ctl.Flip.enabled then
+        Clock.after t.probe_period
+        >>= fun () ->
+        print_endline "IM IN THE LOOP\n";
+        Deferred.List.iter (Async_NetKAT.default t.edge_app) ~f:(fun (sw_id, pt_id) ->
+          Pipe.write t.pkt_outs (to_out sw_id pt_id))
+        >>| fun _ -> `Repeat ()
+      else
+        return (`Finished ()))
 
-    let events t =
-      t.r_evts
+  let handle_probe (t:Net.Topology.t) switch_id port_id probe =
+    let open Probe in
+    let open Net.Topology in
+    let open Async_NetKAT in
+    (* It is possible for the controller to receive PacketIn events from a
+     * switch before its probes are sent. It however should not be possible to
+     * get a PacketIn after a switch has disconnect. *)
+    let v = vertex_of_label t (Switch switch_id) in
+    match next_hop t v port_id with
+      | Some(e) ->
+        let v, port_id' = edge_dst e in
+        begin if not (probe.port_id = port_id') then
+          raise (Assertion_failed (Printf.sprintf
+                  "Discovery.handle_probe: probe inconsistent w/ topology"))
+        end;
+        t, []
+      | None ->
+        Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port %lu }, { switch = %Lu; port = %lu }"
+          switch_id       port_id
+          probe.switch_id probe.port_id;
+        let t, v1 = add_vertex t (Switch switch_id) in
+        let t, v2 = add_vertex t (Switch probe.switch_id) in
+        let t, _  = add_edge t v1 port_id () v2 probe.port_id in
+        let t, _  = add_edge t v2 probe.port_id () v1 port_id in
+        let e1 = (switch_id, port_id) in
+        let e2 = (probe.switch_id, probe.port_id) in
+        t, [NetKAT_Types.LinkUp(e1, e2)]
 
-    let add t switch_id port_id =
-      t.pending <- SwitchMap.change t.pending switch_id (function
-        | None -> Some(PortSet.singleton port_id)
-        | Some(ports) -> Some(PortSet.add port_id ports));
-      Pipe.write t.w_outs (to_out switch_id port_id)
-
-    let remove t switch_id port_id =
-      let removed = ref true in
-      t.pending <- SwitchMap.change t.pending switch_id (function
-        | None -> removed := false; None
-        | Some(ports) -> Some(PortSet.remove port_id ports));
-      !removed
-
-    let remove_switch t switch_id =
-      t.pending <- SwitchMap.remove t.pending switch_id
-
-    let is_pending t switch_id port_id =
-      match SwitchMap.find t.pending switch_id with
-        | None -> false
-        | Some(ps) -> PortSet.mem port_id ps
-
-    let pause t =
-      t.probe <- false;
-      t.loop
-      >>| fun () ->
-        t.loop <- return ()
-
-    let resume t =
-      t.probe <- true;
-      t.loop <- loop t
-
-    let create w_outs =
-      let r_evts, w_evts = Pipe.create () in
-      let t = {
-        r_evts;
-        w_evts;
-        w_outs;
-        pending = SwitchMap.empty;
-        probe = false;
-        loop = return ()
-      } in
-      resume t;
-      t
-  end
-
-  let pred =
-    NetKAT_Types.(Test(EthSrc Probe.mac))
-
-  let guard app =
-    Async_NetKAT.guard (NetKAT_Types.Neg pred) app
-
-  let create () =
+  let stage t nib e =
+    let open Net.Topology in
     let open NetKAT_Types in
     let open Async_NetKAT in
-    (* The default and static policy. This should be installed on all switches
-     * and never change. *)
-    let default = Seq(Filter(pred), Mod(Location(Pipe("probe")))) in
-    (* A pipe for packet_outs *)
-    let o_r, o_w = Pipe.create () in
+    match e with
+      | PacketIn("probe", sw_id, pt_id, payload, len) when enabled t ->
+        let open Packet in
+        begin match parse (SDN_Types.payload_bytes payload) with
+          | { nw = Unparsable (dlTyp, bytes) } when dlTyp = Probe.protocol ->
+            let nib', es =  handle_probe !nib sw_id pt_id (Probe.parse bytes) in
+            nib := nib';
+            return es
+          | _ -> return []
+        end
+      | SwitchUp sw_id ->
+        Log.info ~tags "[topology.switch] ↑ { switch = %Lu }" sw_id;
+        nib := fst (add_vertex !nib (Switch sw_id));
+        return [e]
+      | SwitchDown sw_id ->
+        Log.info ~tags "[topology.switch] ↓ { switch = %Lu }" sw_id;
+        nib := remove_vertex !nib (vertex_of_label !nib (Switch sw_id));
+        return [e]
+      | PortUp (sw_id, pt_id) ->
+        Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port = %lu }" sw_id pt_id;
+        nib := add_port !nib (vertex_of_label !nib (Switch sw_id)) pt_id;
+        Pipe.write t.pkt_outs (to_out sw_id pt_id)
+        >>| fun () -> [e]
+      | PortDown (sw_id, pt_id) ->
+        Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port = %lu }" sw_id pt_id;
+        let v = vertex_of_label !nib (Switch sw_id) in
+        let mh = next_hop !nib v pt_id in
+        let es = begin match mh with
+          | None -> []
+          | Some(edge) ->
+            let (v2, pt_id2) = edge_dst edge in
+            begin match vertex_to_label !nib v2 with
+              | Switch(sw_id2) ->
+                nib := remove_endpoint !nib (v, pt_id);
+                Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port %lu }, { switch = %Lu; port = %lu }"
+                  sw_id  pt_id
+                  sw_id2 pt_id2;
+                [LinkDown((sw_id, pt_id), (sw_id2, pt_id2))]
+              | Host _ -> []
+            end
+        end in
+        return es
+      | LinkUp ((sw1, pt1), (sw2, pt2)) ->
+        assert false
+      | LinkDown ((sw1, pt1), (sw2, pt2)) ->
+        assert false
+      | PacketIn _
+      | HostUp _
+      | HostDown _
+      | Query _ ->
+        return [e]
 
-    (* Stores state for switch topology discovery, and manages the logical
-     * thread that sends probes to unknown ports *)
-    let ctl = Ctl.create o_w in
+  let create () =
+    let open Async_NetKAT in
+    let open NetKAT_Types in
+    let flip_ctl, flip_app =
+      Flip.create (And(Test(EthType Probe.protocol), (Test(EthSrc Probe.mac))))
+    in
+    let r_pkt_outs, pkt_outs = Pipe.create () in
+    let edge_app = Raw.create [] (fun nib send () ->
+      Deferred.don't_wait_for (Pipe.transfer_id r_pkt_outs send);
+      function
+      | PacketIn _
+      | Query _ -> return None
+      | _       -> return (Some (TUtil.edge !nib)))
+    in
+    let probe_period = Time.Span.of_sec 3.0 in
+    let t = { flip_ctl; flip_app; edge_app; pkt_outs; probe_period; loop = return () } in
+    let app =
+      ap ~how:`Parallel
+        (lift (fun _ a -> a) edge_app)
+        (guard' flip_app (Policy.create_static (Mod(Location(Pipe "probe")))))
+    in
+    t, stage t, app
 
-    let handle_probe t switch_id port_id probe : Net.Topology.t =
-      let open Probe in
-      let open Net.Topology in
-      begin if not (Ctl.remove ctl probe.switch_id probe.port_id) then
-        raise (Assertion_failed (Printf.sprintf
-            "Discovery.handle_probe: unknown switch %Lu" probe.switch_id));
-      end;
-      (* It is possible for the controller to receive PacketIn events from a
-       * switch before its probes are sent. It however should not be possible to
-       * get a PacketIn after a switch has disconnect. *)
-      ignore (Ctl.remove ctl switch_id port_id);
-      let v = vertex_of_label t (Switch switch_id) in
-      match next_hop t v port_id with
-        | Some(e) ->
-          let v, port_id' = edge_dst e in
-          begin if not (probe.port_id = port_id') then
-            raise (Assertion_failed (Printf.sprintf
-                    "Discovery.handle_probe: probe inconsistent w/ topology"))
-          end;
-          t
-        | None ->
-          let t, v1 = add_vertex t (Switch switch_id) in
-          let t, v2 = add_vertex t (Switch probe.switch_id) in
-          let t, _  = add_edge t v1 port_id () v2 probe.port_id in
-          let t, _  = add_edge t v2 probe.port_id () v1 port_id in
-          let e1 = (switch_id, port_id) in
-          let e2 = (probe.switch_id, probe.port_id) in
-          Ctl.send_event ctl (LinkUp (e1, e2));
-          t in
+  let managed_traffic t =
+    t.flip_app
 
-    let handler t w () : event -> policy option Deferred.t = fun e ->
-      (* Transfer all packet_outs from the Clt.t to the pipe passed to the
-       * handler. *)
-      Deferred.don't_wait_for (Pipe.transfer_id o_r w);
-      let open Net.Topology in
-      match e with
-        | PacketIn(p, sw_id, pt_id, payload, len) ->
-          if not (p = "probe")
-            then raise (Assertion_failed (Printf.sprintf
-                "Discovery.handler: not listening to pipe \"%s\"" p));
-          let open Packet in
-          begin match parse (SDN_Types.payload_bytes payload) with
-            | { nw = Unparsable (dlTyp, bytes) } when dlTyp = Probe.protocol ->
-              t := handle_probe !t sw_id pt_id (Probe.parse bytes)
-            | _ -> ();
-          end;
-          return None
-        | SwitchUp sw_id ->
-          Log.info ~tags "[topology.switch] ↑ { switch = %Lu }" sw_id;
-          t := fst (add_vertex !t (Switch sw_id));
-          return None
-        | SwitchDown sw_id ->
-          Log.info ~tags "[topology.switch] ↓ { switch = %Lu }" sw_id;
-          Ctl.remove_switch ctl sw_id;
-          t := remove_vertex !t (vertex_of_label !t (Switch sw_id));
-          return None
-        | PortUp (sw_id, pt_id) ->
-          Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port = %lu }" sw_id pt_id;
-          Ctl.add ctl sw_id pt_id
-          >>| fun () ->
-            None
-        | PortDown (sw_id, pt_id) ->
-          Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port = %lu }" sw_id pt_id;
-          ignore (Ctl.remove ctl sw_id pt_id);
-          let v = vertex_of_label !t (Switch sw_id) in
-          let mh = next_hop !t v pt_id in
-          begin match mh with
-            | None -> ()
-            | Some(e) ->
-              let (v2, pt_id2) = edge_dst e in
-              begin match vertex_to_label !t v2 with
-                | Switch(sw_id2) ->
-                  t := remove_endpoint !t (v, pt_id);
-                  Ctl.send_event ctl
-                    (LinkDown((sw_id, pt_id), (sw_id2, pt_id2)))
-                | Host _ -> ()
-              end
-          end;
-          return None
-        | LinkUp ((sw1, pt1), (sw2, pt2)) ->
-          Log.info ~tags "[topology.switch] ↑ { switch = %Lu; port %lu }, { switch = %Lu; port = %lu }"
-            sw1 pt1
-            sw2 pt2;
-          return None
-        | LinkDown ((sw1, pt1), (sw2, pt2)) ->
-          Log.info ~tags "[topology.switch] ↓ { switch = %Lu; port %lu }, { switch = %Lu; port = %lu }"
-            sw1 pt1
-            sw2 pt2;
-          return None
-        | HostUp _
-        | HostDown _
-        | Query _ ->
-          return None in
+  let set_link_probe_period t probe_period =
+    t.probe_period <- probe_period
 
-    let app = Policy.create ~pipes:(PipeSet.singleton "probe") default handler in
-    (ctl, app)
+  let get_link_probe_period t =
+    t.probe_period
+
+  let start t =
+    Flip.enable t.flip_ctl
+    >>| fun () -> t.loop <- loop t
+
+  let stop t =
+    Deferred.all_ignore [ Flip.disable t.flip_ctl; t.loop ]
+    >>| fun () -> t.loop <- return ()
+
 end
 
 module Host = struct
@@ -398,22 +355,23 @@ type t =
   { (* *)
     host_ctl : Flip.t;
     (* *)
-    switch_ctl : Switch.Ctl.t;
+    switch_ctl : Switch.t;
 }
 
 let create () =
-  let switch_ctl, switch = Switch.create () in
+  let switch_ctl, switch_stage, switch = Switch.create () in
   let host_ctl, host_stage, host = Host.create () in
   let t = { host_ctl; switch_ctl } in
-  (t, host_stage, Async_NetKAT.union host switch)
+  let stage =
+    Async_OpenFlow.Stage.(host_stage >=> switch_stage)
+  in
+  (t, stage, Async_NetKAT.union host switch)
 
 let managed_traffic t =
-  Async_NetKAT.Pred.create_static NetKAT_Types.False
+  Switch.managed_traffic t.switch_ctl
 
 let start t =
-  Switch.Ctl.resume t.switch_ctl;
-  Host.start t.host_ctl
+  Deferred.all_unit [Switch.start t.switch_ctl; Host.start t.host_ctl]
 
 let stop t =
-  Switch.Ctl.pause t.switch_ctl
-  >>= fun () -> Host.stop t.host_ctl
+  Deferred.all_unit [Switch.stop t.switch_ctl; Host.stop t.host_ctl]
