079a22398475911aa58c91b45eb253e4fbbaaf61
Arjun Guha <arjun.guha@gmail.com>
2014-11-03 21:07:13 -0500

    depends on tornado

--- lang/py/setup.py
+++ lang/py/setup.py
@@ -4,5 +4,5 @@ setup(name='netkat',
       version='0.0.1',
       description='Python bindings for the NetKAT policy language and HTTP application API.',
       packages=['netkat'],
-      install_requires=['flask']
+      install_requires=['tornado']
 )
