1813e7fc186d8ca63a43d61847645050a0c73626
nbraga <nicholas.m.braga@gmail.com>
2014-11-04 23:09:14 -0500

    'Fragile' topology discovery without LLDP

--- lang/py/examples/discovery.py
+++ lang/py/examples/discovery.py
@@ -1,9 +1,10 @@
 import base64
-from ryu.lib.packet import packet
+from ryu.lib.packet import packet,ethernet,arp
 from netkat import webkat
 from netkat.syntax import *
 import networkx as nx
 from networkx.readwrite import json_graph
+from ryu.ofproto.ether import ETH_TYPE_ARP
 
 """Topology Discovery"""
 
@@ -20,6 +21,11 @@ from networkx.readwrite import json_graph
 
 PROBE_INTERVAL = 5
 
+# (nb): Arbitrarily assigned ethtype to (switch) discovery packets
+# I will replace this and instead use LLDP
+ETH_TYPE_DISCOVERY_PACKET = 0x3366
+
+# (nb): Could we make this a Graph or do we explicitly need directed edges?
 nib = nx.DiGraph()
 
 ##
@@ -33,7 +39,7 @@ def get_ethernet(pkt):
         if p.protocol_name == 'ethernet':
             return p
 
-# NetKAT policy that diverts packets to the controllerc
+# NetKAT policy that diverts packets to the controller
 def controller():
     return modify("port", "http")
 
@@ -41,29 +47,37 @@ def controller():
 # Policy
 ###
 def policy():
-    return (filter(test("ethTyp", 0x3366)) >> controller()) | (filter(test("ethTyp", 0x806)) >> controller())
+    return (filter(test("ethTyp", ETH_TYPE_DISCOVERY_PACKET)) >> controller()) | (filter(test("ethTyp", ETH_TYPE_ARP)) >> controller())
 
 ##
 # Emit Probes
 ##
 def probe():
+    print '=== PROBE ==='
     for node in nib.nodes():
-        for port in nib.node[node]['ports']:
-            print "sending probe",node,port
+        if nib.node[node]['device'] == 'switch':
+            for port in nib.node[node]['ports']:
+                # (nb): NOTE: Force node,port into dst,src fields of Ethernet packet. Temporary only!
+                dp = ethernet.ethernet(dst=node,src=port,ethertype=0x3366)
+                p = packet.Packet()
+                p.add_protocol(dp)
+                p.serialize()
+                webkat.pkt_out(node,port,p.data)
+    print nib.edges(data=True)
     return
 
 ##
 # Handle Events
 ##
 def handler(event):
-    print event
+    #print event
     typ = event['type']
     if typ  == 'switch_up':
         sw = event['switch_id']
         if nib.node.has_key(sw):
             pass
         else:
-            nib.add_node(sw)
+            nib.add_node(sw,device='switch')
             nib.node[sw]['ports'] = set()
     elif typ == 'port_up':
         nib.node[event['switch_id']]['ports'].add(event['port_id'])
@@ -75,11 +89,24 @@ def handler(event):
         pt = event['port_id']
         bits = base64.b64decode(event['payload']['buffer'])
         pkt = packet.Packet(bits)
+        p = get_ethernet(pkt)
+        if p.ethertype == ETH_TYPE_ARP:
+            # (nb): For now, assign a unique hostname concat of parent-switch:port
+            host_id = "h%s" % (str(sw) + ":" + str(pt))
+            nib.add_node(host_id,device='host')
+            nib.add_edge(sw,host_id,outport=pt,inport=0)
+            nib.add_edge(host_id,sw,outport=0,inport=pt)
+        elif p.ethertype == ETH_TYPE_DISCOVERY_PACKET:
+            # (nb): Remove ryu.packet's MAC string format
+            src_sw = int((p.dst).replace(':', ''), 16)
+            src_port = int((p.src).replace(':',''), 16)
+            nib.add_edge(src_sw,sw,outport=src_port,inport=pt)
+            nib.add_edge(sw,src_sw,outport=pt,inport=src_port)
         pass
     else:
         pass
 
-    print "NIB: %s" % json_graph.node_link_data(nib)
+    #print "NIB: %s" % json_graph.node_link_data(nib)
     return
 
 def main():
