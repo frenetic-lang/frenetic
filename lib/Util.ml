a15215b647d30a41a541bd8e805ebcada310a360
Arjun Guha <arjun.guha@gmail.com>
2015-01-08 04:53:51 -0800

    Removed dead code and compiler warnings with it

--- lib/Util.ml
+++ lib/Util.ml
@@ -20,34 +20,6 @@
  * below.
  *)
 
-let intercalate f s = function
-  | [] ->
-    ""
-  | h::t ->
-    List.fold_left (fun acc x -> acc ^ s ^ f x) (f h) t
-
-let load_file f =
-  let ic = open_in f in
-  let n = in_channel_length ic in
-  let s = String.create n in
-  really_input ic s 0 n;
-  close_in ic;
-  (s)
-
-let write_to_file filename text_to_write =
-  try
-    let cout = open_out filename in
-    let co = Format.formatter_of_out_channel cout in
-    Format.fprintf co "%s\n" text_to_write;
-    close_out cout
-  with Sys_error _ as e ->
-    Format.printf "Cannot open file \"%s\": %s\n" filename (Printexc.to_string e)
-
-
-let list_intercalate f sep l =
-  List.fold_left
-    (fun acc li -> Printf.sprintf "%s%s%s" acc (if acc = "" then "" else sep) (f li)) "" l
-
 let make_string_of formatter x =
   let open Format in
   let buf = Buffer.create 100 in
