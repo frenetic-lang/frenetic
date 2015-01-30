de96aad823ee6a93dc851524f173e8a6b8a7ad1f
Steffen Juilf Smolka <smolka@cs.cornell.edu>
2015-01-30 03:16:19 -0500

    more suggestive naming

--- lib/NetKAT_GlobalCompiler.ml
+++ lib/NetKAT_GlobalCompiler.ml
@@ -35,46 +35,46 @@ let rec tag_links (pol : policy) : policy =
   in
   tag pol
 
-(* splits policy into E and D policy *)
+(* splits policy into E, D, and K policy *)
 let rec split_pol (pol: policy) : policy * policy * policy =
   match pol with
   | Filter _ | Mod _ -> (pol, drop, drop)
   | Union (p,q) ->
-    let (e_p, d1_p, d2_p) = split_pol p in
-    let (e_q, d1_q, d2_q) = split_pol q in
-    (mk_union e_p e_q, mk_union d1_p d1_q, mk_union d2_p d2_q)
+    let (e_p, d_p, k_p) = split_pol p in
+    let (e_q, d_q, k_q) = split_pol q in
+    (mk_union e_p e_q, mk_union d_p d_q, mk_union k_p k_q)
   | Seq (p,q) ->
-    let (e_p, d1_p, d2_p) = split_pol p in
-    let (e_q, d1_q, d2_q) = split_pol q in
+    let (e_p, d_p, k_p) = split_pol p in
+    let (e_q, d_q, k_q) = split_pol q in
     let e = mk_seq e_p e_q in
     (* SJS: loss of precision!!! Sound but not optimal *)
     let e_p_ind = if e_p = drop then drop else id in
-    let d1 = mk_union d1_p (mk_seq e_p d1_q) in
-    let d2 = mk_union (mk_seq e_p_ind d2_q) (mk_seq d2_p q) in
+    let d = mk_union d_p (mk_seq e_p d_q) in
+    let k = mk_union (mk_seq e_p_ind k_q) (mk_seq k_p q) in
     (* inline fdds into policies to avoid duplication *)
-    (e, d1, d2)
+    (e, d, k)
   | Star p ->
-    let (e_p, d1_p, d2_p) = split_pol p in
+    let (e_p, d_p, k_p) = split_pol p in
     let e = mk_star e_p in
-    let d1 = mk_seq e d1_p in
-    let d2 = mk_seq d2_p (mk_union e d1) in
-    (e, d1, d2)
+    let d = mk_seq e d_p in
+    let k = mk_seq k_p (mk_union e d) in
+    (e, d, k)
   | Link (sw1,pt1,sw2,pt2) -> (drop, match_location sw1 pt1, match_location sw2 pt2)
 
 let compile (ingress : pred) (egress : pred) (p : policy) =
   let ingress = mk_filter ingress in
   let egress = mk_filter egress in
   let p = tag_links p in
-  let rec loop i e_acc d_acc p =
-    let (e, d1, d2) = split_pol p in
+  let rec loop e_acc d_acc p =
+    let (e, d, k) = split_pol p in
     let e_acc = mk_union e_acc e in
-    let d_acc = mk_union d_acc d1 in
-    if d2 = drop then
+    let d_acc = mk_union d_acc d in
+    if k = drop then
       mk_union d_acc (mk_seq e_acc egress)
     else
-      loop (i+1) e_acc d_acc d2
+      loop e_acc d_acc k
   in
-  loop 1 drop drop (mk_seq ingress p)
+  loop drop drop (mk_seq ingress p)
 
 let compile_pol ~(pol : policy) : policy = match pol with
   | Seq (Filter ingress, Seq (p, Filter egress)) -> compile ingress egress p
