fe5638c0094d171090b3d8f0e5889384ea7b4ec1
jcollard <josephmcollard@gmail.com>
2015-02-05 13:12:38 -0500

    Added pretty printer for flow tables.

--- frenetic/Shell.ml
+++ frenetic/Shell.ml
@@ -10,13 +10,14 @@ module Log = Async_OpenFlow.Log
 type showable =
   | Ordering
   | Policy
+  | FlowTable of (policy * string) option
+  | Help
 
 type command =
-  | Update of policy * string
+  | Update of (policy * string)
   | Order of LC.order
   | Show of showable
   | Exit
-  | Help
 
 module Parser = struct
 
@@ -63,17 +64,21 @@ module Parser = struct
       | Parsing.Parse_error ->
 	 Error (sprintf "error parsing policy at %s" (string_of_position lexbuf.lex_curr_p))
 
-    let update : (command, bytes list) MParser.t =
-      Tokens.symbol "update" >>
+    let policy' : ((policy * string), bytes list) MParser.t =
       many_until any_char eof >>=
 	(fun pol_chars ->
 	 let pol_str = String.of_char_list pol_chars in
 	 match parse_policy pol_str with
-	 | Ok pol -> return (Update (pol, pol_str))
-	 | Error msg -> fail msg)
+	 | Ok pol -> return (pol, pol_str)
+	 | Error msg -> fail msg)      
+
+    let update : (command, bytes list) MParser.t =
+      Tokens.symbol "update" >> 
+	policy' >>=
+	(fun pol -> return (Update pol))
 
     let help : (command, bytes list) MParser.t =
-      Tokens.symbol "help" >> return Help
+      Tokens.symbol "help" >> return (Show Help)
 
     let exit : (command, bytes list) MParser.t =
       Tokens.symbol "exit" >> return Exit
@@ -81,11 +86,18 @@ module Parser = struct
     let policy : (command, bytes list) MParser.t =
       Tokens.symbol "policy" >> return (Show Policy)
 
+    let flowtable : (command, bytes list) MParser.t =
+      Tokens.symbol "flow-table" >>
+	(eof >> return (Show (FlowTable None)) <|>
+	(policy' >>= 
+	   (fun pol -> return (Show (FlowTable (Some pol))))))
+
     let command : (command, bytes list) MParser.t =
       order <|>
 	update <|>
 	policy <|>
 	help <|>
+	flowtable <|>
 	exit
 end
 
@@ -127,9 +139,150 @@ let set_order (o : LC.order) : unit =
      Controller.set_order (uw !controller) (`Static new_order);
      print_order ()
 
-let policy : string ref = ref "drop"
+let policy : (policy * string) ref = ref (drop, "drop")
 let print_policy () =
-  printf "Current policy: %s\n%!" !policy
+  match !policy with
+    (_, p) -> printf "Current policy: %s\n%!" p
+
+module Table = struct
+    open SDN_Types
+    open Packet
+
+    let vlan (x : int) : string = 
+      Format.sprintf "Vlan = %d" x
+
+    let vlanpcp (x : dlVlanPcp) : string =
+      Format.sprintf "VlanPcp = %d" x
+
+    let ethType (x : dlTyp) : string =
+      let extra = if x = 0x800 then " (ip)" 
+		  else if x = 0x806 then " (arp)"
+		  else ""
+      in
+      Format.sprintf "EthType = 0x%x%s" x extra
+
+    let ipProto (x : nwProto) : string =
+      let extra = match x with
+	| 0x01 -> " (icmp)"
+	| 0x02 -> " (igmp)"
+	| 0x06 -> " (tcp)"
+	| 0x11 -> " (udp)"
+	| _ -> ""
+      in
+      Format.sprintf "ipProto = 0x%x%s" x extra
+
+    let ethSrc (x : dlAddr) : string =
+      Format.sprintf "EthSrc = %s" (Packet.string_of_mac x)
+
+    let ethDst (x : dlAddr) : string =
+      Format.sprintf "EthDst = %s" (Packet.string_of_mac x)
+
+    let ip4src (x : Pattern.Ip.t) : string =
+      Format.sprintf "IP4Src = %s" (Pattern.Ip.string_of x)
+
+    let ip4dst (x : Pattern.Ip.t) : string =
+      Format.sprintf "IP4Dst = %s" (Pattern.Ip.string_of x)
+
+    let tcpSrcPort (x : tpPort) : string =
+      Format.sprintf "TCPSrcPort = %d" x
+
+    let tcpDstPort (x : tpPort) : string =
+      Format.sprintf "TCPDstPort = %d" x
+
+    let inPort (x : portId) : string =
+      Format.sprintf "In Port = %lu" x
+
+    let check (string_of : 'a -> string) 
+	      (x : 'a option) 
+	      (acc : string list) : string list =
+      match x with
+      | None -> acc
+      | Some x' -> (string_of x') :: acc
+
+    let pattern_list (p : SDN_Types.Pattern.t) : string list =
+      check ethSrc p.dlSrc [] |>
+	check ethDst p.dlDst |>
+	check ethType p.dlTyp |>
+	check vlan p.dlVlan |>
+	check vlanpcp p.dlVlanPcp |>
+	check ip4src p.nwSrc |>
+	check ip4dst p.nwDst |>
+	check ipProto p.nwProto |>
+	check tcpSrcPort p.tpSrc |>
+	check tcpDstPort p.tpDst |>
+	check inPort p.inPort
+
+    let entry (f : SDN_Types.flow) : (string list) * (string list) =
+      let open SDN_Types in
+      let open List in
+      let pattern_list = pattern_list f.pattern in
+      let action_list = map (concat (concat f.action)) string_of_action in
+      (pattern_list, action_list)
+
+    let pad (len : int) (e : string) : string =
+      let padding_size = len - (String.length e) in
+      let padding = String.make padding_size ' ' in
+      String.concat [e; padding]
+
+    let unwrap x = 
+      match x with
+      | None -> 0
+      | Some x -> x
+
+    let table_size (entries : ((string list) * (string list)) list) : int * int =
+      let open List in
+      let patterns = map entries fst |> concat in
+      let actions = map entries snd |> concat in
+      let max_p =  max_elt (map patterns String.length) (-) |> unwrap in
+      let max_a = max_elt (map actions String.length) (-) |> unwrap in
+      (max max_p (String.length "Pattern"), max max_a (String.length "Action"))
+
+    let bar max_p max_a =
+      printf "|%s|\n" (String.make (max_p + max_a + 5) '-')
+
+    let title max_p max_a =
+      let pattern = pad max_p "Pattern" in
+      let action = pad max_a "Action" in
+      printf "| %s | %s |\n" pattern action
+
+    let print_entry (max_p : int) (max_a : int) (e : (string list) * (string list)) : unit =
+      let open List in
+      let padded_patterns = map (fst e) (pad max_p) in 
+      let padded_actions = map (snd e) (pad max_a) in 
+      let rec helper pats acts =
+	match pats, acts with
+	| [], [] -> ()
+	| (p::ps), [] -> 
+	   printf "| %s | %s |\n" p (String.make max_a ' ');
+	   helper ps []
+	| [], (a::rest) -> 
+	   printf "| %s | %s |\n" (String.make max_p ' ') a;
+	   helper [] rest
+	| (p::ps), (a::rest) -> 
+	   printf "| %s | %s |\n" p a;
+	   helper ps rest 
+      in 
+      bar max_p max_a;
+      helper padded_patterns padded_actions
+
+    let print (pol : (policy * string) option) : unit =
+      let (p, str) =
+	match pol with
+	| None -> !policy
+	| Some x -> x
+      in 
+      let bdd = LC.compile ~order:!order p in
+      let tbl = LC.to_table 0L bdd in
+      let entries = List.map tbl entry in
+      let (max_p, max_a) = table_size entries in
+      printf "Policy: %s\n%!" str;
+      bar max_p max_a;
+      title max_p max_a;
+      let _ = List.map entries (print_entry max_p max_a) in
+      bar max_p max_a;
+      printf "%!"
+
+end
 
 let parse_command (line : string) : command option = 
   match (MParser.parse_string Parser.command line []) with
@@ -151,9 +304,10 @@ let rec repl (pol_writer : policy Pipe.Writer.t) : unit Deferred.t =
 		     Shutdown.shutdown 0
 		  | Some (Show Ordering) -> print_order ()
 		  | Some (Show Policy) -> print_policy ()
-		  | Some Help -> print_help ()
+		  | Some (Show Help) -> print_help ()
+		  | Some (Show (FlowTable t)) -> Table.print t
 		  | Some (Update (pol, pol_str)) -> 
-		     policy := pol_str; 
+		     policy := (pol, pol_str); 
 		     Pipe.write_without_pushback pol_writer pol
 		  | Some (Order order) -> set_order order
 		  | None -> ()
