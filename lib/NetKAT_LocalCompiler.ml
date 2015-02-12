b761b976dc10828369b65e9b8c8bc0dd422af9c5
Arjun Guha <arjun.guha@gmail.com>
2015-02-12 08:23:10 -0800

    bugfix

--- lib/NetKAT_LocalCompiler.ml
+++ lib/NetKAT_LocalCompiler.ml
@@ -250,6 +250,8 @@ let opt_to_table sw_id t =
   in
   let rec next_table_row tests mk_rest t =
     match T.unget t with
+    | Branch ((Location, Pipe _), _, f) ->
+      next_table_row tests mk_rest f
     | Branch (test, t, f) ->
       next_table_row (test::tests) (fun t' -> mk_rest (mk_branch_or_leaf test t' f)) t
     | Leaf actions ->
@@ -270,6 +272,7 @@ let rec naive_to_table sw_id (t : T.t) =
   | Leaf actions ->
     let openflow_instruction = [to_action (get_inport tests) actions tests] in
     [mk_flow (to_pattern tests) openflow_instruction]
+  | Branch ((Location, Pipe _), _, fls) -> dfs tests fls
   | Branch (test, tru, fls) ->
     dfs (test :: tests) tru @ dfs tests fls in
   dfs [] t
