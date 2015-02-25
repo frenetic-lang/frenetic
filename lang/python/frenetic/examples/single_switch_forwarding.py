89b01f26d8874e95175dd2a4a67909e0933075ec
jcollard <josephmcollard@gmail.com>
2015-02-25 10:15:43 -0500

    Policy floods

--- lang/python/frenetic/examples/single_switch_forwarding.py
+++ lang/python/frenetic/examples/single_switch_forwarding.py
@@ -2,6 +2,16 @@ import frenetic
 from frenetic.syntax import *
 
 def policy(ps):
-    return Union(Mod(Location(Physical(p))) for p in range(1,ps+1))
+    vs = 0
+    for src in range(1,ps+1):
+        f = Filter(Test(Location(Physical(src))))
+        dst = filter(lambda x: x != src, range(1,ps+1))
+        mods = Union(Mod(Location(Physical(p))) for p in dst)
+        both = f >> mods
+        if vs == 0:
+            vs = both
+        else:
+            vs = vs | both
+    return vs
                      
                      
