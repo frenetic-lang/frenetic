542f50302e1a7afd655a2a595bb991f8e090a872
Arjun Guha <arjun@cs.umass.edu>
2015-03-09 19:03:50 -0400

    Added compile_pretty and fixed JSON flow-tables.

--- frenetic/Compile_Server.ml
+++ frenetic/Compile_Server.ml
@@ -7,34 +7,39 @@ open Common
 
 let policy = ref NetKAT_Types.drop
 
+let compile_respond pol =
+  (* Compile pol to tables and time everything. *)
+  let (time, tbls) = profile (fun () ->
+  let fdd = NetKAT_LocalCompiler.compile pol in
+  let sws =
+    let sws = NetKAT_Misc.switches_of_policy pol in
+    if List.length sws = 0 then [0L] else sws in
+  List.map sws ~f:(fun sw ->
+    (sw, NetKAT_LocalCompiler.to_table ~opt:false sw fdd))) in
+  (* JSON conversion is not timed. *)
+  let json_tbls = List.map tbls ~f:(fun (sw, tbl) ->
+  `Assoc [("switch_id", `Int (Int64.to_int_exn sw));
+         ("tbl", NetKAT_SDN_Json.flowTable_to_json tbl)]) in
+  let resp = Yojson.Basic.to_string ~std:true (`List json_tbls) in
+  let headers = Cohttp.Header.init_with
+  "X-Compile-Time" (Float.to_string time) in
+  Cohttp_async.Server.respond_with_string ~headers resp
+
 let handle_request
   ~(body : Cohttp_async.Body.t)
    (client_addr : Socket.Address.Inet.t)
    (request : Request.t) : Server.response Deferred.t =
   match request.meth, extract_path request with
+    | `POST, ["compile_pretty"] -> handle_parse_errors body
+        parse_update
+        compile_respond
     | `POST, ["compile"] ->
       printf "POST /compile";
       handle_parse_errors body
         (fun body ->
            Body.to_string body >>= fun str ->
            return (NetKAT_Json.policy_from_json_string str))
-        (fun pol ->
-           (* Compile pol to tables and time everything. *)
-           let (time, tbls) = profile (fun () ->
-             let fdd = NetKAT_LocalCompiler.compile pol in
-             let sws =
-               let sws = NetKAT_Misc.switches_of_policy pol in
-               if List.length sws = 0 then [0L] else sws in
-             List.map sws ~f:(fun sw ->
-               (sw, NetKAT_LocalCompiler.to_table sw fdd))) in
-           (* JSON conversion is not timed. *)
-           let json_tbls = List.map tbls ~f:(fun (sw, tbl) ->
-             `Assoc [("switch_id", `Int (Int64.to_int_exn sw));
-                     ("tbl", NetKAT_SDN_Json.flowTable_to_json tbl)]) in
-           let resp = Yojson.Basic.to_string ~std:true (`List json_tbls) in
-           let headers = Cohttp.Header.init_with
-             "X-Compile-Time" (Float.to_string time) in
-           Cohttp_async.Server.respond_with_string ~headers resp)
+        compile_respond
     | `POST, ["update"] ->
       printf "POST /update";
       handle_parse_errors body parse_update_json
