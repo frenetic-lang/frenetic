39ae4622b12233f185db9b616bf82ba61caeb42a
Nate Foster <jnfoster@cs.cornell.edu>
2015-04-22 19:45:08 -0400

    Add missing file

---
+++ lib/Frenetic_Bits.mli
@@ -0,0 +1,6 @@
+val clear_bit : int -> int32 -> int32
+val set_bit : int -> int32 -> int32
+val bit : int32 -> int -> bool -> int32
+val test_bit : int -> int32 -> bool
+val get_byte32 : int32 -> int -> int
+val get_byte : int64 -> int -> int
