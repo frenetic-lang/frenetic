289fb79efc52bc555831444a2644692db62703fe
jcollard <josephmcollard@gmail.com>
2015-08-13 09:59:03 -0400

    Revert "Merged GroupTable0x04 into lib directory."
    This reverts commit b8631725d070a5646efafe79fba6595ce61db89f.

--- openflow/lib/GroupTable0x04.mli
+++ openflow/lib/GroupTable0x04.mli
@@ -1,7 +1,6 @@
 (** Controller's representation of the group table configuration on an
     OpenFlow 1.3 switch. *)
 open Frenetic_OpenFlow0x04
-open Frenetic_Packet
 type t
 
 val create : unit -> t
@@ -18,10 +17,4 @@ val clear_groups : t -> unit
 
 (** Produces a list of messages to realize the previous [add_group] and
    [clear_groups] commands, upto the last [commit] command. *)
-
-val commit : t -> Message.t list
-
-(** Creates a new fast fail group which forwards out of the first live port in 
- * [ports] and otherwise drops. *)
-val add_fastfail_group : t -> portId list -> groupId
-
+val commit : t -> message list
