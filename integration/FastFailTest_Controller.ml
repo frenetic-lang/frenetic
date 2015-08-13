9954a26fea3d747bf8f5331848af2b8a634ee189
jcollard <josephmcollard@gmail.com>
2015-08-13 15:39:37 -0400

    Switched to destination routing/

--- integration/FastFailTest_Controller.ml
+++ integration/FastFailTest_Controller.ml
@@ -8,7 +8,7 @@ open Frenetic_NetKAT
 open Frenetic_NetKAT_Pretty
 
 let polAtoB = 
-  let src_a = Filter (Test (EthSrc 1L)) in
+  let src_a = Filter (Test (EthDst 2L)) in
   let sw1 = Seq ((Filter (Test (Switch 1L))), (Mod (Location (FastFail [1l;2l])))) in
   let sw2 = Seq ((Filter (Test (Switch 2L))), (Mod (Location (Physical 1l)))) in
   let sw3 = Seq ((Filter (Test (Switch 3L))), (Mod (Location (Physical 1l)))) in
@@ -16,7 +16,7 @@ let polAtoB =
   (Seq (src_a, (Union (sw1, (Union (sw2, (Union (sw3, sw4)))))))) 
 
 let polBtoA = 
-  let src_b = Filter (Test (EthSrc 2L)) in
+  let src_b = Filter (Test (EthDst 1L)) in
   let sw4 = Seq ((Filter (Test (Switch 4L))), (Mod (Location (Physical 2l)))) in
   let sw3 = Seq ((Filter (Test (Switch 3L))), (Mod (Location (Physical 2l)))) in
   let sw1 = Seq ((Filter (Test (Switch 1L))), (Mod (Location (Physical 3l)))) in
