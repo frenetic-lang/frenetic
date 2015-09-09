1ce99f9603a77588c891a813ab9eefe59ffbed91
Steffen Juilf Smolka <smolka@cs.cornell.edu>
2015-09-09 00:58:11 -0400

    undo accidental renaming

--- lib_test/Test.ml
+++ lib_test/Test.ml
@@ -1,7 +1,7 @@
 (* Write tests in independent modules, then just include them here to run them *)
 open Test_Frenetic_Bits
 open Test_Frenetic_NetKAT
-open Test_Frenetic_NetKAT_Compiler
+open Test_Frenetic_NetKAT_Local_Compiler
 open Test_Frenetic_NetKAT_Pretty
 open Test_Frenetic_Network
 open Test_Frenetic_Packet
