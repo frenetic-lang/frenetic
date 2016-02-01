58dc440054255c0a58c26ea0a36de9a64a8b334e
Steffen Smolka <smolka@cs.cornell.edu>
2016-02-01 12:02:04 -0800

    fixed tswitch example, improved  scripts, added README

--- examples/virtual/1/run.sh
+++ examples/virtual/1/run.sh
@@ -6,6 +6,6 @@ for arg in "g_fabric" "g_pruned" "pg" "vg" "g_raw"
 do 
   echo $arg
   arg1="${arg}.dot"
-  arg2="${arg}.png"
-  dot -Tpng $arg1 > $arg2
+  arg2="${arg}.svg"
+  dot -Tsvg $arg1 > $arg2
 done
