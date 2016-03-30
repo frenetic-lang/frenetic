c05319ccba3645aae49a5de780d063d1099c4c21
Dhruv Singhal <dhruv@singhal.me>
2016-03-30 16:50:26 -0700

    Added discoveryapp MLI.

---
+++ async/discoveryapp.mli
@@ -0,0 +1,20 @@
+open Core.Std
+open Async.Std
+open Frenetic_NetKAT
+open Cohttp_async
+module Log = Frenetic_Log
+module Net = Frenetic_NetKAT_Net.Net
+
+module Discovery : sig
+  type t = {
+    nib : Net.Topology.t ref;
+    policy : policy;
+  }
+
+  val start : event Pipe.Reader.t -> (policy -> unit Deferred.t) ->
+    (switchId -> Frenetic_OpenFlow.pktOut -> unit Deferred.t) -> t
+
+  val start_server : int -> policy -> unit
+
+  val get_policy : t -> policy
+end
