418e3513c5b29c9be69d50c4887cc61abab0a64e
Shrutarshi Basu <shr@basus.me>
2016-04-21 15:41:35 -0400

    Streamline parser

--- async/Frenetic_Autoshell.ml
+++ async/Frenetic_Autoshell.ml
@@ -6,120 +6,114 @@ open Frenetic_Network
 module Compiler = Frenetic_NetKAT_Compiler
 module Log = Frenetic_Log
 
+type source =
+  | String of string
+  | Filename of string
+
+type element =
+  | Policy   of policy
+  | Topology of Net.Topology.t
+  | Fabric   of (switchId, Frenetic_OpenFlow.flowTable) Hashtbl.t
+
+type input =
+  | IPolicy
+  | ITopology
+  | IFabric
+
 type command =
-  | Policy of string
-  | Net of string
+  | Load of (input * source)
   | Compile
   | Fabric
   | Write of string
-  | Speckat
   | Exit
 
-
 module Parser = struct
+
+  (** Monadic Parsers for the command line *)
   open MParser
 
   module Tokens = MParser_RE.Tokens
 
+  let symbol = Tokens.symbol
+
+  (* Parser for sources *)
+  let source : (source, bytes list) MParser.t =
+    (char '"' >> many_chars_until any_char (char '"') >>=
+     (fun string -> return ( String string) ) ) <|>
+    (many_until any_char eof >>=
+     (fun w -> return (Filename (String.of_char_list w))))
+
+  (* Parser for the load command *)
+  let loadable : (command, bytes list) MParser.t =
+    symbol "load" >> (
+      (symbol "policy"   >> source >>= (fun s -> return (Load (IPolicy, s)))) <|>
+      (symbol "topology" >> source >>= (fun s -> return (Load (ITopology, s)))) <|>
+      (symbol "fabric"   >> source >>= (fun s -> return (Load (IFabric, s)))))
+
+  (* Parser for the compile command *)
+  let compile : (command, bytes list) MParser.t =
+    symbol "compile" >> return Compile
+
+  (* Parser for the fabric command *)
+  let fabric : (command, bytes list) MParser.t =
+    symbol "fabric" >> return Fabric
+
+  (* Parser for the write command *)
+  let write : (command, bytes list) MParser.t =
+    symbol "write" >>
+      many_until any_char eof >>=
+      (fun filename -> return (Write (String.of_char_list filename)))
+
+  (* Parser for the exit command *)
+  let exit : (command, bytes list) MParser.t =
+    (symbol "exit" <|> symbol "quit") >> return Exit
+
+  let command : (command, bytes list) MParser.t =
+    loadable <|>
+    compile  <|>
+    fabric   <|>
+    write    <|>
+    exit
+
+  (** Non-Monadic parsers for the information that the shell can
+  manipulate. Mostly just wrappers for parsers from the rest of the Frenetic
+  codebase. *)
+
   (* Use the netkat parser to parse policies *)
-  let parse_policy (pol_str : string) : (policy, string) Result.t =
+  let policy (pol_str : string) : (element, string) Result.t =
     try
-      Ok (Frenetic_NetKAT_Parser.policy_of_string pol_str)
+      Ok (Policy (Frenetic_NetKAT_Parser.policy_of_string pol_str))
     with Camlp4.PreCast.Loc.Exc_located (error_loc,x) ->
       Error (sprintf "Error: %s\n%s"
                (Camlp4.PreCast.Loc.to_string error_loc)
                (Exn.to_string x))
 
-  (* Parser for the policy command *)
-  let policy: (command, bytes list) MParser.t =
-    Tokens.symbol "policy" >>
-      many_until any_char eof >>=
-      (fun filename -> return (Policy (String.of_char_list filename)))
-
-  (* Parser for the net command *)
-  let net: (command, bytes list) MParser.t =
-    Tokens.symbol "net" >>
-      many_until any_char eof >>=
-      (fun filename -> return (Net (String.of_char_list filename)))
-
-  (* Parser for the compile command *)
-  let compile : (command, bytes list) MParser.t =
-    Tokens.symbol "compile" >> return Compile
-
-  (* Parser for the fabric command *)
-  let fabric : (command, bytes list) MParser.t =
-    Tokens.symbol "fabric" >> return Fabric
-
-  (* Parser for the write command *)
-  let write : (command, bytes list) MParser.t =
-    Tokens.symbol "write" >>
-      many_until any_char eof >>=
-      (fun filename -> return (Write (String.of_char_list filename)))
-
-  (* Parser for the Speckat command *)
-  let speckat : (command, bytes list) MParser.t =
-    Tokens.symbol "speckat" >> return Speckat
-
-  (* Parser for the exit command *)
-  let exit : (command, bytes list) MParser.t =
-    Tokens.symbol "exit" >> return Exit
-
-  (* Parser for the exit command *)
-  let quit : (command, bytes list) MParser.t =
-    Tokens.symbol "quit" >> return Exit
-
-  let command : (command, bytes list) MParser.t =
-    policy <|>
-    net <|>
-    compile <|>
-    fabric <|>
-    speckat <|>
-    write <|>
-    quit <|>
-    exit
-
 end
 
+let string_of_source (s:source) : string = match s with
+  | String s -> s
+  | Filename f ->
+    let chan = In_channel.create f in
+    In_channel.input_all chan
+
 (* A reference to the current policy and the associated string. *)
-let policy : (policy * string) ref = ref (drop, "drop")
+let policy = ref drop
 
 (* A reference to the current automaton and the associated policy. *)
-let automaton : (Frenetic_NetKAT_Compiler.automaton * policy) option ref = ref
-  None
+let automaton = ref None
 
 (* A reference to the current topoology and associated filename *)
-let topology : (Net.Topology.t * string) option ref = ref None
+let topology : Net.Topology.t option ref = ref None
 
-(* A reference to the current fabri *)
-let fabric : (switchId, Frenetic_OpenFlow0x01.flowMod list) Hashtbl.t ref = ref (Hashtbl.Poly.create ())
+(* A reference to the current fabric *)
+let fabric : (switchId, Frenetic_OpenFlow.flowTable) Hashtbl.t ref = ref (Hashtbl.Poly.create ())
 
-let load_policy_file (filename : string) : unit =
-  try
-    let open In_channel in
-    let chan = create filename in
-    let policy_string = input_all chan in
-    let pol = Parser.parse_policy policy_string in
-    close chan;
-    match pol with
-    | Ok p ->
-      policy := (p, policy_string);
-      printf " File Contents \n\n";
-      printf "%s\n%!" policy_string;
-      printf "\n Policy \n\n";
-      printf "%s\n%!" (Frenetic_NetKAT_Pretty.string_of_policy p)
-    | Error msg -> print_endline msg
-  with
-  | Sys_error msg -> printf "Loading policy failed: %s\n%!" msg
-
-let load_net_file (filename : string) : Net.Topology.t option =
-  try
-    let net = Net.Parse.from_dotfile filename in
-    printf "\n Topology: \n\n";
-    printf "%s\n%!" (Net.Pretty.to_dot net);
-    topology := Some (net, filename);
-    Some net
-  with
-  | Sys_error msg -> printf "Loading policy failed: %s\n%!" msg; None
+let load (l:input) (s:source) : (element, string) Result.t = match l with
+  | IPolicy -> Parser.policy (string_of_source s)
+  | ITopology -> begin match s with
+      | Filename f -> Ok (Topology (Net.Parse.from_dotfile f))
+      | _ -> Error "Topologies can only be loaded from DOT files" end
+  | IFabric -> Error "Fabric loading unimplemented"
 
 let compile (pol : policy) : Frenetic_NetKAT_Compiler.automaton =
   Frenetic_NetKAT_Compiler.compile_to_automaton pol
@@ -132,34 +126,10 @@ let write (at : Frenetic_NetKAT_Compiler.automaton) (filename:string) : unit =
   let string = Frenetic_NetKAT_Compiler.automaton_to_string at in
   Out_channel.write_all "%s\n%!" ~data:string
 
-let print_fabric (table:(switchId, Frenetic_OpenFlow0x01.flowMod list) Hashtbl.t)
-  : unit =
-  printf "\n Fabric: \n";
-  Hashtbl.iteri table (fun ~key:swid ~data:flow_mods ->
-    printf "\n\t Switch %Ld:\n" swid;
-    List.iter flow_mods (fun fm -> printf "\t\t %s\n"
-      (Frenetic_OpenFlow0x01.FlowMod.to_string fm)));
-  printf "\n"
-
-let speckat () : unit =
-    let dotpath = "examples/speckat/fork.dot" in
-    printf "Loading topology from %s\n" dotpath;
-    match load_net_file dotpath with
-    | Some net ->
-      printf "Generating VLAN per port fabric";
-      let table = Frenetic_Fabric.vlan_per_port net in
-      print_fabric table;
-      printf "Generating VLAN per shortest path fabric";
-      let table = Frenetic_Fabric.shortest_path net [1L;2L;3L] [1L;2L;3L] in
-      print_fabric table ;
-    | None ->
-      printf "Failed to generate fabric. Topology might be wrong."
-
 let parse_command (line : string) : command option =
   match (MParser.parse_string Parser.command line []) with
   | Success command -> Some command
   | Failed (msg, e) -> (print_endline msg; None)
-
 
 let rec repl () : unit Deferred.t =
   printf "autoshell> %!";
@@ -167,22 +137,23 @@ let rec repl () : unit Deferred.t =
   let handle line = match line with
     | `Eof -> Shutdown.shutdown 0
     | `Ok line -> match parse_command line with
-      | Some (Policy filename) -> load_policy_file filename
-      | Some (Net filename) -> ignore(load_net_file filename)
+      | Some (Load (l,s)) -> begin match load l s with
+          | Ok (Policy p)   -> policy := p
+          | Ok (Topology t) -> topology := Some t
+          | Ok (Fabric f)   -> fabric := f
+          | Error s         -> print_endline s end
       | Some Fabric -> begin match !topology with
-        | Some(net,_) ->
-          let table = mk_fabric net in
-          print_fabric table;
-          fabric := table
-        | None -> printf "Please load a topology with the `net` command first."
+          | Some net ->
+            (* TODO(basus) : fix fabric generation interface *)
+          ignore(mk_fabric net)
+        | None -> printf "Please load a topology with the `load topology` command first."
       end
       | Some Compile ->
-        let (pol,_) = !policy in
+        let pol = !policy in
         automaton := Some (compile pol, pol)
       | Some (Write filename) -> begin match !automaton with
         | None -> print_endline "No compiled automaton available"
         | Some (at, _) -> write at filename end
-      | Some Speckat -> speckat ()
       | Some Exit ->
 	print_endline "Goodbye!";
 	Shutdown.shutdown 0
