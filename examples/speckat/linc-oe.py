8e6f9fc06c94281b7630b4397d14e1ee0a235f3f
Shrutarshi Basu <shr@basus.me>
2016-05-10 15:04:43 -0400

    Added optical Mininet testbed for fork example

--- examples/speckat/linc-oe.py
+++ examples/speckat/linc-oe.py
@@ -14,30 +14,41 @@ def start(ip="127.0.0.1",port=6633):
     lg.setLogLevel('output')
 
     ctrlr = lambda n: RemoteController(n, ip=ip, port=port, inNamespace=False)
-    net = LINCNet(switch=OVSSwitch, controller=ctrlr, autoStaticArp=True, listenPort=6635)
+    net = LINCNet(switch=OVSSwitch, controller=ctrlr, listenPort=6635)
     c1 = net.addController('c1')
-    
+
     # Add hosts
     h1 = net.addHost('h1')
     h2 = net.addHost('h2')
     h3 = net.addHost('h3')
-    h4 = net.addHost('h4')
+
+    # Add electrical switches
+    s1 = net.addSwitch('s1', dpid='00:00:00:00:00:00:00:01')
+    s2 = net.addSwitch('s2', dpid='00:00:00:00:00:00:00:02')
+    s3 = net.addSwitch('s3', dpid='00:00:00:00:00:00:00:03')
+
 
     # Add optical switches
-    r1 = net.addSwitch('r1', dpid='00:00:00:00:00:00:00:11', cls=LINCSwitch)
-    r2 = net.addSwitch('r2', dpid='00:00:00:00:00:00:00:12', cls=LINCSwitch)
-    r3 = net.addSwitch('r3', dpid='00:00:00:00:00:00:00:13', cls=LINCSwitch)
-    r4 = net.addSwitch('r4', dpid='00:00:00:00:00:00:00:14', cls=LINCSwitch)
+    r1 = net.addSwitch('r1', dpid='00:00:00:00:00:00:00:04', cls=LINCSwitch)
+    r2 = net.addSwitch('r2', dpid='00:00:00:00:00:00:00:05', cls=LINCSwitch)
+    r3 = net.addSwitch('r3', dpid='00:00:00:00:00:00:00:06', cls=LINCSwitch)
+    r4 = net.addSwitch('r4', dpid='00:00:00:00:00:00:00:07', cls=LINCSwitch)
 
     # Add electrical host-switch links
-    net.addLink(h1,r1)
-    net.addLink(h2,r2)
-    net.addLink(h3,r3)
+    net.addLink(h1, s1)
+    net.addLink(h2, s2)
+    net.addLink(h3, s3)
 
     # Add optical links
-    net.addLink(r1, r4, 2, 1, cls=LINCLink)
-    net.addLink(r2, r4, 2, 2, cls=LINCLink)
-    net.addLink(r3, r4, 2, 3, cls=LINCLink)
+    net.addLink(r1, r4, 1, 1, cls=LINCLink)
+    net.addLink(r2, r4, 1, 2, cls=LINCLink)
+    net.addLink(r3, r4, 1, 3, cls=LINCLink)
+
+    # Add electrical-optical links
+    net.addLink(s1, r1, 2, 2, cls=LINCLink)
+    net.addLink(s1, r1, 3, 3, cls=LINCLink)
+    net.addLink(s2, r2, 2, 2, cls=LINCLink)
+    net.addLink(s3, r3, 2, 2, cls=LINCLink)
 
     # Start the network and prime other ARP caches
     net.start()
