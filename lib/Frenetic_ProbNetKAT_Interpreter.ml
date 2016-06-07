4da5cb2e1b8daa9d2829a4ba2acfc39c4b9fcb89
Steffen Smolka <smolka@cs.cornell.edu>
2016-06-07 15:57:03 -0700

    sequential composition of markov kernels

--- lib/Frenetic_ProbNetKAT_Interpreter.ml
+++ lib/Frenetic_ProbNetKAT_Interpreter.ml
@@ -63,14 +63,19 @@ end
 module SetDist (Key : Map.Key) (Prob : PROB) = struct
 
   module Set = Set.Make(Key) (* Set Key.t *)
-  module T = Map.Make(Set)
+  module T = Map.Make(Set) (* point -> 'a *)
 
   type point = Set.t
-  type t = Prob.t T.t (* Set Key.t -> Prob.t *)
+  type t = Prob.t T.t (* point -> Prob.t *)
 
   let empty : t = T.empty
+
+  (* point mass distribution *)
   let dirac (p : point) = T.singleton p Prob.one
 
+  (* nonstandard operation: given distributions [t1] and [t2],
+     sample independently from both to get values point1 and point2 and take
+     union of the results *)
   let union t1 t2 : t =
     T.fold t1 ~init:T.empty ~f:(fun ~key:point1 ~data:p1 acc ->
       T.fold t2 ~init:acc ~f:(fun ~key:point2 ~data:p2 acc ->
@@ -79,6 +84,34 @@ module SetDist (Key : Map.Key) (Prob : PROB) = struct
         T.change acc point (function
           | None -> Some p
           | Some p' -> Some Prob.(p' + p))))
+
+  let sum t1 t2 : t =
+    Map.merge t1 t2 ~f:(fun ~key vals ->
+      Some (match vals with
+      | `Both (v1, v2) -> Prob.(v1 + v2)
+      | `Left v | `Right v -> v))
+
+  let scale t ~(scalar : Prob.t) =
+    T.map t ~f:(fun p -> Prob.(p * scalar))
+
+
+  (* Markov Kernel *)
+  module Kernel = struct
+    type kernel = point -> t
+
+    (* lift Markov Kernel to measure transformer *)
+    let lift (k : kernel) (dist : t) : t =
+      T.fold dist ~init:empty ~f:(fun ~key:point ~data:prob acc ->
+        k point (* output distribution for that intermediate result *)
+        |> scale ~scalar:prob
+        |> sum acc)
+
+    (* sequential composition of kernels *)
+    let seq (k1 : kernel) (k2 : kernel) : kernel = fun input_point ->
+      let intermediate_dist = k1 input_point in
+      (lift k2) intermediate_dist
+
+  end
 
 
 end
