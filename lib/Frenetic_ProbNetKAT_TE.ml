040ff6f8eebcb967cac78c9a2d3d9bb5ebd2a78a
Praveen Kumar <praveenk@cs.cornell.edu>
2016-06-14 17:17:51 -0400

    use infix operators to write policies

--- lib/Frenetic_ProbNetKAT_TE.ml
+++ lib/Frenetic_ProbNetKAT_TE.ml
@@ -105,9 +105,9 @@ let topo_to_pnk (topo : Topology.t) (v_id, id_v) =
     let dst, dst_port = edge_dst edge in
     let src_id = VertexMap.find_exn v_id src in
     let dst_id = VertexMap.find_exn v_id dst in
-    let edge_src_test = Seq (Test (Switch src_id), Test (Port (port_to_pnk src_port))) in
-    let edge_dest_mod = Seq (Mod (Switch dst_id), Mod (Port (port_to_pnk dst_port))) in
-    let edge_prog = Seq (edge_src_test, Seq(Dup, Seq(edge_dest_mod, Dup))) in
+    let edge_src_test = ??(Switch src_id) >> ??(Port (port_to_pnk src_port)) in
+    let edge_dest_mod =  !!(Switch dst_id) >> !!(Port (port_to_pnk dst_port)) in
+    let edge_prog = edge_src_test >> Dup >> edge_dest_mod >> Dup in
     if pol_acc = Drop then
       edge_prog
     else
@@ -225,9 +225,7 @@ let all_pair_ksp (topo : Topology.t) (k : int) hosts =
   VertexSet.fold hosts
   ~init:SrcDstMap.empty
   ~f:(fun acc src ->
-        VertexSet.fold
-        hosts
-        ~init:acc
+        VertexSet.fold hosts ~init:acc
         ~f:(fun nacc dst ->
           let ksp = k_shortest_path topo src dst k in
           SrcDstMap.add nacc ~key:(src, dst) ~data:ksp))
@@ -235,9 +233,7 @@ let all_pair_ksp (topo : Topology.t) (k : int) hosts =
 let route_ksp (topo : Topology.t) (k : int) : scheme =
   let host_set = get_hosts_set topo in
   let all_ksp = all_pair_ksp topo k host_set in
-  SrcDstMap.fold
-    all_ksp
-    ~init:SrcDstMap.empty
+  SrcDstMap.fold all_ksp ~init:SrcDstMap.empty
     ~f:(fun ~key:(v1,v2) ~data:paths acc ->
       if (v1 = v2) then acc
       else
@@ -260,51 +256,41 @@ let path_to_pnk (topo : Topology.t) (path : path) (include_links : bool) (v_id, 
       let dst_sw_id = VertexMap.find_exn v_id dst_sw in
       let dst_pt_id = port_to_pnk dst_pt in
       let edge_pol = (* src_port -> dst_port *)
-        Seq(Test(Switch src_sw_id),
-            Seq(Test(Port src_pt_id),
-               Seq(Dup,
-                  Seq(Mod(Switch dst_sw_id),
-                      Seq(Mod(Port dst_pt_id),
-                          Dup))))) in
+        ??(Switch src_sw_id) >> ??(Port src_pt_id) >> Dup >>
+        !!(Switch dst_sw_id) >> !!(Port dst_pt_id) >> Dup in
       let new_pol =
         if in_sw_id = 0 && in_pt_id = 0 then
           if include_links then edge_pol
           else pol
         else
           let curr_sw_pol = (* policy at switch : dst_port of prev edge to src_port of curr edge *)
-            Seq(Test(Switch in_sw_id),
-              Seq(Test(Port in_pt_id),
-                  Seq(Mod(Switch src_sw_id),
-                      Mod(Port src_pt_id)))) in
-          if pol = Drop && include_links then Seq(curr_sw_pol, edge_pol)
+            ??(Switch  in_sw_id) >> ??(Port  in_pt_id) >>
+            !!(Switch src_sw_id) >> !!(Port src_pt_id) in
+          if pol = Drop && include_links then curr_sw_pol >> edge_pol
           else if pol = Drop && (not include_links) then curr_sw_pol
-          else if pol <> Drop && include_links then Seq(pol, Seq(curr_sw_pol, edge_pol))
-          else Union(pol, curr_sw_pol) in
-      ((dst_sw_id, dst_pt_id), new_pol)
-    ) in
+          else if pol <> Drop && include_links then pol >> curr_sw_pol >> edge_pol
+          else pol & curr_sw_pol in
+      ((dst_sw_id, dst_pt_id), new_pol)) in
       pol
 
 let routing_scheme_to_pnk (topo : Topology.t) (routes : scheme) (include_links : bool) (v_id, id_v) =
-  SrcDstMap.fold routes
-  ~init:Drop
-  ~f:(fun ~key:(src,dst) ~data:path_dist pol_acc ->
-    if src = dst then
-      pol_acc
-    else
-      let src_id = VertexMap.find_exn v_id src in
-      let dst_id = VertexMap.find_exn v_id dst in
-      let path_choices = PathMap.fold path_dist
-      ~init:[]
-      ~f:(fun ~key:path ~data:prob acc ->
-        (path_to_pnk topo path include_links (v_id, id_v), prob)::acc) in
-      let sd_route_pol =
-        Seq (Test(Src src_id),
-            Seq (Test(Dst dst_id),
-                Choice path_choices)) in
-      if pol_acc = Drop then
-        sd_route_pol
+  SrcDstMap.fold routes ~init:Drop
+    ~f:(fun ~key:(src,dst) ~data:path_dist pol_acc ->
+      if src = dst then
+        pol_acc
       else
-        Union (pol_acc, sd_route_pol))
+        let src_id = VertexMap.find_exn v_id src in
+        let dst_id = VertexMap.find_exn v_id dst in
+        let path_choices =
+          PathMap.fold path_dist ~init:[]
+            ~f:(fun ~key:path ~data:prob acc ->
+              (path_to_pnk topo path include_links (v_id, id_v), prob)::acc) in
+        let sd_route_pol =
+          ??(Src src_id) >> ??(Dst dst_id) >> ?@path_choices in
+        if pol_acc = Drop then
+          sd_route_pol
+        else
+          Union (pol_acc, sd_route_pol))
 
 let q1 (pk,h) = (Prob.of_int (List.length h )) // (Prob.of_int 2)
 
@@ -312,7 +298,7 @@ let analyze_routing_scheme (routing_pol : Pol.t) (topo_pol : Pol.t) (include_lin
   let pnk_program = if include_links_in_path then
       routing_pol
     else
-      Seq(Star (Seq (topo_pol, routing_pol)), topo_pol) in
+      (Star (topo_pol >> routing_pol)) >> topo_pol in
   let p = (!!(Switch 101) >> !!(Port 10) >> !!(Src 101) >> !!(Dst 102) >> pnk_program) in
   Dist.print (eval 10 p);
   expectation' 1 p ~f:q1
@@ -335,5 +321,4 @@ let () = begin
   analyze_routing_scheme routing_spf_pol topo_pol include_links_in_path;
   Printf.printf "KSP Routing: %s\n\nLatency: " (Pol.to_string routing_ksp_pol);
   analyze_routing_scheme routing_ksp_pol topo_pol include_links_in_path
-
 end
