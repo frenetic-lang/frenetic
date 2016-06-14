8c6cf2fcfa502e3619fa070ca06fe0c810821c85
Praveen Kumar <praveenk@cs.cornell.edu>
2016-06-14 13:22:11 -0400

    Option to include links in paths

--- lib/Frenetic_ProbNetKAT_TE.ml
+++ lib/Frenetic_ProbNetKAT_TE.ml
@@ -112,37 +112,55 @@ let topo_to_pnk (topo : Topology.t) (v_id, id_v) =
 
 (* Topology -> scheme *)
 let route_spf (topo : Topology.t) : scheme =
-  let device v = let lbl = Topology.vertex_to_label topo v in (Node.device lbl) in
-  let apsp = NetPath.all_pairs_shortest_paths ~topo:topo
+  let device v = vertex_to_label topo v
+    |> Node.device in
+  let apsp = NetPath.all_pairs_shortest_paths
+    ~topo:topo
     ~f:(fun x y ->
-      (match (device x, device y) with | (Node.Host,Node.Host) -> true | _ -> false)
-      ) in
-  List.fold_left apsp ~init:SrcDstMap.empty ~f:(fun acc (c,v1,v2,p) ->
-    SrcDstMap.add acc ~key:(v1,v2) ~data:( PathMap.singleton p (num_of_int 1)) )
+        match (device x, device y) with
+        | (Node.Host,Node.Host) -> true
+        | _ -> false) in
+  List.fold_left apsp
+    ~init:SrcDstMap.empty
+    ~f:(fun acc (c,v1,v2,p) ->
+      SrcDstMap.add acc ~key:(v1,v2) ~data:(PathMap.singleton p (num_of_int 1)))
 
-let path_to_pnk (topo : Topology.t) (path : path) (v_id, id_v) =
+let path_to_pnk (topo : Topology.t) (path : path) (include_links : bool) (v_id, id_v) =
   let (_,_), pol = List.fold_left path
     ~init:((0,0), Drop)
-    ~f:(fun ((in_sw, in_pt), pol) edge ->
+    ~f:(fun ((in_sw_id, in_pt_id), pol) edge ->
       let src_sw, src_pt = edge_src edge in
       let dst_sw, dst_pt = edge_dst edge in
-      if in_sw = 0 && in_pt = 0 then
-        (((VertexMap.find_exn v_id dst_sw), port_to_pnk dst_pt), pol)
-      else
-        let curr_sw_pol =
-          Seq(Test(Switch in_sw),
-              Seq(Test(Port in_pt),
-                  Seq(Mod(Switch (VertexMap.find_exn v_id src_sw)),
-                      Mod(Port (port_to_pnk src_pt))))) in
-        if pol = Drop then
-          (((VertexMap.find_exn v_id dst_sw), port_to_pnk dst_pt),
-          curr_sw_pol)
+      let src_sw_id = VertexMap.find_exn v_id src_sw in
+      let src_pt_id = port_to_pnk src_pt in
+      let dst_sw_id = VertexMap.find_exn v_id dst_sw in
+      let dst_pt_id = port_to_pnk dst_pt in
+      let edge_pol = (* src_port -> dst_port *)
+        Seq(Test(Switch src_sw_id),
+            Seq(Test(Port src_pt_id),
+               Seq(Dup,
+                  Seq(Mod(Switch dst_sw_id),
+                      Seq(Mod(Port dst_pt_id),
+                          Dup))))) in
+      let new_pol =
+        if in_sw_id = 0 && in_pt_id = 0 then
+          if include_links then edge_pol
+          else pol
         else
-          (((VertexMap.find_exn v_id dst_sw), port_to_pnk dst_pt), Union(pol,
-          curr_sw_pol))) in
-  pol
+          let curr_sw_pol = (* policy at switch : dst_port of prev edge to src_port of curr edge *)
+            Seq(Test(Switch in_sw_id),
+              Seq(Test(Port in_pt_id),
+                  Seq(Mod(Switch src_sw_id),
+                      Mod(Port src_pt_id)))) in
+          if pol = Drop && include_links then Seq(curr_sw_pol, edge_pol)
+          else if pol = Drop && (not include_links) then curr_sw_pol
+          else if pol <> Drop && include_links then Seq(pol, Seq(curr_sw_pol, edge_pol))
+          else Union(pol, curr_sw_pol) in
+      ((dst_sw_id, dst_pt_id), new_pol)
+    ) in
+      pol
 
-let routing_scheme_to_pnk (topo : Topology.t) (routes : scheme) (v_id, id_v) =
+let routing_scheme_to_pnk (topo : Topology.t) (routes : scheme) (include_links : bool) (v_id, id_v) =
   SrcDstMap.fold routes
   ~init:Drop
   ~f:(fun ~key:(src,dst) ~data:path_dist pol_acc ->
@@ -154,7 +172,7 @@ let routing_scheme_to_pnk (topo : Topology.t) (routes : scheme) (v_id, id_v) =
       let path_choices = PathMap.fold path_dist
       ~init:[]
       ~f:(fun ~key:path ~data:prob acc ->
-        (path_to_pnk topo path (v_id, id_v), prob)::acc) in
+        (path_to_pnk topo path include_links (v_id, id_v), prob)::acc) in
       let sd_route_pol =
         Seq (Test(Src src_id),
             Seq (Test(Dst dst_id),
@@ -165,15 +183,18 @@ let routing_scheme_to_pnk (topo : Topology.t) (routes : scheme) (v_id, id_v) =
         Union (pol_acc, sd_route_pol))
 
 let () = begin
+  let include_links_in_path = true in
   let topo = Parse.from_dotfile "examples/3cycle.dot" in
   let vertex_id_bimap = gen_node_pnk_id_map topo in
   let topo_pol = topo_to_pnk topo vertex_id_bimap in
   let spf = route_spf topo in
-  let routing_pol = routing_scheme_to_pnk topo spf vertex_id_bimap in
+  let routing_pol = routing_scheme_to_pnk topo spf include_links_in_path vertex_id_bimap in
   Printf.printf "Topo: %s\n" (Pol.to_string topo_pol);
   Printf.printf "Scheme: %s" (dump_scheme topo spf);
-  Printf.printf "Routing: %s\n" (Pol.to_string routing_pol);
-  let net = Seq(Star (Seq (topo_pol, routing_pol)), topo_pol) in
-  Dist.print (eval 10 (!!(Switch 101) >> !!(Port 10) >> !!(Src 101) >> !!(Dst 102)
-  >> net))
+  Printf.printf "Routing: %s\n\n" (Pol.to_string routing_pol);
+  let pnk_program = if include_links_in_path then
+      routing_pol
+    else
+      Seq(Star (Seq (topo_pol, routing_pol)), topo_pol) in
+  Dist.print (eval 10 (!!(Switch 101) >> !!(Port 10) >> !!(Src 101) >> !!(Dst 102) >> pnk_program))
 end
