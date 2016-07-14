33f0d2cfd6b55d2f493acc9bc58d1204d0f29cc9
Steffen Smolka <smolka@cs.cornell.edu>
2016-07-14 14:36:31 -0400

    sdx

--- bench/sdx/benchmark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TRIALS=`seq 1 10`
-POLICIES=`ls *.json`
-
-parallel --progress --slf $1 "cd $PWD && $HOME/compilekat/compilekat compile sdx {1} {2}" \
-  ::: $POLICIES \
-  ::: $TRIALS  | tr - ,
