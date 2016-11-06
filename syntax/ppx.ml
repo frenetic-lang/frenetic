c7b62f4e24aa7150729f32b267df1c35c6298c01
Steffen Smolka <smolka@cs.cornell.edu>
2016-11-05 19:58:58 -0700

    syntax extension for let-declarations

--- syntax/ppx.ml
+++ syntax/ppx.ml
@@ -3,16 +3,21 @@ open Parsetree
 
 (* TODO: also support let declarations and constant expressions *)
 
-let ext_name = "nk"
+(* extension is triggered by keword 'nk' *)
+let ext_keyw = "nk"
+
+let expand_netkat ~loc s =
+  (* FIXME: this is where we would call the NetKAT parser, together with a source location *)
+    Pexp_constant (Pconst_string ("NetKAT is awesome!!!", Some ""))
 
 let expand_binding binding =
   let loc = binding.pvb_expr.pexp_loc in
   let pexp_desc  =
     match binding.pvb_expr.pexp_desc with
     | Pexp_constant (Pconst_string (s, Some "")) ->
-      (* FIXME: this is where we would call the NetKAT parser, together with a source location *)
-      Pexp_constant (Pconst_string ("NetKAT is awesome!!!", Some ""))
-    | _ -> Location.raise_errorf ~loc "'let%%%s' may only bind quoted NetKAT" ext_name
+      expand_netkat ~loc s
+    | _ ->
+      Location.raise_errorf ~loc "'let%%%s' may only bind quoted NetKAT" ext_keyw
   in
   { binding with pvb_expr = { binding.pvb_expr with pexp_desc } }
 
@@ -23,20 +28,43 @@ let expand_expr ~loc:_ ~path:_ expr =
     | Pexp_let (Nonrecursive, bindings, body) ->
       Pexp_let (Nonrecursive, List.map expand_binding bindings, body)
     | Pexp_let (Recursive, _, _) ->
-      Location.raise_errorf ~loc "'let%%%s' may not be recursive" ext_name
+      Location.raise_errorf ~loc "'let%%%s' may not be recursive" ext_keyw
     | _ ->
-      Location.raise_errorf ~loc "'%%%s' can only be used with 'let'" ext_name
+      Location.raise_errorf ~loc "'%%%s' can only be used with 'let'" ext_keyw
   in
   { expr with pexp_desc }
 
-let nk_ext =
+let expand_struct_item ~loc:_ ~path:_ sitem =
+  let loc = sitem.pstr_loc in
+  let pstr_desc =
+    match sitem.pstr_desc with
+    | Pstr_value (Nonrecursive, bindings) ->
+      Pstr_value (Nonrecursive, List.map expand_binding bindings)
+    | Pstr_value (Recursive, _) ->
+      Location.raise_errorf ~loc "'let%%%s' may not be recursive" ext_keyw
+    | _ ->
+      Location.raise_errorf ~loc "'%%%s' can only be used with 'let'" ext_keyw
+  in
+  { sitem with pstr_desc }
+
+(* syntax extension for let-expressions *)
+let nk_ext_expr =
   Extension.V2.declare
-    "nk"
+    ext_keyw
     Extension.Context.expression
     Ast_pattern.(single_expr_payload __)
     expand_expr
 
+(* syntax extension for let-declarations *)
+let nk_ext_struct =
+  Extension.V2.declare
+    ext_keyw
+    Extension.Context.structure_item
+    Ast_pattern.(pstr (__ ^:: nil))
+    expand_struct_item
+
 let () =
   (* SJS: deprecated; use rule instead *)
-  Ppx_driver.register_transformation "netkat" ~extensions:[nk_ext];
+  Ppx_driver.register_transformation "netkat"
+    ~extensions:[nk_ext_expr; nk_ext_struct];
   Ppx_driver.standalone ()
--- syntax/test.ml
+++ syntax/test.ml
@@ -1,5 +1,16 @@
-let%nk x = {| hi |} in
+(* let declaration *)
+let%nk x = {| hi |}
+
+let%nk p = {| foo |}
+
+let test =
+  (* let expression*)
+  let%nk p = {| more |} in
+  p
+
+(* in *)
 (* SJS: the latter form will be rejected *)
 (* let%nk x = "hi" in  *)
-let%nk y = "hi" in
-"ciao"
+(* let%nk y = "hi" in *)
+(* "ciao" *)
+(*  *)
