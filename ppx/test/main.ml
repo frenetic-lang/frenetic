4ea151b9a73771102ab25bfd235ad391c5501f27
Steffen Smolka <smolka@cs.cornell.edu>
2016-12-14 04:35:15 +0000

    update tests

--- ppx/test/main.ml
+++ ppx/test/main.ml
@@ -5,10 +5,16 @@ let%nk_pred egress = {| switch=1 and port=1 |}
 let%nk egress' = {| filter $egress |}
 let%nk r = {| let `inport := port in while not $egress do $q + drop |}
 
+(* let expressions *)
+let _ =
+  let%nk s = {| $p; ($q + $r) |} in
+  let open Frenetic_NetKAT_Optimize in
+  mk_seq s s
+
 (* can have open terms *)
 let%nk r = {| `inport := 1 |}
 
-(*  *)
+(* can use IP and MAC accresses with meta fields *)
 let%nk r = {| `aux := 192.168.2.1; filter `aux = 00:0a:95:9d:68:16 |}
 
 (* ERRORS *)
