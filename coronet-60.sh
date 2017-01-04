505a76c2665ffb5464b98410c560b9ef989012ea
Shrutarshi Basu <shr@basus.me>
2017-01-04 14:05:28 -0500

    Fixes to coronet-60 script

--- coronet-60.sh
+++ coronet-60.sh
@@ -3,9 +3,10 @@
 EX_DIR="examples/coronet"
 FSH="./fresh.native"
 
-for i in {1..4}
+for i in {1..10}
 do
-    cat $EX_DIR/coronet-30-$i.ash | $FSH >> results.txt
+    echo "Running with $i nodes on each coast\n"
+    cat $EX_DIR/coronet-60-$i.ash | $FSH >> results.txt
 done
 
 echo "EdgeNodes	Formulation(msecs)	Solution(msecs)	Generation(msecs)"
