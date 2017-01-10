469bbc3c8f507011731845fb0134a73b40f2504d
Sam Baxter <sbaxter@cs.umass.edu>
2017-01-10 13:01:59 -0500

    Parameterize virtual compiler module by fabric generation method

--- lib/Frenetic_NetKAT_Virtual_Compiler.mli
+++ lib/Frenetic_NetKAT_Virtual_Compiler.mli
@@ -1,15 +1,18 @@
 open Core.Std
 open Frenetic_NetKAT
+open Frenetic_NetKAT_FabricGen
 
-val compile : ?log:bool
-           -> ?record_paths:string
-           -> vrel:pred
-           -> vtopo:policy
-           -> ving_pol:policy
-           -> ving:pred
-           -> veg:pred
-           -> ptopo:policy
-           -> ping:pred
-           -> peg:pred
-           -> policy (* vpol, the virtual policy *)
-           -> policy
+module Make(FG:FABRIC_GEN) : sig
+  val compile : ?log:bool
+    -> ?record_paths:string
+    -> vrel:pred
+    -> vtopo:policy
+    -> ving_pol:policy
+    -> ving:pred
+    -> veg:pred
+    -> ptopo:policy
+    -> ping:pred
+    -> peg:pred
+    -> policy (* vpol, the virtual policy *)
+    -> policy
+end
