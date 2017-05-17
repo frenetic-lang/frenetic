cac66f565f5d70fd07f7d1749fe8ed51959c3d73
Steffen Smolka <smolka@cs.cornell.edu>
2017-05-17 14:55:12 -0400

    checkpoint

--- lib/prob/ProbNetKAT_NomPacket.ml
+++ lib/prob/ProbNetKAT_NomPacket.ml
@@ -5,8 +5,8 @@ open ProbNetKAT
 type t = value Field.Map.t
 type pk = t
 
-type 'domain_witness hyperpoint = private int list
-type 'domain_witness codepoint = private int
+type 'domain_witness hyperpoint = int list
+type 'domain_witness codepoint = int
 
 module type S = sig
   val domain : domain
@@ -46,10 +46,25 @@ end) : S = struct
     let dimension =
       List.map fields ~f:(fun f -> Set.length (Map.find_exn domain f))
 
-    let to_codepoint _ = failwith "todo"
-    let of_codepoint _ = failwith "todo"
-    let to_pk _ = failwith "todo"
-    let of_pk _ = failwith "todo"
+    let values = []
+
+    let inv = []
+
+    let to_codepoint t =
+      List.fold2_exn t dimension ~init:0 ~f:(fun cp v n -> v + n * cp)
+
+    let of_codepoint cp =
+      List.fold_right dimension ~init:(cp,[]) ~f:(fun n (cp, hp) ->
+        let (cp, v) = Int.(cp /% n, cp % n) in
+        (cp, v::hp))
+      |> snd
+
+    let to_pk t =
+      List.fold2_exn t inv ~init:Field.Map.empty ~f:(fun pk vidx (f, vinv) ->
+        Field.Map.add pk f vinv.(vidx))
+
+    let of_pk pk =
+      List.map2_exn fields values ~f:(fun f vinj -> vinj (Field.Map.find_exn pk f))
   end
 
   module Codepoint = struct
@@ -61,69 +76,3 @@ end) : S = struct
   end
 
 end
-
-
-(* module NomPk = struct
-  module type S = sig
-    type t
-    val projection : projection
-    val bot : t
-
-    val of_index : index -> t
-    val to_index : t -> index
-
-    val to_pk : t -> Pk.t
-    val of_pk : Pk.t -> t
-
-    val modify : field -> value -> t -> t
-    val test : field -> value -> t -> bool
-  end
-
-  module Make (I : sig val domain : domain end) = struct
-    type t = int
-    
-    let fields = Array.Permissioned.of_list (Map.keys I.domain)
-    let values = Array.init (Array.length fields) ~f:(fun i -> 
-      Map.find_exn I.domain fields.(i)
-      |> Set.to_array)
-    let bot = 0
-
-    let of_index x = x
-    let to_index x = x
-
-    let inject (pk : Pk.t) =
-      List.fold_left fields ~init:0 ~f:(fun code (f, n, vinj, _vej) ->
-        vinj (Pk.get f) + code * n)
-
-    let eject t =
-      List.fold_right fields ~init:(t, Pk.dontcare) ~f:(fun (t, pk) (f, n, _vinj, vej) ->
-        let (v, t) = Value.(t % n, t /% n) in
-
-    )
-
-
-    let eject =
-      Field.Map.map projection.projf ~f:(fun { projf; ...} ->
-        Value.Map.to_alist projf
-        |> List.map ~f:(fun (v, idx) -> (idx, v))
-        |> Index.Map.of_alist_exn)
-
-    let to_pk t =
-      List.fold fields ~init:(t, Pk.dontcare) ~f:(fun (code, pk) f ->
-        let size = (Map.find_exn projection.projf f).
-        let v = code % 
-        )
-      Field.Map.mapi eject ~f:(fun ~key:f ~data:ejectv ->
-
-        )
-
-    let of_pk pk = failwith "not implemented"
-
-    let modify f v t =
-      to_pk t |> Pk.modify f v |> of_pk
-
-    let test f v t =
-      to_pk t |> Pk.test f v |> of_pk
-  end
-
-end *)
