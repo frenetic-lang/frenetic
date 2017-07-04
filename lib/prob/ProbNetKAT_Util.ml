53f511a4c945303c62480fe9104f3a545a05b90e
Steffen Smolka <smolka@cs.cornell.edu>
2017-07-04 17:04:47 +0100

    Util

---
+++ lib/prob/ProbNetKAT_Util.ml
@@ -0,0 +1,10 @@
+open Core
+
+let time f x =
+  let t1 = Unix.gettimeofday () in
+  let r = f x in
+  let t2 = Unix.gettimeofday () in
+  (t2 -. t1, r)
+
+let print_time time =
+  printf "time: %.4f\n" time
