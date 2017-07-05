4f7d20d9e3dbdcf19d7231a263ebae3f10d498e2
Praveen Kumar <praveenk@cs.cornell.edu>
2017-07-05 12:25:17 -0700

    Reset local fields; Update matrix queries; test equivalence

--- lib/prob/apps/lb/LoadBalancer.ml
+++ lib/prob/apps/lb/LoadBalancer.ml
@@ -6,7 +6,8 @@ open ProbNetKAT.Syntax.Dumb
 let build_list (n : int) ~(f : int -> 'a) : 'a list =
   let rec loop (i : int): 'a list =
     if i = n then []
-    else (f i) :: loop (i + 1) in
+    else let h = (f i) in
+      h :: loop (i + 1) in
   loop 0
 
 type core = { k: int; core: int }
@@ -19,6 +20,41 @@ type switch
   | Aggregation of aggregation
   | Edge of edge
   | Host of host
+
+let persistent_fields =
+  List.fold ["Switch"; "Port"] ~f:Field.Set.add ~init:Field.Set.empty
+
+let rec get_fields (p:policy) : Field.Set.t =
+  match p.p with
+  | Skip ->
+    Field.Set.empty
+  | Drop ->
+    Field.Set.empty
+  | Test (f,n) ->
+    Field.Set.singleton f
+  | Neg a ->
+    get_fields a
+  | Or (a,b) ->
+    Field.Set.union (get_fields a) (get_fields b)
+  | Modify (f,n) ->
+    Field.Set.singleton f
+  | Seq (p,q) ->
+    Field.Set.union (get_fields p) (get_fields q)
+  | Choice ps ->
+    List.fold ps ~init:Field.Set.empty
+      ~f:(fun acc (p,_) ->
+        Field.Set.union (get_fields p) acc)
+  | Ite (a,p,q) ->
+    begin
+      get_fields p
+      |> Field.Set.union(get_fields q)
+      |> Field.Set.union (get_fields a)
+    end
+  | While(a,p) ->
+    Field.Set.union (get_fields a) (get_fields p)
+
+let get_tmp_fields (p:policy) =
+  Field.Set.diff (get_fields p) persistent_fields
 
 module Switch : sig
   val get_id : switch -> int
@@ -103,7 +139,7 @@ module CoreDecision : Routing = struct
         build_list k ~f:(fun edge ->
           build_list k ~f:(fun host ->
             let host_id = Switch.get_id (Host { k; cluster; edge; host }) in
-            (!!("DestIP", host_id), 1//(k*k*k))))
+            (!!("DstIP", host_id), 1//(k*k*k))))
         |> List.concat_no_order)
       |> List.concat_no_order
       |> choice in
@@ -113,7 +149,7 @@ module CoreDecision : Routing = struct
         build_list k ~f:(fun edge ->
           build_list k ~f:(fun host ->
             let host_id = Switch.get_id (Host { k; cluster; edge; host }) in
-            (??("DestIP", host_id), !!("Port", cluster+1))))
+            (??("DstIP", host_id), !!("Port", cluster+1))))
         |> List.concat_no_order)
       |> List.concat_no_order
       |> mk_big_ite ~default:drop in
@@ -129,7 +165,7 @@ module CoreDecision : Routing = struct
       build_list k ~f:(fun edge ->
         build_list k ~f:(fun host ->
           let host_id = Switch.get_id (Host { k; cluster; edge; host }) in
-          (??("DestIP", host_id), !!("Port", edge+1))))
+          (??("DstIP", host_id), !!("Port", edge+1))))
       |> List.concat_no_order
       |> mk_big_ite ~default:drop in
     let sw_id = Switch.get_id (Aggregation { k; cluster }) in
@@ -143,7 +179,7 @@ module CoreDecision : Routing = struct
     let pol =
         build_list k ~f:(fun host ->
           let host_id = Switch.get_id (Host { k; cluster; edge; host }) in
-          (??("DestIP", host_id), !!("Port", edge+1)))
+          (??("DstIP", host_id), !!("Port", host+1)))
       |> mk_big_ite ~default:drop in
 
     let sw_id = Switch.get_id (Edge { k; cluster; edge }) in
@@ -247,6 +283,7 @@ let access_links ~(k : int) : link list =
       let edge_sw_id = Switch.get_id (Edge { k; cluster; edge }) in
       build_list k ~f:(fun host ->
         let host_id = Switch.get_id (Host { k; cluster; edge; host}) in
+        Printf.printf "hostid: %d cluster %d edge %d host %d\n" host_id cluster edge host;
         [(??("Switch", edge_sw_id) >> ??("Port", host + 1),
           !!("Switch", host_id) >> !!("Port", 0));
          (??("Switch", host_id) >> ??("Port", 0),
@@ -289,12 +326,12 @@ let in_network ~(k : int) : policy =
 let in_traffic ~(k : int) : policy =
   let core = 0 in
   let core_sw_id = Switch.get_id (Core { k; core }) in
-  ??("Switch", core_sw_id) >> ??("Port", 0)
+  ??("Switch", core_sw_id) >> ??("Port", 0) >> ??("DstIP", 0)
 
 (* Load-balancer program with traffic *)
 let lb_policy ~(k : int) ~(alg : lb_algs)=
-  let ingress = in_traffic ~k in
   let t = topology_program ~k in
+  let ingress = in_traffic ~k in
   let p = match alg with
     | PerHop -> PerHop.routing_policy k
     | CoreDecision -> CoreDecision.routing_policy k
@@ -306,13 +343,52 @@ let lb_policy ~(k : int) ~(alg : lb_algs)=
   (* let pol = ingress >> (p >> t) >> (p >> t) >> (p >> t) >> egress in *)
   run pol ~row_query:ingress ~col_query:egress
 
+let reset_tmp_fields p =
+  get_tmp_fields p |>
+  Field.Set.fold ~init:skip
+    ~f:(fun acc f -> acc >> !!(f, 0))
+
+let test_reset_tmp_fields p =
+  get_tmp_fields p |>
+  Field.Set.fold ~init:skip
+    ~f:(fun acc f -> acc >> ??(f, 0))
+
+let uniform_ingress_tm =
+  !!("Switch", 0) >> !!("Port", 0) >> !!("DstIP", 0) >>
+  ?@[(!!("SrcIPHash%k", 0), 1//2) ; (!!("SrcIPHash%k", 1), 1//2)] >>
+  ?@[(!!("SrcPortHash%k", 0), 1//2) ; (!!("SrcPortHash%k", 1), 1//2)] >>
+  ?@[(!!("DstPortHash%k", 0), 1//2) ; (!!("DstPortHash%k", 1), 1//2)]
+
+let test ~(k : int) =
+  let t = topology_program ~k in
+  let ingress = in_traffic ~k in
+  let p1 = PerHop.routing_policy k in
+  let p2 = CoreDecision.routing_policy k in
+  let p3 = EcmpLite.routing_policy k in
+  let egress = (delivered_to_host ~k) in
+  let output_query = egress >> (test_reset_tmp_fields p2) in
+  let within_network = in_network ~k in
+  let pol1 = ingress >> mk_while (neg egress) (p1 >> t) >> egress >> (reset_tmp_fields p2) in
+  let pol2 = ingress >> mk_while (neg egress) (p2 >> t) >> egress >> (reset_tmp_fields p2) in
+  let pol3 = ingress >> mk_while (neg egress) (p3 >> t) >> egress >> (reset_tmp_fields p2) in
+  (* let pol = ingress >> mk_while (within_network) (p >> t) >> egress in *)
+  (* let pol = ingress >> (p >> t) >> (p >> t) >> (p >> t) >> egress in *)
+  (* run' pol1 pol2 pol3 ~row_query:ingress ~col_query:egress *)
+  let matrices = run' ~row_query:ingress ~col_query:output_query [pol1; pol2] pol2 in
+  let (f1, m1) = List.nth_exn matrices 0 in
+  let (f2, m2) = List.nth_exn matrices 1 in
+  Printf.printf "%b\n" (f1=f2);
+  Printf.printf "%b\n" (m1=m2)
+
+
 let () = begin
   let k = 2 in
-  let lbmap = List.fold ~init:String.Map.empty
-    ~f:(fun acc alg -> String.Map.add acc ~key:(alg_to_string alg) ~data:(lb_policy ~k ~alg))
-    [PerHop; CoreDecision; ECMPLite] in
-  let m1 = String.Map.find_exn lbmap (alg_to_string PerHop) in
-  let m2 = String.Map.find_exn lbmap (alg_to_string CoreDecision) in
-  Printf.printf "%b\n" (m1=m2);
+  let _ = test ~k in
+  (* let lbmap = List.fold ~init:String.Map.empty *)
+    (* ~f:(fun acc alg -> String.Map.add acc ~key:(alg_to_string alg) ~data:(lb_policy ~k ~alg)) *)
+    (* [PerHop; CoreDecision; ECMPLite] in *)
+  (* let m1 = String.Map.find_exn lbmap (alg_to_string PerHop) in *)
+  (* let m2 = String.Map.find_exn lbmap (alg_to_string CoreDecision) in *)
+  (* Printf.printf "%b\n" (m1=m2); *)
   ()
 end
--- lib/prob/apps/lb/Runner.ml
+++ lib/prob/apps/lb/Runner.ml
@@ -25,6 +25,7 @@ let identity = fun x -> x
 let rec test_filter test_fn (query:policy) i =
   match query.p with
   | Skip -> true
+  | Drop -> false
   | Test (f, n) -> test_fn f n i
   | Neg a -> not (test_filter test_fn a i)
   | Or (a1, a2) ->
@@ -75,3 +76,37 @@ let run ?(print=true) ?(lbl=true) ?(transpose=false) ?(debug=false)
   end;
   print_time t;
   dense_mc
+
+let run' ?(print=true) ?(lbl=true) ?(transpose=false) ?(debug=false)
+      ?(row_query=skip) ?(col_query=skip) programs dom_prog =
+  printf "\n========================= EIGEN ==========================\n\n%!";
+  fprintf fmt "setting domain for policy = %a\n\n%!" pp_policy dom_prog;
+  let dom = domain dom_prog in
+  let module Repr = ProbNetKAT_Packet_Repr.Make(struct let domain = dom end) in
+  let n = Repr.Index0.max.i + 1 in
+  print_dom_size n dom;
+  let module Mc = ProbNetKAT_Mc.MakeOwl(Repr) in
+  List.fold programs ~init:[] ~f:(fun acc p ->
+  let (t, mc) = time (fun () -> Mc.of_pol ~debug p) in
+  let dense_mc = (if transpose then Sparse.transpose else ident) mc |>
+                   Sparse.to_dense in
+  let rows = Dense.filteri_rows (fun i _ -> test_filter Repr.Index.test' row_query i) dense_mc in
+  let cols = Dense.filteri_cols (fun i _ -> test_filter Repr.Index.test' col_query i) dense_mc in
+  let fd_mc = Dense.rows dense_mc (Array.map ~f:(fun i -> i - 1) rows) in
+  let fd_mc = Dense.cols fd_mc (Array.map ~f:(fun i -> i - 1) cols) in
+  (* let fd_mc = Dense.cols fd_mc cols in *)
+  let print_mat ?(row_map=identity) ?(col_map=identity) =
+    Format.printf "@[MATRIX:@\n%a@\n@]@."
+      (if not lbl then Owl_pretty.pp_fmat else
+         Owl_pretty.pp_labeled_fmat
+           ~pp_left:(Some (fun fmt i -> fprintf fmt "%a|" Repr.Index.pp' (row_map i)))
+           ~pp_head:(Some (fun fmt i -> fprintf fmt "%a|" Repr.Index.pp' (col_map i)))
+           ~pp_foot:None
+           ~pp_right:None ()) in
+
+  if print then begin
+    print_mat ~row_map:(fun x -> Array.nget rows (x-1)) ~col_map:(fun x -> Array.nget cols (x-1)) fd_mc;
+    (* print_mat dense_mc; *)
+  end;
+  print_time t;
+  (dense_mc, fd_mc) :: acc)
