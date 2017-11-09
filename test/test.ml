7d604f129a3727b9185ba850727d98bf834e293b
Steffen Smolka <smolka@cs.cornell.edu>
2017-11-09 18:40:47 -0500

    basic testing set up

---
+++ test/test.ml
@@ -0,0 +1,14 @@
+open Probnetkat
+
+let add () =
+    Alcotest.(check int) "1+1=2" (1+1) 3
+
+let test_set = [
+  "\xF0\x9F\x90\xAB  Capitalize", `Quick, add;
+  "\xF0\x9F\x90\xAB  Capitalize", `Quick, add;
+]
+
+let () =
+  Alcotest.run "Probnetkat" [
+    "", test_set;
+  ]
