e55f9456ea6e241e8e08b0ff08318a6870984075
Steffen Smolka <smolka@cs.cornell.edu>
2017-11-11 22:50:31 -0500

    print execution times

--- src/Util.ml
+++ src/Util.ml
@@ -6,6 +6,12 @@ let time f x =
   let t2 = Unix.gettimeofday () in
   (t2 -. t1, r)
 
+let timed descr f =
+  printf "%s...%!" descr;
+  let t, y = time f () in
+  printf " %f" t;
+  y
+
 let print_time time =
   printf "time: %.4f\n" time
 
