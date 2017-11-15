4e09e14794ec62f9b27df00b7242ad447b106749
Steffen Smolka <smolka@cs.cornell.edu>
2017-11-15 13:09:01 -0500

    more ProbNetKAT modelling

--- src/Model.ml
+++ src/Model.ml
@@ -16,7 +16,7 @@ module Make(Params: sig
 
   (* switch and topo terms for a particular switch *)
   val sw_pol : int -> int -> string policy
-  val topo_pol : int -> string policy
+  val links_from : Topo.vertex -> string policy
 
   (* probability of link failure for particular link (sw,pt) *)
   val failure_prob : int -> int -> Prob.t
@@ -27,64 +27,60 @@ module Make(Params: sig
   val eggress : string pred
 end) = struct
 
-include Params
+  include Params
 
-let switches =
-  Topo.vertexes topo
-  |> Set.filter ~f:(fun v -> match Node.device (Topo.vertex_to_label topo v) with
-      | Node.Switch -> true
-      | Node.Middlebox | Node.Host -> false
-  )
-  |> Set.to_list
-  (* |> List.map ~f:(Topology.sw_val topo) *)
+  let rec make () : string policy =
+    PNK.(
+      filter ingress >>
+      whl (neg eggress) (
+        (* p; t *)
+        hop ~final:false ()
+      ) >>
+      (* p *)
+      hop ~final:true ()
+    )
 
-let locs : (int * int list) list =
-  List.map switches ~f:(fun sw ->
-    let pts =
-      Topo.vertex_to_ports topo sw
-      |> Set.to_list
-      |> List.map ~f:Topology.pt_val
-    in
-    (Topology.sw_val topo sw, pts)
-  )
+  and hop ~final () =
+    let open PNK in
+    ite_cascade (Topology.locs topo) ~otherwise:drop ~f:(fun (sw, pts) ->
+      let sw_id = Topology.sw_val topo sw in
+      let guard = ???(Params.sw, sw_id) in
+      let body =
+        (if final then ident else with_init_up_bits sw_id pts) begin
+          ite_cascade pts ~otherwise:drop ~f:(fun pt_id ->
+            (???(pt, pt_id), for_loc sw_id pt_id)
+          )
+          >>
+          if final then skip else links_from sw
+        end
 
-let rec make () : string policy =
-  PNK.(
-    filter ingress >>
-    whl (neg eggress) (
-      hop ~final:false ()
-    ) >>
-    hop ~final:true ()
-  )
+      in
+      (guard, body)
+    )
 
-and hop ~final () =
-  let open PNK in
-  ite_cascade locs ~otherwise:drop ~f:(fun (sw_id, pts) ->
-    let guard = ???(sw, sw_id) in
-    let body =
-      (if final then ident else with_init_up_bits sw_id pts) @@
-      ite_cascade pts ~otherwise:drop ~f:(fun pt_id ->
-        (???(pt, pt_id), for_loc sw_id pt_id)
-      )
-    in
-    (guard, body)
-  )
+  and for_loc sw pt =
+    Params.sw_pol sw pt
 
-and for_loc sw pt =
-  Params.sw_pol sw pt
+  and with_init_up_bits sw pts body =
+    let up_bits = List.map pts ~f:(fun pt -> (up sw pt, 0, true)) in
+    PNK.locals up_bits body
 
-and with_init_up_bits sw pts body =
-  let up_bits = List.map pts ~f:(fun pt -> (up sw pt, 0, true)) in
-  PNK.locals up_bits body
+  and init_up_bits sw pts =
+    let open PNK in
+    List.map pts ~f:(fun pt ->
+      let p = failure_prob sw pt in
+      ?@[ !!(up sw pt, 1) @ p; !!(up sw pt, 0) @ Prob.(one - p) ]
+    )
+    |> mk_big_seq
 
-and init_up_bits sw pts =
-  let open PNK in
-  List.map pts ~f:(fun pt ->
-    let p = failure_prob sw pt in
-    ?@[ !!(up sw pt, 1) @ p; !!(up sw pt, 0) @ Prob.(one - p) ]
-  )
-  |> mk_big_seq
+end
 
 
+module DefaultParams() = struct
+  let sw = "sw"
+  let pt = "pt"
+  let up _sw pt = sprintf "up_%d" pt
+
+  let failure_prob = PNK.(1 // 10)
 
 end
--- src/Topology.ml
+++ src/Topology.ml
@@ -6,6 +6,7 @@ open Syntax
 
 module Net = Frenetic.Network.Net
 open Net.Topology
+module Node = Frenetic.Network.Node
 
 
 (** things that don't need to be parameterized  *)
@@ -20,6 +21,27 @@ let sw_val topo sw : int =
   vertex_to_string topo sw
   (* |> Util.tap ~f:(eprintf "switch \"%s\"\n") *)
   |> Int.of_string
+
+
+let switches topo : vertex list =
+  vertexes topo
+  |> Set.filter ~f:(fun v -> match Node.device (vertex_to_label topo v) with
+      | Node.Switch -> true
+      | Node.Middlebox | Node.Host -> false
+  )
+  |> Set.to_list
+  (* |> List.map ~f:(Topology.sw_val topo) *)
+
+let locs topo : (vertex * int list) list =
+  List.map (switches topo) ~f:(fun sw ->
+    let pts =
+      vertex_to_ports topo sw
+      |> Set.to_list
+      |> List.map ~f:pt_val
+    in
+    (sw, pts)
+  )
+
 end
 
 include Generic
@@ -32,30 +54,36 @@ module Make(Parameters: sig
   val up : int -> int -> string
 end) = struct
 
-open Parameters
-include Generic
+  open Parameters
+  include Generic
 
-let to_probnetkat (topo : Net.Topology.t) ~(guard_links:bool) : string policy =
-  fold_edges (fun edge t ->
-      let (src_sw,src_pt) = edge_src edge in
-      let (dst_sw,dst_pt) = edge_dst edge in
-      let src_sw = sw_val topo src_sw in
-      let dst_sw = sw_val topo dst_sw in
-      let src_pt = pt_val src_pt in
-      let dst_pt = pt_val dst_pt in
-      let guard = 
-        if guard_links then PNK.(???(up src_sw src_pt, 1)) else False
-      in
-      PNK.(
-        ite ( ???(sw, src_sw) & ???(pt, src_pt) & guard ) (
-          !!(sw, dst_pt) >> !!(pt, dst_pt)
-        ) (
-          t
-        )
-      )
+  let link_of_edge (topo : Net.Topology.t) edge ~(guard:bool) : (string pred * string policy) =
+    let (src_sw,src_pt) = edge_src edge in
+    let (dst_sw,dst_pt) = edge_dst edge in
+    let src_sw = sw_val topo src_sw in
+    let dst_sw = sw_val topo dst_sw in
+    let src_pt = pt_val src_pt in
+    let dst_pt = pt_val dst_pt in
+    let guard = PNK.(
+      ???(sw, src_sw) & ???(pt, src_pt) &
+      (if guard then ???(up src_sw src_pt, 1) else True)
     )
-    topo
-    PNK.drop
+    in
+    (guard, PNK.( !!(sw, dst_pt) >> !!(pt, dst_pt) ))
 
+  let to_probnetkat (topo : Net.Topology.t) ~(guard_links:bool) : string policy =
+    fold_edges (fun edge t ->
+        let (guard, action) = link_of_edge topo edge ~guard:guard_links in
+        PNK.ite guard action t
+      )
+      topo
+      PNK.drop
+
+  let links_from (topo : Net.Topology.t) sw ~(guard_links: bool) : string policy =
+    neighbors topo sw
+    |> Set.to_list
+    |> List.map ~f:(find_edge topo sw)
+    |> List.map ~f:(link_of_edge topo ~guard:guard_links)
+    |> PNK.(mk_big_ite ~default:drop)
 
 end
