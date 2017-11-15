5e33ae262109919bc76f8efa9335b5ec8f82fc94
Steffen Smolka <smolka@cs.cornell.edu>
2017-11-15 00:52:50 -0500

    dot to PNK term: done :)

--- bin/topo.ml
+++ bin/topo.ml
@@ -1,8 +1,28 @@
+open! Core
 open Probnetkat
+
+module Parameters = struct
+  (* switch field *)
+  let sw = "sw"
+
+  (* port field *)
+  let pt = "pt"
+
+  (* up bit associated with link *)
+  let up sw pt = sprintf "up_%d" pt
+end
+
+module Topology = struct
+  include Topology
+  include Topology.Make(Parameters)
+end
+
+
 
 let () = begin
 
 let topo = Topology.parse (Sys.argv.(1)) in
-()
-
+let topo_prog = Topology.to_probnetkat topo ~guard_links:true in
+Format.printf "%a\n" Syntax.pp_policy topo_prog;
+Util.timed "topo to Fdd" (fun () -> Symbolic.Fdd.of_pol topo_prog; ())
 end
--- src/Topology.ml
+++ src/Topology.ml
@@ -14,26 +14,39 @@ let pt_val pt : int =
   Int.of_int32_exn pt
 
 let sw_val topo sw : int =
-  let str = vertex_to_string topo sw in
-  String.sub str 1 (String.length str - 1)
+  vertex_to_string topo sw
+  (* |> Util.tap ~f:(eprintf "switch \"%s\"\n") *)
   |> Int.of_string
 
+module Make(Parameters: sig
+  val sw : string
+  val pt : string
+  val up : int -> int -> string
+end) = struct
 
-let to_probnetkat (topo : Net.Topology.t) : string policy =
-  fold_edges (fun edge t ->
-      let (src_sw,src_pt) = edge_src edge in
-      let (dst_sw,dst_pt) = edge_dst edge in
-      let src_sw = sw_val topo src_sw in
-      let dst_sw = sw_val topo dst_sw in
-      let src_pt = pt_val src_pt in
-      let dst_pt = pt_val dst_pt in
-      PNK.(
-        ite ( ???("sw", src_sw) & ???("pt", src_pt) ) (
-          !!("sw", dst_pt) >> !!("pt", dst_pt)
-        ) (
-          t
+  open Parameters
+
+  let to_probnetkat (topo : Net.Topology.t) ~(guard_links:bool) : string policy =
+    fold_edges (fun edge t ->
+        let (src_sw,src_pt) = edge_src edge in
+        let (dst_sw,dst_pt) = edge_dst edge in
+        let src_sw = sw_val topo src_sw in
+        let dst_sw = sw_val topo dst_sw in
+        let src_pt = pt_val src_pt in
+        let dst_pt = pt_val dst_pt in
+        let guard = 
+          if guard_links then PNK.(???(up src_sw src_pt, 1)) else False
+        in
+        PNK.(
+          ite ( ???(sw, src_sw) & ???(pt, src_pt) & guard ) (
+            !!(sw, dst_pt) >> !!(pt, dst_pt)
+          ) (
+            t
+          )
         )
       )
-    )
-    topo
-    PNK.drop
+      topo
+      PNK.drop
+
+
+end
