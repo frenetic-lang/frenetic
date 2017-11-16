7e2acdabe7f8f6cd52e2b50a477bd0a65998b0d7
Steffen Smolka <smolka@cs.cornell.edu>
2017-11-16 16:55:28 -0500

    smarter pretty printing

--- src/Prob.ml
+++ src/Prob.ml
@@ -15,4 +15,15 @@ include (Q : module type of Q with type t := t)
 
 let t_of_sexp s = of_float (Float.t_of_sexp s)
 let sexp_of_t t = Float.sexp_of_t (to_float t)
-let pp = pp_print
+
+let pp fmt t =
+  if Z.(t.den <= of_int 100) then
+    pp_print fmt t
+  else
+    Float.pp fmt (to_float t)
+
+let to_string t =
+  if Z.(t.den <= of_int 100) then
+    to_string t
+  else
+    sprintf "%.3f" (to_float t)
