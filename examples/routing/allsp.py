33c27a66c12147ebf2727a6a399e28ff3077ffed
Praveen Kumar <praveenk@cs.cornell.edu>
2017-11-16 15:39:29 -0800

    Compute next hops for all shortest paths

--- examples/routing/allsp.py
+++ examples/routing/allsp.py
@@ -4,8 +4,10 @@ from collections import defaultdict
 def route(G, dst=0):
     root = dst
     print root
-    all_sps = defaultdict(list)
+    next_hops = defaultdict(set)
     for s in G.nodes():
         paths = nx.all_shortest_paths(G,source=s,target=root)
-        all_sps[s] = paths
-    return all_sps
+        for path in paths:
+            if len(path) >= 2:
+                next_hops[s].add(path[1])
+    return next_hops
--- examples/routing/routing_lib.py
+++ examples/routing/routing_lib.py
@@ -9,16 +9,14 @@ def serialize_trees(trees, outfile):
                 f.write(str(u) + ' -- ' + str(v) + '\n')
             counter += 1
 
-def serialize_paths(allpaths, outfile):
-    with open(outfile+'.allsp', 'w') as f:
-        for s,paths in allpaths.iteritems():
-            f.write('# Source: ' + str(s) + '\n')
-            for path in paths:
-                f.write(" -- ".join([str(v) for v in path]) + '\n')
+def serialize_nexthops(allnhs, outfile):
+    with open(outfile+'.nexthops', 'w') as f:
+        for s,nhs in allnhs.iteritems():
+            f.write(str(s) + " : " + " ".join([str(v) for v in sorted(nhs)]) + '\n')
 
 def serialize_routes(routes, outfile):
     if type(routes) == list:
         serialize_trees(routes, outfile)
     elif type(routes) == defaultdict:
-        serialize_paths(routes, outfile)
+        serialize_nexthops(routes, outfile)
 
