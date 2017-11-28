frenetic-lang/frenetic

commit c22028e7ddeda48f097121e99541d972a0dfd63c

use inline records for meta fields (#577)

lib/netkat/Pretty.ml
@@ -103,7 +103,7 @@ module Formatting = struct
           | SEQ_L -> fprintf fmt "@[%a;@ %a@]" (pol SEQ_L) p1 (pol SEQ_R) p2
           | _ -> fprintf fmt "@[(@[%a;@ %a@])@]" (pol SEQ_L) p1 (pol SEQ_R) p2
         end
-      | Let (id, init, mut, p) ->
+      | Let {id; init; mut; body} ->
         fprintf fmt "@[%s %s := %a in %a@]"
           (if mut then "var" else "let")
           id
@@ -135,7 +135,7 @@ let rec pretty_assoc (p : policy) : policy = match p with
   | Union (p1, p2) -> pretty_assoc_par p
   | Seq (p1, p2) -> pretty_assoc_seq p
   | Star p' -> Star (pretty_assoc p')
-  | Let (id, init, mut, p) -> Let (id, init, mut, pretty_assoc p)
+  | Let {id; init; mut; body} -> Let {id; init; mut; body = pretty_assoc body}
 and pretty_assoc_par (p : policy) : policy = match p with
   | Union (p1, Union (p2, p3)) ->
     Union (pretty_assoc_par (Union (p1, p2)), pretty_assoc_par p3)

lib/netkat/Compiler.ml
@@ -116,7 +116,7 @@ module FDD = struct
                         of_local_pol_k env q (fun q' ->
                           k (seq p' q')))
     | Star p -> of_local_pol_k env p (fun p' -> k (star p'))
-    | Let (field, init, mut, p) ->
+    | Let { id=field; init; mut; body=p } ->
       let env = Field.Env.add env field init mut in
       of_local_pol_k env p (fun p' -> k (erase env p' field init))
     | Link _ | VLink _ | Dup -> raise Non_local

lib/netkat/Fdd.ml
@@ -166,7 +166,7 @@ module Field = struct
           f_seq' q lst env (fun (n, lst) ->
             k (m * n, lst)))
       | Union _ -> k (f_union pol env, lst)
-      | Let (id, init, mut, p) ->
+      | Let { id; init; mut; body=p } ->
         let env = Env.add env id init mut in
         f_seq' p lst env k
       | Star p -> k (f_union p env, lst)
@@ -185,7 +185,7 @@ module Field = struct
           f_union' q lst env (fun (n, lst) ->
             k (m + n, lst)))
       | Seq _ -> k (f_seq pol env, lst)
-      | Let (id, init, mut, p) ->
+      | Let { id; init; mut; body=p } ->
         let env = Env.add env id init mut in
         k (f_seq p env, lst)
       | Star p -> f_union' p lst env k

lib/netkat/Optimize.ml
@@ -102,8 +102,8 @@ let specialize_policy sw pol =
         loop pol1 (fun p1 -> loop pol2 (fun p2 -> k (mk_seq p1 p2)))
       | Star pol ->
         loop pol (fun p -> k (mk_star p))
-      | Let (id, init, mut, pol) ->
-        loop pol (fun p -> k (Let (id, init, mut, p)))
+      | Let {id; init; mut; body} ->
+        loop body (fun body -> k (Let {id; init; mut; body}))
       | Link _ | VLink _ | Dup ->
         failwith "cannot specialize global policy" in
   loop pol (fun x -> x)
@@ -157,8 +157,8 @@ let rec norm_policy (pol : policy) : policy = match pol with
   | Seq (p, q) ->
     let pol' = Seq (norm_policy p, norm_policy q) in
     mk_big_seq (list_of_seq pol')
-  | Let (id, init, mut, pol) ->
-    Let (id, init, mut, norm_policy pol)
+  | Let {id; init; mut; body} ->
+    Let {id; init; mut; body = norm_policy body}
   | Dup -> Dup
 
 (*

lib/netkat/Parser.cppo.mly
@@ -117,11 +117,11 @@ pol:
       AST( Star p )
       PPX( Star [%e p] )
   | mut=letexp; id=METAID; ASSIGN; v=metaval; IN; p=pol
-      AST( Let(id         , Const v     , mut     , p     ) )
-      PPX( Let([%e estring ~loc id], Const [%e v], [%e mut], [%e p]) )
+      AST( Let{id                     ; init=Const v     ; mut         ; body=p     } )
+      PPX( Let{id=[%e estring ~loc id]; init=Const [%e v]; mut=[%e mut]; body=[%e p]} )
   | mut=letexp; id=METAID; ASSIGN; a=alias; IN; p=pol
-      AST( Let(id         , Alias a     , mut    , p     ) )
-      PPX( Let([%e estring ~loc id], Alias [%e a], [%e mut], [%e p]) )
+      AST( Let{id                     ; init=Alias a     ; mut         ; body=p     } )
+      PPX( Let{id=[%e estring ~loc id]; init=Alias [%e a]; mut=[%e mut]; body=[%e p]} )
   | sw1=int64; AT; pt1=int32; LINK; sw2=int64; AT; pt2=int32
       AST( Link (sw1, pt1, sw2, pt2) )
       PPX( Link ([%e sw1], [%e pt1], [%e sw2], [%e pt2]) )

lib/netkat/Semantics.ml
@@ -114,7 +114,7 @@ let size (pol:policy) : int =
     | Union(pol1, pol2)
     | Seq(pol1, pol2) -> size pol1 (fun spol1 -> size pol2 (fun spol2 -> f (1 + spol1 + spol2)))
     | Star(pol) -> size pol (fun spol -> f (1 + spol))
-    | Let (id, init, mut, pol) -> size pol (fun s -> f (s+1))
+    | Let {body; _} -> size body (fun s -> f (s+1))
     | Link(_,_,_,_) -> f 5
     | VLink(_,_,_,_) -> f 5
     | Dup -> f 1
@@ -254,7 +254,7 @@ let queries_of_policy (pol : policy) : string list =
     | Filter _ | Mod _ | Link _ | VLink _ | Dup -> acc
     | Union (p, q) | Seq (p, q) -> loop q (loop p acc)
     | Star p -> loop p acc
-    | Let (id, init, mut, body) -> loop body acc
+    | Let {body; _} -> loop body acc
   in
   loop pol []
 
@@ -284,7 +284,7 @@ let switches_of_policy (p:policy) =
        sw1 :: sw2 :: acc
     | VLink _ | Dup ->
       acc
-    | Let(_,_,_,p) ->
-      collect p acc
+    | Let {body; _} ->
+      collect body acc
   in
   collect p [] |> List.dedup |> List.to_list

lib/netkat/Syntax.ml
@@ -63,10 +63,7 @@ type policy =
   | Star of policy
   | Link of switchId * portId * switchId * portId
   | VLink of vswitchId * vportId * vswitchId * vportId
-  (* TODO: move to inline records, as soon as derriving sexp supports them, see
-     https://github.com/janestreet/ppx_sexp_conv/issues/9 *)
-  (* | Let of { id : metaId; init : meta_init; body : policy; mut : bool } *)
-  | Let of metaId * meta_init * bool * policy
+  | Let of { id : metaId; init : meta_init; mut : bool; body : policy }
   | Dup
   [@@deriving sexp]
 

lib/netkat/Syntax.mli
@@ -76,10 +76,7 @@ type policy =
   | Star of policy
   | Link of switchId * portId * switchId * portId
   | VLink of vswitchId * vportId * vswitchId * vportId
-  (* TODO: move to inline records, as soon as derriving sexp supports them, see
-     https://github.com/janestreet/ppx_sexp_conv/issues/9 *)
-  (* | Let of { id : metaId; init : meta_init; body : policy; mut : bool } *)
-  | Let of metaId * meta_init * bool * policy
+  | Let of { id : metaId; init : meta_init; mut : bool; body : policy }
   | Dup
   [@@deriving sexp]
 

commit a77c483fdda1be22a2d7cb4c6e203539996566a3

fix bug introduced by #577 (c22028e); ensure ppx extension is tested automatically so we don't introduce bugs in the future

lib/netkat/Pretty.ml
@@ -108,7 +108,7 @@ module Formatting = struct
           | SEQ_L -> fprintf fmt "@[%a;@ %a@]" (pol SEQ_L) p1 (pol SEQ_R) p2
           | _ -> fprintf fmt "@[(@[%a;@ %a@])@]" (pol SEQ_L) p1 (pol SEQ_R) p2
         end
-      | Let {id; init; mut; body} ->
+      | Let {id; init; mut; body=p} ->
         fprintf fmt "@[%s %s := %a in %a@]"
           (if mut then "var" else "let")
           id

ppx/test/jbuild
@@ -9,5 +9,7 @@
 ;; makes sure the executable is built in test mode
 (alias
   ((name    runtest)
-   (deps ((file main.exe)))
-))
+   (deps (main.exe))
+   (action (run ${<}))
+  )
+)