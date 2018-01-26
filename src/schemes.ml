frenetic-lang/frenetic

commit 4607698b39d05d2d8047c10b827faeb9a5797baa

Fix bug introduced in 8433fae547feb54e0b314b28f10aff193422cc07. Avoid refering to up fields that were never introduced.

src/schemes.ml
@@ -214,7 +214,7 @@ let random_walk topo : Net.Topology.vertex -> string policy =
 let resilient_random_walk topo : Net.Topology.vertex -> string policy =
   fun sw ->
     let pts =
-      Topology.vertex_to_ports topo.graph sw
+      Topology.(vertex_to_ports topo.graph sw ~dst_filter:(is_host topo.graph))
       |> List.map ~f:Topology.pt_val
     in
     let choose_port = random_walk topo sw in

src/Topology.ml
@@ -38,7 +38,7 @@ let sw_val topo sw : int =
   |> Node.id
   |> Int.of_int64_exn
 
-let vertex_to_ports ?(dst_filter=fun _ -> true) topo vertex =
+let vertex_to_ports ~dst_filter topo vertex =
   neighbors topo vertex
   |> Set.filter ~f:dst_filter
   |> Set.to_list
@@ -48,7 +48,7 @@ let vertex_to_ports ?(dst_filter=fun _ -> true) topo vertex =
 
 let locs topo : (vertex * int list) list =
   List.map (switches topo) ~f:(fun sw ->
-    vertex_to_ports topo sw
+    vertex_to_ports ~dst_filter:(fun _ -> true) topo sw
     |> List.map ~f:pt_val
     |> (fun pts -> (sw, pts))
   )

commit 1168f346425d8350ab23f9d742f2dc8d33b34a03

fix bug introduced in 4607698b39d05d2d8047c10b827faeb9a5797baa. Random walk between switches, not hosts

src/schemes.ml
@@ -214,7 +214,7 @@ let random_walk topo : Net.Topology.vertex -> string policy =
 let resilient_random_walk topo : Net.Topology.vertex -> string policy =
   fun sw ->
     let pts =
-      Topology.(vertex_to_ports topo.graph sw ~dst_filter:(is_host topo.graph))
+      Topology.(vertex_to_ports topo.graph sw ~dst_filter:(is_switch topo.graph))
       |> List.map ~f:Topology.pt_val
     in
     let choose_port = random_walk topo sw in