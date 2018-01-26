frenetic-lang/frenetic

commit 8433fae547feb54e0b314b28f10aff193422cc07

optimization: need up bits only for those ports connecting to other switches

src/Topology.ml
@@ -53,6 +53,14 @@ let locs topo : (vertex * int list) list =
     |> (fun pts -> (sw, pts))
   )
 
+(* return (sw, host-connected-ports, switch-connected-ports) triples  *)
+let locs' topo : (vertex * int list * int list) list =
+  List.map (switches topo) ~f:(fun sw ->
+    let host_ports = vertex_to_ports topo sw ~dst_filter:(is_host topo) in
+    let sw_ports = vertex_to_ports topo sw ~dst_filter:(is_switch topo) in
+    (sw, List.map host_ports ~f:pt_val, List.map sw_ports ~f:pt_val)
+  )
+
 let ingress_locs topo ~(dst: int) : (vertex * int) list =
   fold_edges (fun edge acc ->
       let (src_vertex,src_pt) = edge_src edge in

src/Model.ml
@@ -96,18 +96,22 @@ let make
 
   and hop () =
     let open PNK in
-    Topology.locs topo
-    |> List.filter ~f:(fun (sw,_pt) -> Topology.sw_val topo sw <> destination)
-    |> ite_cascade ~otherwise:drop ~f:(fun (sw, pts) ->
+    Topology.locs' topo
+    |> List.filter ~f:(fun (sw,_,_) -> Topology.sw_val topo sw <> destination)
+    |> ite_cascade ~otherwise:drop ~f:(fun (sw, host_pts, sw_pts) ->
       let sw_id = Topology.sw_val topo sw in
       let guard = ???(Params.sw, sw_id) in
       let body =
-        with_init_up_bits sw_id pts @@ begin
+        with_init_up_bits sw_id sw_pts @@ begin
           begin match sw_pol with
           | `Switchwise pol ->
             pol sw
           | `Portwise pol ->
-            ite_cascade pts ~otherwise:drop ~f:(fun pt_id ->
+            (* want rules for both ports connecting to hosts and ports connecting
+               to other switches
+             *)
+            List.append host_pts sw_pts
+            |> ite_cascade ~otherwise:drop ~f:(fun pt_id ->
               (???(pt, pt_id), pol sw pt_id)
             )
           end

commit 4607698b39d05d2d8047c10b827faeb9a5797baa

Fix bug introduced in 8433fae547feb54e0b314b28f10aff193422cc07. Avoid refering to up fields that were never introduced.

src/Topology.ml
@@ -38,7 +38,7 @@ let sw_val topo sw : int =
   |> Node.id
   |> Int.of_int64_exn
 
-let vertex_to_ports ?(dst_filter=fun _ -> true) topo vertex =
+let vertex_to_ports ~dst_filter topo vertex =
   neighbors topo vertex
   |> Set.filter ~f:dst_filter
   |> Set.to_list
@@ -48,7 +48,7 @@ let vertex_to_ports ?(dst_filter=fun _ -> true) topo vertex =
 
 let locs topo : (vertex * int list) list =
   List.map (switches topo) ~f:(fun sw ->
-    vertex_to_ports topo sw
+    vertex_to_ports ~dst_filter:(fun _ -> true) topo sw
     |> List.map ~f:pt_val
     |> (fun pts -> (sw, pts))
   )

src/schemes.ml
@@ -214,7 +214,7 @@ let random_walk topo : Net.Topology.vertex -> string policy =
 let resilient_random_walk topo : Net.Topology.vertex -> string policy =
   fun sw ->
     let pts =
-      Topology.vertex_to_ports topo.graph sw
+      Topology.(vertex_to_ports topo.graph sw ~dst_filter:(is_host topo.graph))
       |> List.map ~f:Topology.pt_val
     in
     let choose_port = random_walk topo sw in