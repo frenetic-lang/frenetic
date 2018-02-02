b52839ce7291117459fec74721516a7acb81e29f
Steffen Smolka <smolka@cs.cornell.edu>
2018-02-02 11:00:43 -0500

    clean up; remove obsolete files & dependencies

--- matlab/main.m
+++
@@ -1,4 +0,0 @@
-function [] = main(file) %#codegen
-% M = load('matrix.m');
-fprintf('Input file:%s\n', file);
-end
--- matlab/make.m
+++
@@ -1,3 +0,0 @@
-% matlab code invoking the C code generator "MATLAB Coder"
-cfg = coder.config('exe');
-codegen -c -config cfg -args [string] -d . main.m;
