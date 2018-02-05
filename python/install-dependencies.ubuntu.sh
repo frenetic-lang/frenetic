0da5c2add6f7ce78383c1a0f87fa490690c2ad06
Steffen Smolka <smolka@cs.cornell.edu>
2018-02-05 17:47:51 -0500

    update python dependency script

--- python/install-dependencies.ubuntu.sh
+++ python/install-dependencies.ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install libsuitesparse-dev
+sudo apt-get install libsuitesparse-dev python3-pip libcurl4-openssl-dev
 sudo -H python3 -m pip install --upgrade pip
 sudo -H python3 -m pip install \
   $(pip3 list --outdated --format=legacy | awk '{ print $1 }') --upgrade
