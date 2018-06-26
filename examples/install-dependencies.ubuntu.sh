37a3629e2de2fa3bfd958ffc73d0ec02233da4e8
Steffen Smolka <smolka@cs.cornell.edu>
2018-06-25 23:26:02 -0700

    Update install-dependencies.ubuntu.sh

--- examples/install-dependencies.ubuntu.sh
+++ examples/install-dependencies.ubuntu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo apt install python-dev graphviz libgraphviz-dev pkg-config
+
+sudo apt-add-repository -y ppa:aims/sagemath
+sudo apt update
+sudo apt install \
+  sagemath-upstream-binary python-dev graphviz libgraphviz-dev pkg-config
+
 sudo sage -pip install --upgrade pip
 sudo sage -pip install netaddr pygraphviz
-
