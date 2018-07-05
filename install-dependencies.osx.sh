5dbae5a3080c2e046bc795a0bf1d396962759033
Steffen Smolka <smolka@cs.cornell.edu>
2018-07-05 15:30:07 -0700

    update dependencies

--- install-dependencies.osx.sh
+++ install-dependencies.osx.sh
@@ -6,4 +6,4 @@ sudo -H python3 -m pip install --upgrade pip
 sudo -H python3 -m pip install \
   $(pip3 list --outdated --format=legacy | awk '{ print $1 }') --upgrade
 python3 -m pip install --user --upgrade \
-  numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-umfpack bson
+  numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-umfpack bson cython
