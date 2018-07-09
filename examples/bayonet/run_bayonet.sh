c494dfda046f96fba68f20efeca6b0f4fcbd9f39
Praveen Kumar <praveenk@cs.cornell.edu>
2018-07-09 18:47:26 -0400

    Add timeout for bayonet; Add probnetkat script

--- examples/bayonet/run_bayonet.sh
+++ examples/bayonet/run_bayonet.sh
@@ -3,7 +3,8 @@ BAYONET="../../../bayonet/bayonet"
 PSI="../../../psi/psi --noboundscheck --trace --dp --expectation --mathematica"
 INPUT="$1"
 PSI_FILE=${1%.bayonet}.psi
-RESULT_FILE=${1%.bayonet}.log
+RESULT_FILE=${1}.log
+TIMEOUT=3600
 
 if [[ -z $1 ]]; then
   echo "usage: $0 <bayonet-file>"
@@ -19,7 +20,7 @@ fi
 
 if [ ! -f $RESULT_FILE ]; then
     echo "analyzing: $PSI $PSI_FILE"
-    { { time $PSI $PSI_FILE; } 2>&1; } > $RESULT_FILE
+    { { time timeout $TIMEOUT $PSI $PSI_FILE; } 2>&1; } > $RESULT_FILE
     tail $RESULT_FILE
 else
     echo "skipping $PSI_FILE; $RESULT_FILE already exists."
---
+++ examples/bayonet/run_probnetkat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname $(readlink -f $0))"
+ROOT_DIR="${SCRIPT_DIR}/../../"
+PROBNETKAT="jbuilder exec probnetkat.bayonet"
+INPUT="${SCRIPT_DIR}/$1"
+RESULT_FILE="${SCRIPT_DIR}/${1%.dot}.probnetkat.log"
+TIMEOUT=3600
+
+if [[ -z $1 ]]; then
+  echo "usage: $0 <dot-file>"
+  exit 1
+fi
+
+if [ ! -f $RESULT_FILE ]; then
+    echo "analyzing: $INPUT"
+    pushd ${ROOT_DIR}
+    { { time timeout $TIMEOUT $PROBNETKAT $INPUT; } 2>&1; } > $RESULT_FILE
+    tail $RESULT_FILE
+    popd
+else
+    echo "skipping $INPUT; $RESULT_FILE already exists."
+    tail $RESULT_FILE
+fi
