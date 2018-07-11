ae58c757e54526e7f39191353fbdc30a508188a6
Praveen Kumar <praveenk@cs.cornell.edu>
2018-07-10 22:54:03 -0400

    Script to plot bayonet comparisons

---
+++ plots/bayonet.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import re
+import sys
+from collections import defaultdict
+from glob import glob
+
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
+label_of_method = {
+  'bayonet' : 'Bayonet',
+  'probnetkat' : 'ProbNetKAT',
+}
+
+markers = {
+  'bayonet' : 'o',
+  'probnetkat' : 's',
+}
+
+colors = {
+  'bayonet' : 'navy',
+  'probnetkat' : 'darkgreen',
+}
+
+def parse_output(folder):
+  results = []
+  for file in glob(os.path.join(folder, '*.log*')):
+    print (file)
+    params = re.match(r'.*bayonet.*sw_(?P<num_switches>\d+).(?P<method>\S+).log.*', file)
+    if params is None:
+      raise Exception ("Could not parse file name: " + file)
+    with open(file) as f:
+      for l in f.readlines():
+         realtime_line = re.match(r'real\t(?P<minutes>\d+)m(?P<seconds>\d+(\.\d*))s', l)
+         if realtime_line is not None:
+           minutes = int(realtime_line.groupdict()['minutes'])
+           seconds = float(realtime_line.groupdict()['seconds'])
+           result = dict()
+           result['time'] = minutes * 60 + seconds
+           result['num_switches'] = int(params.groupdict()['num_switches'])
+           result['method'] = params.groupdict()['method']
+           results.append(result)
+           break
+  return results
+  
+
+def plot(data, methods):
+  times = defaultdict(lambda: defaultdict(list))
+  time_mean = defaultdict(dict)
+  time_std = defaultdict(dict)
+  plt.figure(figsize=(6,4))
+  ax = plt.subplot(111)    
+  ax.get_xaxis().tick_bottom()    
+  ax.get_xaxis().set_ticks_position('both')
+  ax.get_yaxis().tick_left() 
+  ax.get_yaxis().set_ticks_position('both')
+  ax.tick_params(axis='both', which='both', direction='in')
+  ax.set_xscale("log", nonposx='clip')
+  ax.set_yscale("log", nonposy='clip')
+  for pt in data:
+    times[pt['method']][pt['num_switches']].append(pt['time'])
+  for method, sw_times in times.items():
+    if method in methods:
+      for sw, time_vals in sw_times.items():
+        time_mean[method][sw] = np.mean(time_vals)
+        time_std[method][sw] = np.std(time_vals)
+
+  for method, sw_times in sorted(time_mean.items()):
+    sorted_pts = sorted(sw_times.items())
+    xs, ys = zip(*sorted_pts)
+    errors = [time_std[method][x] for x in xs]
+    plt.errorbar(xs[:-1], ys[:-1], yerr=errors[:-1], label = label_of_method[method],
+                 marker=markers[method], color=colors[method], zorder=10)
+    plt.errorbar(xs[-2:], ys[-2:], yerr=errors[-2:],
+                 marker=markers[method], color='gray', ls=':', zorder=1)
+   
+
+  # Customize plots
+  ax.grid(alpha=0.2)
+  plt.xlim(1, 10000)
+  plt.ylim(0.5, 10000)
+  ax.fill_between([0,10000], 3600, ax.get_ylim()[1], facecolor='red', alpha=0.2)
+  ax.spines['bottom'].set_color('#999999')
+  ax.spines['top'].set_color('#999999') 
+  ax.spines['right'].set_color('#999999')
+  ax.spines['left'].set_color('#999999')
+
+  plt.xlabel("Number of switches")
+  plt.ylabel("Time (seconds)")
+  leg = plt.legend(fancybox=True, loc='best')
+  leg.get_frame().set_alpha(0.9)
+  plt.savefig('bayonet.pdf', bbox_inches='tight')
+
+
+def main(data_dir):
+  data = parse_output(data_dir)
+  plot(data, ['bayonet', 'probnetkat'])
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: " + sys.argv[0] + " <output directory with logs>")
+    sys.exit(1)
+  else:
+    main(sys.argv[1])
