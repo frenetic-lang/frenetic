9cfe302db60a0b549adb466ebb7a6766c657806f
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-05 15:11:21 -0500

    tweak bench script

--- examples/bayonet/run_bench.py
+++ examples/bayonet/run_bench.py
@@ -16,22 +16,28 @@ def prism(k):
 
 
 def run():
-  bayonet = pnk = prism = True
-  for i in range(1):
+  run_bayonet = run_pnk = run_prism = True
+  for i in range(14):
     k = 2**i
     print("k = %d" % k)
     print("=" * 80)
 
-    if bayonet:
-      bayonet = call(bayonet(k)) == 0
+    if run_bayonet:
+      print("\nBAYONET")
+      print("-" * 80)
+      run_bayonet = call(bayonet(k)) == 0
     
-    if pnk:
-      pnk = call(pnk(k)) == 0
+    if run_pnk:
+      print("\n\nPROBNETKAT")
+      print("-" * 80)
+      run_pnk = call(pnk(k)) == 0
     
-    if prism:
-      prism = call(prism(k)) == 0
+    if run_prism:
+      print("\n\nPRISM")
+      print("-" * 80)
+      run_prism = call(prism(k)) == 0
     
-    print("\n")
+    print("\n\n")
   
 
 if __name__ == "__main__":
