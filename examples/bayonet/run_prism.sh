f743eda5a09d2fd21cd3e4095e744b675ffd377b
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-05 11:21:29 -0500

    log prism result

--- examples/bayonet/run_prism.sh
+++ examples/bayonet/run_prism.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-BAYONET="../../../bayonet/bayonet"
-PSI="../../../psi/psi --noboundscheck --trace --dp --expectation --mathematica"
-INPUT="$1"
-PSI_FILE=${1%.bayonet}.psi
-RESULT_FILE=${1}.log
-TIMEOUT=3600
 
 if [[ -z $1 ]]; then
   echo "usage: $0 <TOPOLOGY FACTOR k>"
   exit 1
 fi
 
-prism prism/bayonet.pm prism/bayonet.pctl -const k=$1 -exact
+K="$1"
+SW=$(expr 4 \* ${K})
+RESULT_FILE="bayonet_resilience_sw_${SW}.prism.log"
+TIMEOUT=3600
+CMD="prism prism/bayonet.pm prism/bayonet.pctl -const k=${K} -exact"
+
+{ { time timeout $TIMEOUT $CMD; } 2>&1; } > $RESULT_FILE
+tail $RESULT_FILE
