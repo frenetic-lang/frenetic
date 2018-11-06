003636ca0c80831582e1bce9861e3bf4e9f5cdec
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-05 22:01:21 -0500

    update benchmark code to include approximate prism, pnk no cps

--- examples/bayonet/run_bench.py
+++ examples/bayonet/run_bench.py
@@ -14,7 +14,6 @@ def pnk(k, cps=True):
     return ['./run_probnetkat_no_cps.sh', 'bayonet_resilience_sw_%d.dot' % (4*k)]
 
 
-
 def prism(k, approx=False):
   if approx:
     return ['./run_prism_approx.sh', str(k)]
@@ -22,9 +21,8 @@ def prism(k, approx=False):
     return ['./run_prism.sh', str(k)]
 
 
-
 def run():
-  run_bayonet = run_pnk = run_prism = True
+  run_bayonet = run_pnk = run_pnk_no_cps = run_prism = run_prism_approx = True
   for i in range(14):
     k = 2**i
     print("k = %d" % k)
@@ -38,12 +36,22 @@ def run():
     if run_pnk:
       print("\n\nPROBNETKAT")
       print("-" * 80)
-      run_pnk = call(pnk(k)) == 0
+      run_pnk = call(pnk(k, cps=True)) == 0
+
+    if run_pnk_no_cps:
+      print("\n\nPROBNETKAT (no CPS)")
+      print("-" * 80)
+      run_pnk_no_cps = call(pnk(k, cps=False)) == 0
     
     if run_prism:
       print("\n\nPRISM")
       print("-" * 80)
-      run_prism = call(prism(k)) == 0
+      run_prism = call(prism(k, approx=False)) == 0
+
+    if run_prism_approx:
+      print("\n\nPRISM (approximate)")
+      print("-" * 80)
+      run_prism_approx = call(prism(k, approx=True)) == 0
     
     print("\n\n")
   
