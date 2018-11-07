60d4dcd4837312691200c7341769650af8c1ac78
Steffen Smolka <smolkaj@cs.cornell.edu>
2018-11-06 21:26:56 -0500

    update scripts

--- install-dependencies.ubuntu.sh
+++ install-dependencies.ubuntu.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 sudo apt update
-sudo apt install pkg-config python-dev libsuitesparse-dev python3-pip libcurl4-openssl-dev graphviz libgraphviz-dev swig
+sudo apt install python-dev graphviz libgraphviz-dev pkg-config
+sudo apt install libsuitesparse-dev
+sudo apt install python3-pip
+sudo apt install libcurl4-nss-dev
+sudo apt install swig
 
 # python 3, to solve linear systems
 sudo -H python3 -m pip install --upgrade pip
-sudo -H python3 -m pip install \
-  $(pip3 list --outdated --format=legacy | awk '{ print $1 }') --upgrade
+python3 -m pip list --outdated --format=freeze |\
+  grep -v '^\-e' |\
+  cut -d = -f 1  |\
+  xargs -n1 sudo -H python3 -m pip install --user --upgrade
 python3 -m pip install --user --upgrade \
-  numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-umfpack bson cython networkx pygraphviz
+  numpy scipy matplotlib ipython jupyter pandas sympy nose \
+  scikit-umfpack cython networkx pygraphviz bson
