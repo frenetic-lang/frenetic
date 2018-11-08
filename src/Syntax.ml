fe4d05b523d977f0bdc237e7db0839ad1dad6603
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-08 14:13:15 -0500

    PNK syntax: conjuncts, disjuncts, optimization

--- src/Syntax.ml
+++ src/Syntax.ml
@@ -57,6 +57,7 @@ let nnf (a : 'field pred) : 'field pred =
   in
   nnf a false
 
+(** disjunctive normal form *)
 let dnf (a : 'field pred) : (('field header_val * [`Eq | `Neq]) list) list =
   let rec nnf_to_dnf a =
     match a with
@@ -78,6 +79,27 @@ let dnf (a : 'field pred) : (('field header_val * [`Eq | `Neq]) list) list =
   in
   nnf_to_dnf (nnf a)
 
+let conjuncts a =
+  let exception IsFalse in
+  let rec go a acc =
+    match a with
+    | And (a,b) -> acc |> go b |> go a
+    | True -> acc
+    | False -> raise IsFalse
+    | a -> a::acc
+  in
+  try go a [] with IsFalse -> [False]
+
+let disjuncts a =
+  let exception IsTrue in
+  let rec go a acc =
+    match a with
+    | Or (a,b) -> acc |> go b |> go a
+    | True -> raise IsTrue
+    | False -> acc
+    | a -> a::acc
+  in
+  try go a [] with IsTrue -> [True]
 
 let nr_of_loops p =
   let rec do_pol p acc =
@@ -163,117 +185,135 @@ let pp_policy fmt (p : string policy) =
 
 (** constructors *)
 module Constructors = struct
-  (* module Dumb = struct *)
-    let drop = Filter False
-    let skip = Filter True
-    let test hv = Test hv
-    let filter a = Filter a
-    let modify hv = Modify hv
-    let observe p a = ObserveUpon (p,a)
-    (* let repeat n p = Repeat (n,p) *)
+  let drop = Filter False
+  let skip = Filter True
+  let test hv = Test hv
+  let filter a = Filter a
+  let modify hv = Modify hv
+  let observe p a = ObserveUpon (p,a)
+  (* let repeat n p = Repeat (n,p) *)
 
-    let neg a = match a with
-      | Neg a -> a
-      | _ -> Neg a
+  let neg = function
+    | True -> False
+    | False -> True
+    | Neg a -> a
+    | a -> Neg a
 
-    let disj a b = match a,b with
-      | True, _
-      | _, True -> True
-      | False, c
-      | c, False -> c
-      | _ -> Or (a, b)
+  let disj a b = match a,b with
+    | True, _
+    | _, True -> True
+    | False, c
+    | c, False -> c
+    | _ -> Or (a, b)
 
-    let mk_big_disj =
-      List.fold ~init:False ~f:disj
+  let mk_big_disj =
+    List.fold ~init:False ~f:disj
 
-    let conj a b = match a,b with
-      | False, _
-      | _, False -> False
-      | True, c
-      | c, True -> c
-      | _ -> And (a,b)
+  let conj a b = match a,b with
+    | False, _
+    | _, False -> False
+    | True, c
+    | c, True -> c
+    | _ -> And (a,b)
 
-    let seq p q = match p,q with
-      | Filter False, _
-      | _, Filter False -> Filter False
-      | Filter True, c
-      | c, Filter True -> c
-      | _ -> Seq (p, q)
+  let seq p q = match p,q with
+    | Filter False, _
+    | _, Filter False -> Filter False
+    | Filter True, c
+    | c, Filter True -> c
+    | Filter a, Filter b -> Filter (And (a,b))
+    | _ -> Seq (p, q)
 
-    let choice ps =
-      (* smash equal -> requires hashconsing *)
-      match List.filter ps ~f:(fun (p,r) -> not Prob.(equal r zero)) with
-      | [(p,r)] -> assert Prob.(equal r one); p
-      | ps -> Choice ps
+  let choice ps =
+    (* smash equal -> requires hashconsing *)
+    match List.filter ps ~f:(fun (p,r) -> not Prob.(equal r zero)) with
+    | [(p,r)] -> assert Prob.(equal r one); p
+    | ps -> Choice ps
 
-    let ite a p q = match a with
-      | True -> p
-      | False -> q
-      | _ -> Ite (a, p, q)
+  let ite a p q = match a with
+    | True -> p
+    | False -> q
+    | _ -> Ite (a, p, q)
 
-    let ite_cascade (xs : 'a list) ~(otherwise: 'field policy)
-      ~(f : 'a -> 'field pred * 'field policy) : 'field policy =
-      List.fold_right xs ~init:otherwise ~f:(fun x acc ->
-        let guard, body = f x in
-        ite guard body acc
-      )
+  let ite_cascade (xs : 'a list) ~(otherwise: 'field policy)
+    ~(f : 'a -> 'field pred * 'field policy) : 'field policy =
+    List.fold_right xs ~init:otherwise ~f:(fun x acc ->
+      let guard, body = f x in
+      ite guard body acc
+    )
 
-    let whl a p = match a with
-      | True -> drop
-      | False -> skip
-      | _ -> While (a,p)
+  let whl a p = match a with
+    | True -> drop
+    | False -> skip
+    | _ -> While (a,p)
+
+  let rec optimize = function
+    | Filter a -> filter (optimize_pred a)
+    | Modify hv -> modify hv
+    | Seq (p, q) -> seq (optimize p) (optimize q)
+    | Ite (a, p, q) -> ite (optimize_pred a) (optimize p) (optimize q)
+    | While (a, p) -> whl (optimize_pred a) (optimize p)
+    | Choice ps -> choice (Util.map_fst ps ~f:optimize)
+    | Let { id; init; mut; body } -> Let { id; init; mut; body = optimize body }
+    | ObserveUpon (p, a) -> observe (optimize p) (optimize_pred a)
+  and optimize_pred a =
+    match a with
+    | True | False | Test _ -> a
+    | Neg a -> neg (optimize_pred a)
+    | And (a, b) -> conj (optimize_pred a) (optimize_pred b)
+    | Or (a, b) -> disj (optimize_pred a) (optimize_pred b)
 
 (*     let bounded_whl a p ~bound = match a with
-      | True -> drop
-      | False -> skip
-      | _ -> repeat bound (ite a p skip) *)
+    | True -> drop
+    | False -> skip
+    | _ -> repeat bound (ite a p skip) *)
 
-    let do_whl a p =
-      seq p (whl a p)
+  let do_whl a p =
+    seq p (whl a p)
 
-    let conji n ~f =
-      Array.init n ~f
-      |> Array.fold ~init:True ~f:conj
+  let conji n ~f =
+    Array.init n ~f
+    |> Array.fold ~init:True ~f:conj
 
-    let disji n ~f =
-      Array.init n ~f
-      |> Array.fold ~init:False ~f:disj
+  let disji n ~f =
+    Array.init n ~f
+    |> Array.fold ~init:False ~f:disj
 
-    let seqi n ~f =
-      Array.init n ~f
-      |> Array.fold ~init:skip ~f:seq
+  let seqi n ~f =
+    Array.init n ~f
+    |> Array.fold ~init:skip ~f:seq
 
-    let mk_big_seq pols =
-      List.fold pols ~init:skip ~f:seq
+  let mk_big_seq pols =
+    List.fold pols ~init:skip ~f:seq
 
-    let then_observe a p =
-      ObserveUpon (p, a)
+  let then_observe a p =
+    ObserveUpon (p, a)
 
-    let choicei n ~f =
-      Array.init n ~f
-      |> Array.to_list
-      |> choice
+  let choicei n ~f =
+    Array.init n ~f
+    |> Array.to_list
+    |> choice
 
-    let uniformi n ~f =
-      choicei n ~f:(fun i -> (f i, Prob.(1//n)))
+  let uniformi n ~f =
+    choicei n ~f:(fun i -> (f i, Prob.(1//n)))
 
-    let uniform ps =
-      let ps = Array.of_list ps in
-      let n = Array.length ps in
-      uniformi n ~f:(fun i -> ps.(i))
+  let uniform ps =
+    let ps = Array.of_list ps in
+    let n = Array.length ps in
+    uniformi n ~f:(fun i -> ps.(i))
 
 
-    let mk_big_ite ~default = List.fold ~init:default ~f:(fun q (a, p) -> ite a p q)
+  let mk_big_ite ~default = List.fold ~init:default ~f:(fun q (a, p) -> ite a p q)
 
-    let alias (id, aliasee) ~(mut:bool) body =
-      Let { id; init = Alias aliasee; mut; body }
-    let local (id, value) ~(mut:bool) body =
-      Let { id; init = Const value; mut; body }
+  let alias (id, aliasee) ~(mut:bool) body =
+    Let { id; init = Alias aliasee; mut; body }
+  let local (id, value) ~(mut:bool) body =
+    Let { id; init = Const value; mut; body }
 
-    let locals binds body =
-      List.fold_right binds ~init:body ~f:(fun (id, value, mut) body ->
-        local (id,value) ~mut body
-      )
+  let locals binds body =
+    List.fold_right binds ~init:body ~f:(fun (id, value, mut) body ->
+      local (id,value) ~mut body
+    )
 
 end
 
