8b4e3259e31d05661746142188ee0f5b7bb3ed67
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-08 14:13:56 -0500

    prism compiler: prism understands arbitrary predicates; generate states in topological order for better readability

--- src/Prism.ml
+++ src/Prism.ml
@@ -83,7 +83,7 @@ module Automaton = struct
           add_state auto [
             { guard = pred;
               transitions = TransitionDist.none };
-            { guard = Neg pred;
+            { guard = PNK.neg pred;
               transitions = TransitionDist.dirac (Transition.to_state drop_state) };
           ]
         in
@@ -102,30 +102,33 @@ module Automaton = struct
         let auto = wire_all auto final_p start_q in
         (auto, start_p, final_q)
       | Ite (a, p, q) ->
+        let (start, auto) = add_state auto [] in
         let (auto, start_p, final_p) = thompson p auto in
         let (auto, start_q, final_q) = thompson q auto in
         let rules = [
             { guard = a;
               transitions = TransitionDist.dirac (Transition.to_state start_p) };
-            { guard = Neg a;
+            { guard = PNK.neg a;
               transitions = TransitionDist.dirac (Transition.to_state start_q) };
           ]
         in
-        let (start, auto) = add_state auto rules in
+        let auto = Map.set auto ~key:start ~data:rules in
         (auto, start, final_p @ final_q)
       | While (a, p) ->
+        let (start, auto) = add_state auto [] in
         let (auto, start_p, final_p) = thompson p auto in
         let (final, auto) = (add_state auto dummy_state) in
-        let (start, auto) = add_state auto [
+        let auto = Map.set auto ~key:start ~data:[
             { guard = a;
               transitions = TransitionDist.dirac (Transition.to_state start_p) };
-            { guard = Neg a;
+            { guard = PNK.neg a;
               transitions = TransitionDist.dirac (Transition.to_state final) };
           ]
         in
         let auto = wire_all auto final_p start in
         (auto, start, [final])
       | Choice (dist : (string policy * Prob.t) list) ->
+        let (start, auto) = add_state auto [] in
         let (pols, probs) = List.unzip dist in
         let auto, wires = List.fold_map pols ~init:auto ~f:(fun auto p ->
             let (auto, start, final) = thompson p auto in
@@ -137,7 +140,7 @@ module Automaton = struct
           List.zip_exn (List.map starts ~f:Transition.to_state) probs
           |> TransitionDist.of_alist_exn
         in
-        let (start, auto) = add_state auto [{
+        let auto = Map.set auto ~key:start ~data:[{
             guard = True;
             transitions;
           }]
@@ -145,9 +148,10 @@ module Automaton = struct
         (auto, start, List.concat finals)
       | Let { id; init; body; _ } ->
         (* SJS/FIXME: ensure id is "fresh"! *)
+        let (start, auto) = add_state auto [] in
         let (auto, start_body, final_body) = thompson body auto in
         (* initialize local field *)
-        let (start, auto) = add_state auto [{
+        let auto = Map.set auto ~key:start ~data:[{
             guard = True;
             transitions =
               Transition.to_state start_body
@@ -169,7 +173,7 @@ module Automaton = struct
         (auto, start, [final])
       | ObserveUpon (p, a) ->
         (* SJS/FIXME: ensure that p is idempotent! *)
-        thompson PNK.(do_whl (Neg a) p) auto
+        thompson PNK.(do_whl (neg a) p) auto
 
     in
     thompson p empty
@@ -255,19 +259,9 @@ module Ast = struct
     init : int option;
   }
 
-  type trans_rule = {
-    guard : trans_constr list;
-    transitions : TransitionDist.t;
-  }
-  and trans_constr = {
-    field : string;
-    value : int;
-    typ : [`Eq | `Neq];
-  }
-
   type model = {
     decls : field_decl list;
-    rules : trans_rule list;
+    rules : Automaton.rule list;
   }
 
   let decls_of_domain (dom : Domain.t) : field_decl list =
@@ -280,10 +274,10 @@ module Ast = struct
       }
     )
 
-  let rules_of_auto_rules ({ guard; transitions } : Automaton.rule)
-    (state_id : int) (subst : (int, int) Hashtbl.t) : trans_rule list =
+  let rule_of_auto_rule ({ guard; transitions } : Automaton.rule)
+    (state_id : int) (subst : (int, int) Hashtbl.t) : Automaton.rule =
+    let guard = PNK.(test (state, state_id) & guard) in
     (* rename states *)
-    let state_id = Hashtbl.find_exn subst state_id in
     let transitions =
       TransitionDist.pushforward transitions ~f:(fun t ->
         Base.Map.change t state ~f:(function 
@@ -293,52 +287,40 @@ module Ast = struct
         )
       )
     in
-    (* then compile to rules *)
-    let clauses = dnf guard in
-    List.map clauses ~f:(fun conjunct ->
-      let state_guard = { field = state; value = state_id; typ = `Eq } in
-      let guard =
-        List.map conjunct ~f:(fun ((field, value), typ) ->
-          { field; value; typ }
-        )
-        |> List.cons state_guard
-      in
-      { guard; transitions }
-    )
+    { guard; transitions }
     
-  let rules_of_auto (auto : Automaton.t) (subst : (int, int) Hashtbl.t) : trans_rule list =
+  let rules_of_auto (auto : Automaton.t) (subst : (int, int) Hashtbl.t) : Automaton.rule list =
     Map.to_alist auto
+    |> Util.map_fst ~f:(fun state_id -> Hashtbl.find_exn subst state_id)
+    |> List.sort ~compare:(fun (s, _) (t, _) -> Int.compare s t)
     |> List.concat_map ~f:(fun (state_id, rules) -> 
-      List.concat_map rules ~f:(fun rule -> 
-        rules_of_auto_rules rule state_id subst
+      List.map rules ~f:(fun rule ->
+        rule_of_auto_rule rule state_id subst
       )
     )
+    |> List.filter ~f:(function { guard = False; _ } -> false | _ -> true)
 
   let model_of_pol p ~(input_dist : input_dist) : model =
     let (start, auto, final) = Automaton.of_pol p ~input_dist in
     let state_subst = Int.Table.create () in
     (* rename states *)
-    Hashtbl.add_exn state_subst ~key:Automaton.drop_state ~data:0;
-    Hashtbl.add_exn state_subst ~key:final ~data:1;
-    Hashtbl.add_exn state_subst ~key:start ~data:2;
-    let max_state =
-      Map.keys auto
-      |> List.fold ~init:3 ~f:(fun new_id old_id ->
-        if List.exists [Automaton.drop_state; final; start] ~f:((=) old_id) then
-          new_id
-        else
-          (Hashtbl.add_exn state_subst ~key:old_id ~data:new_id; new_id + 1)
-      )
-    in
-    let domain = Domain.of_pol p in
+    Hashtbl.add_exn state_subst ~key:Automaton.drop_state ~data:(-1);
+    Hashtbl.add_exn state_subst ~key:final ~data:0;
+    Hashtbl.add_exn state_subst ~key:start ~data:1;
+    Map.keys auto
+    |> List.filter ~f:(fun id -> id <> Automaton.drop_state && id <> final && id <> start)
+    |> List.sort ~compare:Int.compare
+    |> List.iteri ~f:(fun i old_id ->
+        Hashtbl.add_exn state_subst ~key:old_id ~data:(i + 2)
+    );
 
     let state_decl = 
       { name = state;
-        lower_bound = 0;
-        upper_bound = max_state;
-        init = Some 2 }
+        lower_bound = -1;
+        upper_bound = Map.length auto - 2;
+        init = Some (Hashtbl.find_exn state_subst start) }
     in
-    let decls = state_decl :: decls_of_domain domain in
+    let decls = state_decl :: decls_of_domain (Domain.of_pol p) in
     let rules = rules_of_auto auto state_subst in
     { decls; rules}
 end
@@ -367,22 +349,31 @@ module Code = struct
     Format.sprintf "%s : [%d..%d]%s;"
       name lower_bound upper_bound (of_init init)
 
-  let of_typ = function
-    | `Eq -> "="
-    | `Neq -> "!="
-
   let of_value = function
     | `Int n -> Int.to_string n
     | `Field f -> f
-
-  let of_trans_constr Ast.{ field; value; typ } =
-    Format.sprintf "%s%s%d" field (of_typ typ) value
 
   let of_prob prob =
     match Prob.to_int_frac prob with
     | 0, _ -> "0"
     | 1,1 -> "1"
     | num, den -> Format.sprintf "%d/%d" num den
+
+  let of_pred (a : string pred) =
+    let rec do_pred a =
+      match a with
+      | True -> "true"
+      | False -> "false"
+      | Test (f,v) -> Format.sprintf "%s=%d" f v
+      | Neg (Test (f,v)) -> Format.sprintf "%s!=%d" f v
+      | Neg a -> Format.sprintf "!(%s)" (do_pred a)
+      | And _ -> of_many do_conjunct (conjuncts a) ~sep:" & "
+      | Or _ -> of_many do_pred (disjuncts a) ~sep:" | "
+    and do_conjunct = function
+      | Or _ as a -> Format.sprintf "(%s)" (do_pred a)
+      | a -> do_pred a
+    in
+    do_pred a
 
   let of_mod (field, value) =
     Format.sprintf "(%s'=%s)" field (of_value value)
@@ -392,9 +383,9 @@ module Code = struct
       (of_prob prob)
       (of_many of_mod ~sep:" & " (Base.Map.to_alist mods))
 
-  let of_rule Ast.{ guard; transitions } =
+  let of_rule Automaton.{ guard; transitions } =
     Format.sprintf "[] %s -> %s;"
-      (of_many of_trans_constr guard ~sep:" & ")
+      (of_pred guard)
       (of_many of_transition (TransitionDist.to_alist transitions) ~sep:" + ")
 
   let of_model (model : Ast.model) : string =
