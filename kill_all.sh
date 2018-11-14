8a71fda9c032741a151808d2341428333ccd31e0
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-14 18:09:44 -0500

    forgot to comit

--- kill_all.sh
+++ kill_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#
+# Kills all worker processes, locally and remotely.
+#
+
 sudo pkill -f RPC_PARALLEL_WORKER
 sudo pkill -f probnetkat
-for I in {1..24}
+
+for I in {24..1}
 do
   ssh -t "abilene@atlas-$I" "sudo pkill -f RPC_PARALLEL_WORKER"
   ssh -t "abilene@atlas-$I" "sudo pkill -f probnetkat"
