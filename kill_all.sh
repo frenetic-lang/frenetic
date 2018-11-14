edfc5b27496fc31ee89ff174eb85f21a3061731e
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-14 13:57:46 -0500

    update kill all script: we potentially use the entire cluster now

--- kill_all.sh
+++ kill_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pkill -f RPC_PARALLEL_WORKER
 sudo pkill -f probnetkat
-for I in 16 17 18 19 20 21 22 23 24
+for I in {1..24}
 do
   ssh -t "abilene@atlas-$I" "sudo pkill -f RPC_PARALLEL_WORKER"
   ssh -t "abilene@atlas-$I" "sudo pkill -f probnetkat"
