2a053f8c5a4eb982cf64c19da8ce8664ea95cbe6
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-13 23:24:25 -0500

    improved prism model to avoid warnings

--- examples/bayonet/prism/bayonet.pm
+++ examples/bayonet/prism/bayonet.pm
@@ -10,12 +10,21 @@ module Bayonet
   at_sw : [0..1] init 1;
   delivered : [0..1] init 0;
   
-  [] at_sw=1 & mod(sw, 4)=0 -> 0.5 : (pt'=1) & (at_sw'=0) + 0.5 : (pt'=2) & (at_sw'=0);
-  [] at_sw=1 & mod(sw, 4)=1 -> 1 : (pt'=2) & (at_sw'=0);
-  [] at_sw=1 & mod(sw, 4)=2 -> 0.999 : (pt'=1) & (at_sw'=0) + 0.001 : (pt'=-1) & (at_sw'=0);
-  [] at_sw=1 & mod(sw, 4)=3 -> 1 : (pt'=1) & (at_sw'=0);
+  [] at_sw=1 & delivered=0 & mod(sw, 4)=0 ->
+    0.5 : (pt'=1) & (at_sw'=0) + 0.5 : (pt'=2) & (at_sw'=0);
+  [] at_sw=1 & delivered=0 & mod(sw, 4)=1 ->
+    1 : (pt'=2) & (at_sw'=0);
+  [] at_sw=1 & delivered=0 & mod(sw, 4)=2 ->
+    0.999 : (pt'=1) & (at_sw'=0) + 0.001 : (pt'=-1) & (at_sw'=0);
+  [] at_sw=1 & delivered=0 & mod(sw, 4)=3 ->
+    1 : (pt'=1) & (at_sw'=0);
 
-  [] at_sw=0 & pt>=0 & sw+pt<4*k -> 1 : (sw'=sw+pt) & (pt'=mod(sw, 3)) & (at_sw'=1);
-  [] at_sw=0 & sw=4*k-1 -> 1 : (delivered'=1);
+  [] at_sw=0 & delivered=0 & pt>0 & sw+pt<4*k ->
+    1 : (sw'=sw+pt) & (pt'=mod(sw, 3)) & (at_sw'=1);
+  [] at_sw=0 & delivered=0 & sw=4*k-1 ->
+    1 : (delivered'=1);
+  [] at_sw=0 & delivered=0 & sw<4*k-1 & pt<=0 -> 1 : true;
+
+  [] delivered=1 -> 1 : true;
   
 endmodule
