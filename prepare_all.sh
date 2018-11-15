7cd50d79aebb17c7347ee8d338e8b71817ad541d
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-15 15:21:42 -0500

    prepare script: increase stack size and copy over binaries

--- prepare_all.sh
+++ prepare_all.sh
@@ -2,8 +2,17 @@
 #
 # Installs and starts memlockd on workers to avoid DOS.
 #
-
+make && make install
 for I in {24..1}
 do
-  ssh -t "abilene@atlas-$I" "sudo apt install memlockd && memlockd"
+  ssh -t "abilene@atlas-$I" << EOM
+sudo apt install memlockd
+memlockd
+if grep -Fxy "ulimit" ~/.bash_profile; then
+  # ulimit already set
+else
+  echo -e "\n# increase stack size\nulimit -S -s 131072\n" >> ~/.bash_profile
+fi
+EOM
+  scp $(which probnetkat.rpc_compile_branch) abilene@atlas-$I:/tmp/
 done
