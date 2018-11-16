282849ee62cd58cf0b5dac4113279d8cbccb1a0b
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-16 02:12:22 -0500

    fix more naming bugs

--- examples/output/rename.py
+++ examples/output/rename.py
@@ -6,20 +6,50 @@ from glob import glob
 
 
 def main():
-   for dot in glob("*.dot"):
-      d,file = os.path.split(dot)
+   for old in glob("*.dot"):
+      d,file = os.path.split(old)
       base, ext = os.path.splitext(file)
       match = re.match(r'^fattree_(\d+)$', base)
       if match is None:
-         print("no match for %s with base %s" % (dot, base))
+         print("no match for %s with base %s" % (old, base))
          continue
       k = int(match.group(1))
-      print("matched %s with k = %d" % (dot, k))
+      print("matched %s with k = %d" % (old, k))
       sw = (5 * k**2)/4
       base += "_sw_%d" % sw
       new = os.path.join(d, base) + ext
       print("-> new name: %s" % new)
-      os.rename(dot, new)
+      os.rename(old, new)
+
+   for old in glob("*-allsp.nexthops"):
+      d,file = os.path.split(old)
+      base, ext = os.path.splitext(file)
+      match = re.match(r'^fattree_(\d+)-allsp$', base)
+      if match is None:
+         print("no match for %s with base %s" % (old, base))
+         continue
+      k = int(match.group(1))
+      print("matched %s with k = %d" % (old, k))
+      sw = (5 * k**2)/4
+      base = "fattree_%d_sw_%d-allsp" % (k, sw)
+      new = os.path.join(d, base) + ext
+      print("-> new name: %s" % new)
+      os.rename(old, new)
+
+   for old in glob("*-spf.trees"):
+      d,file = os.path.split(old)
+      base, ext = os.path.splitext(file)
+      match = re.match(r'^fattree_(\d+)-spf$', base)
+      if match is None:
+         print("no match for %s with base %s" % (old, base))
+         continue
+      k = int(match.group(1))
+      print("matched %s with k = %d" % (old, k))
+      sw = (5 * k**2)/4
+      base = "fattree_%d_sw_%d-spf" % (k, sw)
+      new = os.path.join(d, base) + ext
+      print("-> new name: %s" % new)
+      os.rename(old, new)
 
 
 if __name__ == '__main__':
