5a535b698b3e6bdd35791e72329f80673503dcb2
Steffen Smolka <smolka@cs.cornell.edu>
2018-11-15 19:15:58 -0500

    fix

--- kill_all.sh
+++ kill_all.sh
@@ -7,11 +7,9 @@ sudo pkill -f RPC_PARALLEL_WORKER
 sudo pkill -f probnetkat
 sudo pkill -f java  # PRISM
 
-for I in {24..1}
-do
-  ssh -t "abilene@atlas-$I" << EOM
-sudo pkill -f RPC_PARALLEL_WORKER
-sudo pkill -f probnetkat
-sudo pkill -f java
-EOM
+for I in {24..1}; do
+  SSH="ssh -t "abilene@atlas-$I""
+  $SSH "sudo pkill -f RPC_PARALLEL_WORKER"
+  $SSH "sudo pkill -f probnetkat"
+  $SSH "sudo pkill -f java"
 done
