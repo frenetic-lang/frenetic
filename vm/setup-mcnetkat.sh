1df9a411efb8ccc637eddb8b5e05b50202aac65c
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-20 13:23:34 -0500

    vagrant

--- vm/setup-mcnetkat.sh
+++ vm/setup-mcnetkat.sh
@@ -6,4 +6,5 @@ git clone https://github.com/frenetic-lang/frenetic.git
 cd frenetic
 git checkout mc-decision
 opam pin add probnetkat . -y --working-dir --inplace-build
+opam pin add frenetic lib -y --working-dir --inplace-build
 opam install probnetkat -y
