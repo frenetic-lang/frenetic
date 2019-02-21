cada8cd1d216a61d246accdf5166794289b29f2b
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-20 19:36:46 -0500

    prism, bayonet

--- vm/setup-mcnetkat.sh
+++ vm/setup-mcnetkat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-# install latest version of ocaml
+# install mcnetkat, prism, bayonet
 git clone https://github.com/frenetic-lang/frenetic.git
 cd frenetic
 git checkout mc-decision
@@ -9,3 +9,4 @@ opam pin add frenetic lib -y --working-dir --inplace-build
 opam install frenetic -y
 opam pin add probnetkat . -y --working-dir --inplace-build
 opam install probnetkat -y
+cd examples/bayonet && source install_dependencies.sh
