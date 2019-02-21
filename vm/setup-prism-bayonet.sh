a194169927e60d57ccbdabdef67ed904ad395a99
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-20 19:55:13 -0500

    automate java installation

--- vm/setup-prism-bayonet.sh
+++ vm/setup-prism-bayonet.sh
@@ -2,9 +2,11 @@
 set -ex
 
 # install java (required by PRISM)
-add-apt-repository ppa:webupd8team/java
-apt-get update
+apt-get install -y software-properties-common python-software-properties
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+add-apt-repository -y -u ppa:webupd8team/java
 apt-get install -y oracle-java8-installer
+apt-get install oracle-java8-set-default
 
 # install PRISM, Bayonet
 cd frenetic/examples/bayonet && source install_dependencies.sh
