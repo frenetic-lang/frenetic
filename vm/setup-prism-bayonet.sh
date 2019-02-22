da356919e15fef75f1b970a8eff0c1e577f2c22f
Praveen Kumar <praveenk@cs.cornell.edu>
2019-02-22 11:48:16 -0500

    Reuse VM script for installing from source method

--- vm/setup-prism-bayonet.sh
+++ vm/setup-prism-bayonet.sh
@@ -3,11 +3,13 @@ set -ex
 
 DIR=`dirname "$(readlink -f "$0")"`
 # install java (required by PRISM)
-apt-get install -y software-properties-common python-software-properties
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-add-apt-repository -y -u ppa:webupd8team/java
-apt-get install -y oracle-java8-installer
-apt-get install oracle-java8-set-default
+sudo apt-get install -y software-properties-common python-software-properties
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository -y -u ppa:webupd8team/java
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
 
 # install PRISM, Bayonet
-cd ${DIR}/../examples/bayonet && source install_dependencies.sh
+pushd ${DIR}/../examples/bayonet
+bash install_dependencies.sh
+popd
