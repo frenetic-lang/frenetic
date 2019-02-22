53827958921484cc152b369b574a1b6603d0c987
Praveen Kumar <praveenk@cs.cornell.edu>
2019-02-22 13:28:19 -0500

    Move prism & bayonet installation in dependencies script for installation from source

--- install-dependencies.ubuntu.sh
+++ install-dependencies.ubuntu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -ex
+DIR=`dirname "$(readlink -f "$0")"`
+
 sudo apt update
 sudo apt install python-dev graphviz libgraphviz-dev pkg-config
 sudo apt install libsuitesparse-dev
@@ -15,3 +18,15 @@ python3 -m pip list --outdated --format=freeze |\
 python3 -m pip install --user --upgrade \
   numpy scipy matplotlib ipython jupyter pandas sympy nose \
   scikit-umfpack cython networkx pygraphviz bson
+
+# install java (required by PRISM)
+sudo apt-get install -y software-properties-common python-software-properties
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository -y -u ppa:webupd8team/java
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+# install PRISM, Bayonet
+pushd ${DIR}/examples/bayonet
+bash install_dependencies.sh
+popd
