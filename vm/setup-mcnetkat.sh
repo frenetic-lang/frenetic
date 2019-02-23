eca6efb3090a88de069380c071a01646447f0ef6
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-22 23:07:25 -0500

    tweaks

--- vm/setup-mcnetkat.sh
+++ vm/setup-mcnetkat.sh
@@ -2,9 +2,15 @@
 set -ex
 
 # install mcnetkat
-git clone https://github.com/frenetic-lang/frenetic.git
-cd frenetic
-git checkout mc-decision
+if [ ! -d "frenetic" ]; then
+  git clone -b mc-decision https://github.com/frenetic-lang/frenetic.git
+  cd frenetic
+else
+  cd frenetic
+  git fetch --all
+  git checkout mc-decision
+  git reset --hard origin/mc-decision
+fi
 opam pin add frenetic lib -y --working-dir --inplace-build --no-action
 opam pin add probnetkat . -y --working-dir --inplace-build
 opam install probnetkat -y
