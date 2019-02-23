d3e9a353a977297daf8048927cecd17a103bda4e
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-23 03:10:45 -0500

    fix

--- vm/setup-gui.sh
+++ vm/setup-gui.sh
@@ -2,6 +2,6 @@
 set -ex
 
 # Initialize the .opam and .bashrc PATH
-sudo echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
+echo "allowed_users=anybody" | sudo tee /etc/X11/Xwrapper.config
 sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 sudo VBoxClient --display
