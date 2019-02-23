690346767743c17dc408924731c05227ba13c6a0
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-23 03:07:32 -0500

    create vm

---
+++ vm/setup-gui.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+
+# Initialize the .opam and .bashrc PATH
+echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
+sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+sudo VBoxClient --display
