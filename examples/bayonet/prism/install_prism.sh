d338bce8051ca83088a01abf2c9b21ea53011e05
Steffen Smolka <smolka@cs.cornell.edu>
2019-02-22 19:39:20 -0500

    fix

--- examples/bayonet/prism/install_prism.sh
+++ examples/bayonet/prism/install_prism.sh
@@ -5,4 +5,5 @@ PRISM_ROOT="prism-4.4/prism"
 wget "https://github.com/prismmodelchecker/prism/archive/$ARCHIVE"
 unzip $ARCHIVE
 (cd $PRISM_ROOT && make)
-ln -s $(realpath ${PRISM_ROOT}/bin/prism) $HOME/bin
+mkdir -p $HOME/bin
+ln -s $(realpath ${PRISM_ROOT}/bin/prism) $HOME/bin/prism
